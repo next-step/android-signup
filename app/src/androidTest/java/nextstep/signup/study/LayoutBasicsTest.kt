590b870111cc90da896c1465fc7e7e09388db5d4
laco-dev <laco2951@gmail.com>
2024-02-07 20:50:02 +0900

    :white_check_mark: 텍스트가 일치하는지 확인하는 UI 테스트

--- app/src/androidTest/java/nextstep/signup/study/LayoutBasicsTest.kt
+++ app/src/androidTest/java/nextstep/signup/study/LayoutBasicsTest.kt
@@ -38,8 +38,7 @@ class LayoutBasicsTest {
         val text = "안녕 난 컴포즈야~"
         composeTestRule.setContent {
             Text(
-                // 바꿔 보세요!
-                text = "텍스트",
+                text = text,
                 style = TextStyle(
                     fontSize = 26.sp,
                     fontWeight = FontWeight.Bold,
