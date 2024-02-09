9787cb452655e01fe80f86c30aa5bee8d02ed040
laco-dev <laco2951@gmail.com>
2024-02-09 13:53:57 +0900

    :white_check_mark: Placeholder에 대한 학습 테스트를 작성한다

--- app/src/androidTest/java/nextstep/signup/study/TextFieldTest.kt
+++ app/src/androidTest/java/nextstep/signup/study/TextFieldTest.kt
@@ -1,6 +1,10 @@
 package nextstep.signup.study
 
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -10,6 +14,7 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performTextInput
+import androidx.compose.ui.tooling.preview.Preview
 import org.junit.Rule
 import org.junit.Test
 
@@ -52,5 +57,55 @@ class TextFieldTest {
                 it.performTextInput("윤컴포즈")
             }
             .assertTextContains("윤컴포즈")
+    }
+
+    @Test
+    fun placeholder() {
+        /**
+         * value 값이 비어있을 때 placeholder가 출력되지 않을까?
+         */
+        composeTestRule.setContent {
+            TextField(
+                value = "",
+                onValueChange = { },
+                placeholder = {
+                    Text("힌트", modifier = Modifier.testTag("1번"))
+                },
+            )
+
+            TextField(
+                value = "이름",
+                onValueChange = { },
+                placeholder = {
+                    Text("힌트", modifier = Modifier.testTag("2번"))
+                },
+            )
+        }
+
+        /**
+         * useUnmergedTree 파라미터를 지정하면 자식 컴포저블을 모두 찾는다.
+         */
+        composeTestRule
+            .onNodeWithTag("1번", useUnmergedTree = true)
+            .assertExists()
+
+        composeTestRule
+            .onNodeWithTag("2번", useUnmergedTree = true)
+            .assertDoesNotExist()
+    }
+}
+
+@Preview
+@Composable
+private fun TextFieldPreview() {
+    MaterialTheme {
+        TextField(
+            value = "이름",
+            onValueChange = { },
+            placeholder = {
+                Text("힌트")
+            },
+            modifier = Modifier.testTag("2번")
+        )
     }
 }
