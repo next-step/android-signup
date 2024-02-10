a7736132aeb9e81f3d4794b6ab3639417b80685b
laco-dev <laco2951@gmail.com>
2024-02-10 12:07:24 +0900

    이메일에 대한 유효성 검사 기능 추가

--- app/src/main/java/nextstep/signup/ui/component/EmailTextField.kt
+++ app/src/main/java/nextstep/signup/ui/component/EmailTextField.kt
@@ -1,6 +1,7 @@
 package nextstep.signup.ui.component
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -8,40 +9,51 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
 import nextstep.signup.R
 
 @Composable
 internal fun EmailTextField(
     email: String,
     onEmailChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
 ) {
+    val supportingText: @Composable (() -> Unit)? = when {
+        email.isEmpty() -> null
+        !EMAIL_REGEX.toRegex().matches(email) -> {
+            { Text(text = stringResource(id = R.string.signup_email_format_error)) }
+        }
+        else -> null
+    }
+
     TextField(
         value = email,
         onValueChange = onEmailChange,
-        modifier = Modifier.fillMaxWidth(),
-        placeholder = { Text(text = stringResource(id = R.string.signup_placeholder_email)) },
+        modifier = modifier.fillMaxWidth(),
+        placeholder = { Text(text = stringResource(id = R.string.signup_email_placeholder)) },
+        supportingText = supportingText,
+        isError = supportingText != null,
         singleLine = true,
     )
 }
 
-@Preview
-@Composable
-private fun EmailTextFieldPreview() {
-    MaterialTheme {
-        EmailTextField(
-            email = "test@test.com",
-            onEmailChange = {}
-        )
-    }
+private const val EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$"
+
+private class EmailTextFieldPreviewParameter : PreviewParameterProvider<String> {
+    override val values: Sequence<String>
+        get() = sequenceOf("", "test@test.com", "이메일")
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
-private fun EmailTextFieldEmptyPreview() {
+private fun EmailTextFieldPreview(@PreviewParameter(EmailTextFieldPreviewParameter::class) email: String) {
     MaterialTheme {
         EmailTextField(
-            email = "",
-            onEmailChange = {}
+            email = email,
+            onEmailChange = {},
+            modifier = Modifier.padding(16.dp),
         )
     }
 }
