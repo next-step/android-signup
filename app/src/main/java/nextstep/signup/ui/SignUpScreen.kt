9b0b851d5aecd14908c9ae4c85fe4fc890eaf32c
namjackson <skawo32167@naver.com>
2024-02-13 17:30:36 +0900

    2단계 - 회원가입(뷰) (#11)
    * Step2 : SignUpScreen 생성
    
    * Step2 : TextInputField 추가
    
    * Step2 : PasswordInputField 추가
    
    * Step2 : TextButton 추가

---
+++ app/src/main/java/nextstep/signup/ui/SignUpScreen.kt
@@ -0,0 +1,76 @@
+package nextstep.signup.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import nextstep.signup.ui.component.PasswordInputField
+import nextstep.signup.ui.component.TextButton
+import nextstep.signup.ui.component.TextInputField
+
+@Composable
+fun SignUpScreen() {
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .padding(32.dp)
+    ) {
+        Text(
+            text = "Welcome to Compose \uD83D\uDE80",
+            fontSize = 26.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        val userName = remember { mutableStateOf("") }
+        TextInputField(
+            modifier = Modifier.padding(top = 32.dp),
+            value = userName.value,
+            label = "UserName",
+            onValueChange = { userName.value = it }
+        )
+
+        val email = remember { mutableStateOf("") }
+        TextInputField(
+            modifier = Modifier.padding(top = 32.dp),
+            value = email.value,
+            label = "Email",
+            onValueChange = { email.value = it }
+        )
+
+        val password = remember { mutableStateOf("") }
+        PasswordInputField(
+            modifier = Modifier.padding(top = 32.dp),
+            value = password.value,
+            label = "Password",
+            onValueChange = { password.value = it }
+        )
+
+        val passwordConfirm = remember { mutableStateOf("") }
+        PasswordInputField(
+            modifier = Modifier.padding(top = 32.dp),
+            value = passwordConfirm.value,
+            label = "Password Confirm",
+            onValueChange = { passwordConfirm.value = it }
+        )
+
+        TextButton(
+            modifier = Modifier.padding(top = 32.dp),
+            text = "Sign Up",
+            onClick = {}
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SignUpScreenPreview() {
+    SignUpScreen()
+}
---
+++ app/src/main/java/nextstep/signup/ui/component/TextButton.kt
@@ -0,0 +1,28 @@
+package nextstep.signup.ui.component
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun TextButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    onClick: () -> Unit,
+) {
+    Button(
+        modifier = modifier.fillMaxWidth(),
+        onClick = onClick
+    ) {
+        Text(text = text)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TextButtonPreview() {
+    TextButton(text = "Preview Button", onClick = {})
+}
