bea4fb08e0e0bec25d729c4551956ffb09abf494
강현석 <kekemusa37@gmail.com>
2024-02-17 18:14:15 +0900

    [구현] Confirm 유효성 로직 분리

--- app/src/main/java/nextstep/signup/ui/component/ConfirmTextField.kt
+++ app/src/main/java/nextstep/signup/ui/component/ConfirmTextField.kt
@@ -14,33 +14,32 @@ import nextstep.signup.R
 
 @Preview
 @Composable
-fun PasswordTextField(
+fun PasswordConfirmTextField(
     modifier: Modifier = Modifier,
     password: String = "",
+    passwordConfirm: String = "",
     onValueChange: (String) -> Unit = {}
 ) {
     val supportingText: @Composable (() -> Unit)? =
-        when (PasswordError.checkBy(password = password)) {
-            PasswordError.OUT_OF_RANGE_LENGTH -> {
-                { Text(text = stringResource(R.string.password_length_error)) }
+        when (ConfirmError.checkBy(src = password, dst = passwordConfirm)) {
+            ConfirmError.NOT_MATCH -> {
+                { Text(text = stringResource(R.string.password_not_confirm_error)) }
             }
 
-            PasswordError.CANNOT_CONTAIN_ENGLISH_AND_NUMBERS -> {
-                { Text(text = stringResource(R.string.your_password_must_contain_english_and_numbers)) }
-            }
-
-            PasswordError.NONE -> {
+            ConfirmError.EMPTY,
+            ConfirmError.NONE -> {
                 null
             }
+
         }
     TextField(
-        modifier = modifier.fillMaxWidth(),
-        value = password,
+        modifier = Modifier.fillMaxWidth(),
+        value = passwordConfirm,
         onValueChange = { value ->
             onValueChange(value)
         },
         label = {
-            Text(stringResource(R.string.password))
+            Text(stringResource(R.string.password_confirm))
         },
         visualTransformation = PasswordVisualTransformation(),
         keyboardOptions = KeyboardOptions.Default.copy(
@@ -51,28 +50,20 @@ fun PasswordTextField(
     )
 }
 
-enum class PasswordError {
-    OUT_OF_RANGE_LENGTH,
-    CANNOT_CONTAIN_ENGLISH_AND_NUMBERS,
+enum class ConfirmError {
+    EMPTY,
+    NOT_MATCH,
     NONE;
 
     companion object {
-        private val VALID_RANGE = 8..16
-        private val PASSWORD_REGEX = "^(?=.*[a-zA-Z])(?=.*[0-9]).{8,16}$".toRegex()
-
-        fun checkBy(password: String): PasswordError {
+        fun checkBy(src: String, dst: String): ConfirmError {
             return when {
-                password.isEmpty() -> {
-                    NONE
+                dst.isEmpty() -> {
+                    EMPTY
                 }
 
-                password.length !in VALID_RANGE -> {
-                    OUT_OF_RANGE_LENGTH
-                }
-
-                PASSWORD_REGEX.matches(input = password)
-                    .not() -> {
-                    CANNOT_CONTAIN_ENGLISH_AND_NUMBERS
+                src != dst -> {
+                    NOT_MATCH
                 }
 
                 else -> {
