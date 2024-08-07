7ae9ed0afa0e40755aab2608db9b258b5b0e4a38
Min <35349220+1stpie@users.noreply.github.com>
2024-08-07 22:16:24 +0900

    test: step1 학습 테스트 구현 (#19)

---
+++ app/src/androidTest/java/nextstep/signup/study/LearningComposeTest.kt
@@ -0,0 +1,121 @@
+package nextstep.signup.study
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.assert
+import androidx.compose.ui.test.assertCountEquals
+import androidx.compose.ui.test.assertIsNotEnabled
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onChildren
+import androidx.compose.ui.test.onFirst
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import org.junit.Rule
+import org.junit.Test
+
+class LearningComposeTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun text() {
+        // given
+        val text = "안녕 난 컴포즈야~"
+        composeTestRule.setContent {
+            TextComponent()
+        }
+
+        // then
+        composeTestRule
+            .onNodeWithText(text)
+            .assertExists()
+    }
+
+    @Test
+    fun column() {
+        // given
+        composeTestRule.setContent {
+            ColumnComponent()
+        }
+
+        // then
+        composeTestRule.onNodeWithTag("이름")
+            .onChildren()
+            .assertCountEquals(3)
+            .onFirst()
+            .assert(hasText("깜포즈"))
+    }
+
+    @Test
+    fun button() {
+        // given
+        composeTestRule.setContent {
+            ButtonComponent()
+        }
+
+        // when
+        val button = composeTestRule
+            .onNodeWithTag("버튼")
+            .performClick()
+
+        // then
+        button.assertIsNotEnabled()
+    }
+}
+
+@Composable
+@Preview
+fun ButtonComponent() {
+    val enabled = remember { mutableStateOf(true) }
+    Button(
+        onClick = {
+            enabled.value = false
+        },
+        enabled = enabled.value,
+        modifier = Modifier.testTag("버튼")
+    ) {
+        Text(text = "클릭해주세요")
+    }
+}
+
+@Composable
+@Preview
+fun TextComponent() {
+    Text(
+        text = "안녕 난 컴포즈야~",
+        color = Color.Blue,
+        style = TextStyle(
+            fontSize = 26.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.SansSerif
+        ),
+    )
+}
+
+@Composable
+@Preview
+fun ColumnComponent() {
+    Column(
+        modifier = Modifier.testTag("이름")
+    ) {
+        // 바꿔 보세요!
+        Text(text = "깜포즈", color = Color.Cyan)
+        Text(text = "끔포즈", color = Color.Yellow)
+        Text(text = "킴포즈", color = Color.Green)
+    }
+}
