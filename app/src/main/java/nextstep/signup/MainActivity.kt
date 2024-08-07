6953a7ae3dbc69b6f3a3656f1c4d36d23d00f353
OYJ <oyj7677@gmail.com>
2024-08-07 22:07:04 +0900

    test : step1 학습 테스트 구현

--- app/src/main/java/nextstep/signup/MainActivity.kt
+++ app/src/main/java/nextstep/signup/MainActivity.kt
@@ -1,14 +1,19 @@
 package nextstep.signup
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import nextstep.signup.ui.theme.SignupTheme
 
@@ -17,30 +22,42 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SignupTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                Counter()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Counter() {
+    var number by remember { mutableStateOf(0) }
+
+    Column {
+        Text(text = "카운터 앱!")
+        Button(onClick = { number++ }) {
+            Text(text = "카운팅: ", fontWeight = FontWeight.Bold)
+            Text(text = number.toString())
+        }
+    }
+}
+
+@SuppressLint("UnrememberedMutableState")
+@Composable
+fun Checker() {
+    val checked = remember{
+        mutableStateOf(false)
+    }
+    Column {
+        Checkbox(
+            checked = checked.value,
+            onCheckedChange = { checked.value = !checked.value }
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    SignupTheme {
-        Greeting("Android")
-    }
+private fun HelloViewPreview() {
+    Counter()
+    Checker()
 }
