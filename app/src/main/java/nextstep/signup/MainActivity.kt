1d642a3fa13a13ff0758962da20bb07d0f6cf7fe
JINHYUK, JANG <ironelder@gmail.com>
2024-08-07 22:41:57 +0900

    test: step1 학습테스트 구현 (#24)
    * Compose 기초 ui 추가
    
    * Test파일 추가
    
    * Test 통과 테스트 구현
    
    * Test파일에서 Component 를 Preview되도록 분리

--- app/src/main/java/nextstep/signup/MainActivity.kt
+++ app/src/main/java/nextstep/signup/MainActivity.kt
@@ -3,13 +3,30 @@ package nextstep.signup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.Role.Companion.Checkbox
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import nextstep.signup.ui.theme.SignupTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +39,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column {
+                        Counter()
+                        Checker()
+                        Checker2()
+                    }
                 }
             }
         }
@@ -37,10 +58,77 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun HelloView(name: String) {
+    Text(
+        text = "$name 안녕하세요!",
+        fontSize = 20.sp,
+        color = Color.White,
+        modifier = Modifier
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(Color.Red, Color.Green),
+                    start = Offset.Zero,
+                    end = Offset.Infinite,
+                ),
+                shape = RoundedCornerShape(16.dp)
+            )
+            .padding(16.dp)
+    )
+}
+
+@Composable
+fun Counter() {
+    var number by remember { mutableStateOf(0) }
+    Column {
+        Text(text = "카운터 앱!")
+        Button(onClick = { number++ }) {
+            Text(text = "카운팅: ", fontWeight = FontWeight.Bold)
+            Text(text = number.toString())
+        }
+    }
+}
+
+@Composable
+fun Checker() {
+    val checked = remember { mutableStateOf(false) }
+    Column {
+        Checkbox(
+            checked = checked.value,
+            onCheckedChange = { checked.value = !checked.value }
+        )
+    }
+}
+
+@Composable
+fun Checker2() {
+    var checked by remember { mutableStateOf(false) }
+    Column {
+        Checkbox(
+            checked = checked,
+            onCheckedChange = { checked = !checked }
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HelloViewPreView() {
+    SignupTheme {
+        Column {
+            HelloView(name = "Ironelder")
+            Counter()
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SignupTheme {
-        Greeting("Android")
+        Column {
+            Greeting("Android")
+            HelloView(name = "Android")
+        }
     }
 }
