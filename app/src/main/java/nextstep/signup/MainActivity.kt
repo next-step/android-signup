8b339462fd6cec223b1b952aaf7fb3b4890313dc
s9hn <tnghqn@naver.com>
2024-08-09 00:18:33 +0900

    feat: 헤더 UI 구현

--- app/src/main/java/nextstep/signup/MainActivity.kt
+++ app/src/main/java/nextstep/signup/MainActivity.kt
@@ -4,19 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import nextstep.signup.ui.theme.RobotoBold
 import nextstep.signup.ui.theme.SignupTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //Greeting("Android")
+                    SignUpScreen()
                 }
             }
         }
@@ -37,28 +40,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun HelloView(
-    name: String,
-    modifier: Modifier = Modifier,
-) {
-    Text(
-        text = "$name 입니다",
-        fontSize = 20.sp,
-        color = Color.White,
-        modifier = modifier
-            .background(
-                brush = Brush.linearGradient(
-                    colors = listOf(Color.Blue, Color.Green),
-                    start = Offset.Zero,
-                    end = Offset.Infinite,
-                ),
-                shape = RoundedCornerShape(16.dp)
-            )
-    )
+fun SignUpScreen() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+            .padding(horizontal = 20.dp),
+    ) {
+        Spacer(modifier = Modifier.height(60.dp))
+        Text(
+            text = "Welcome to Compose \uD83D\uDE80",
+            color = Color.Black,
+            fontSize = 26.sp,
+            fontFamily = RobotoBold,
+        )
+
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-private fun HelloViewPreview() {
-    HelloView("세훈")
+private fun SignUpScreenPreview() {
+    SignUpScreen()
 }
