e1baffd38fae41b594083c95bdd64f6ecd78495e
1002533 <bf.winfive@sk.com>
2024-08-08 16:22:53 +0900

    feat : 타이틀 구현

--- app/src/main/java/nextstep/signup/MainActivity.kt
+++ app/src/main/java/nextstep/signup/MainActivity.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -22,10 +24,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SignupTheme {
-
+                Surface {
+                    TitleView("Welcome to Compose \uD83D\uDE80")
+                }
             }
         }
     }
+}
+
+@Preview
+@Composable
+private fun TitleView(
+    title: String = "Welcome to Compose \uD83D\uDE80"
+) {
+    Text(text = title, fontSize = 26.sp, color = Color(0xFF000000))
 }
 
 @Composable
