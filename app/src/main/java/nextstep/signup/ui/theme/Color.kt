dc54722006a4abc4efd3f110e1b0d52fee2f445f
s9hn <tnghqn@naver.com>
2024-08-08 23:54:49 +0900

    feat: 컬러 상수화

--- app/src/main/java/nextstep/signup/ui/theme/Color.kt
+++ app/src/main/java/nextstep/signup/ui/theme/Color.kt
@@ -2,6 +2,9 @@ package nextstep.signup.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val BlueGray20 = Color(0xFFE3E8F1)
+val Blue50 = Color(0xFF2196F3)
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
