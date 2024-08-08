07cbf5fa231199b8a7f56b4c4fe02d8dfb2a7051
ezhoon <dlwlgns1240@gmail.com>
2024-08-08 09:09:07 +0900

    feat: 기본 Greeting 제거 및 Screen Composable 추가

---
+++ app/src/main/java/nextstep/signup/screen/SignupScreen.kt
@@ -0,0 +1,11 @@
+package nextstep.signup.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SignupScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
