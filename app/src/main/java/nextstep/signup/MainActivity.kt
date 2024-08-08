7f9defcf6e4a4baa51bc5d5727913d49963f81f1
Jay Kang <jihoi.kang@gmail.com>
2024-08-08 21:43:53 +0900

    [step2] sign up screen (#35)
    * docs: create functional requirements
    
    * feat: add welcome text
    
    * feat: add username textfield
    
    * feat: add email textfield
    
    * feat: add password textfield
    
    * feat: add password confirm textfield
    
    * feat: add sign up button
    
    * style: add colors
    
    * refactor: clean up
    
    * feat: add custom textfield

--- app/src/main/java/nextstep/signup/MainActivity.kt
+++ app/src/main/java/nextstep/signup/MainActivity.kt
@@ -3,13 +3,12 @@ package nextstep.signup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import nextstep.signup.ui.signup.SignUpScreen
 import nextstep.signup.ui.theme.SignupTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +16,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SignupTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Scaffold { innerPadding ->
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                    ) {
+                        SignUpScreen()
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SignupTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/nextstep/signup/ui/signup/SignUpScreen.kt
@@ -0,0 +1,109 @@
+package nextstep.signup.ui.signup
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import nextstep.signup.R
+import nextstep.signup.ui.component.NSTextField
+import nextstep.signup.ui.theme.Blue50
+
+@Composable
+fun SignUpScreen(
+    modifier: Modifier = Modifier,
+) {
+    var username by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var passwordConfirm by remember { mutableStateOf("") }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 32.dp),
+    ) {
+        Text(
+            text = stringResource(id = R.string.welcome),
+            color = Color.Black,
+            fontSize = 26.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 60.dp),
+        )
+
+        NSTextField(
+            value = username,
+            onValueChange = { username = it },
+            labelValue = stringResource(id = R.string.username),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 32.dp),
+        )
+
+        NSTextField(
+            value = email,
+            onValueChange = { email = it },
+            labelValue = stringResource(id = R.string.email),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 32.dp),
+        )
+
+        NSTextField(
+            value = password,
+            onValueChange = { password = it },
+            labelValue = stringResource(id = R.string.password),
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 32.dp),
+        )
+
+        NSTextField(
+            value = passwordConfirm,
+            onValueChange = { passwordConfirm = it },
+            labelValue = stringResource(id = R.string.password_confirm),
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 32.dp),
+        )
+
+        Button(
+            onClick = { }, // todo: need to implement
+            colors = ButtonDefaults.buttonColors(containerColor = Blue50),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 32.dp)
+                .height(50.dp),
+        ) {
+            Text(text = stringResource(id = R.string.sign_up))
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SignUpScreenPreview() {
+    SignUpScreen()
+}
