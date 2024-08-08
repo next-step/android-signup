0c18b81337722ce993300b660778fe9666882193
ezhoon <dlwlgns1240@gmail.com>
2024-08-08 09:41:27 +0900

    feat: signup component 조합해서 screen 구성

--- README.md
+++ README.md
@@ -11,5 +11,5 @@
 - [x] Title 추가
 - [x] SignupTextField 구현
 - [x] SignupButton 구현
-- [ ] UI 컴포넌트 조합
+- [x] UI 컴포넌트 조합
 - [ ] 테스트 코드 확인
--- app/src/main/java/nextstep/signup/screen/SignupScreen.kt
+++ app/src/main/java/nextstep/signup/screen/SignupScreen.kt
@@ -1,12 +1,22 @@
 package nextstep.signup.screen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,15 +29,93 @@ import nextstep.signup.ui.theme.SignupTheme
 fun SignupScreen(
     modifier: Modifier = Modifier
 ) {
+    var userName by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var passwordConfirm by remember { mutableStateOf("") }
+
     Column(
-        modifier = modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = modifier
+            .fillMaxSize()
+            .padding(horizontal = 32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(42.dp)
     ) {
-        Text(
-            modifier = Modifier.padding(vertical = 42.dp),
-            text = stringResource(R.string.signup_title),
-            fontSize = 26.sp,
-            fontWeight = FontWeight.Bold
+        SignupTitle(text = stringResource(R.string.signup_title))
+
+        SignupInputComponent(
+            userName = userName,
+            email = email,
+            password = password,
+            passwordConfirm = passwordConfirm,
+            onUserNameChange = { userName = it },
+            onEmailChange = { email = it },
+            onPasswordChange = { password = it },
+            onPasswordConfirmChange = { passwordConfirm = it }
+        )
+
+        SignupButton(
+            onClick = { /* TODO */ }
+        )
+    }
+}
+
+@Composable
+private fun SignupTitle(
+    text: String,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        modifier = modifier.padding(top = 42.dp),
+        text = text,
+        fontSize = 26.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+
+@Composable
+private fun SignupInputComponent(
+    userName: String,
+    email: String,
+    password: String,
+    passwordConfirm: String,
+    onUserNameChange: (String) -> Unit,
+    onEmailChange: (String) -> Unit,
+    onPasswordChange: (String) -> Unit,
+    onPasswordConfirmChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(36.dp)
+    ) {
+        SignupTextField(
+            text = userName,
+            placeHolder = "UserName",
+            label = "UserName",
+            onValueChange = onUserNameChange
+        )
+
+        SignupTextField(
+            text = email,
+            placeHolder = "Email",
+            label = "Email",
+            onValueChange = onEmailChange
+        )
+
+        SignupTextField(
+            text = password,
+            placeHolder = "Password",
+            label = "Password",
+            onValueChange = onPasswordChange
+        )
+
+        SignupTextField(
+            text = passwordConfirm,
+            placeHolder = "Password Confirm",
+            label = "Password Confirm",
+            onValueChange = onPasswordConfirmChange
         )
     }
 }
@@ -36,12 +124,14 @@ fun SignupScreen(
 private fun SignupTextField(
     text: String,
     placeHolder: String,
+    label: String,
     onValueChange: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
     TextField(
         modifier = modifier.fillMaxWidth(),
         value = text,
+        label = { Text(text = label) },
         placeholder = { Text(text = placeHolder) },
         onValueChange = onValueChange,
         singleLine = true
@@ -89,6 +179,7 @@ private fun SignupTextFieldPreview() {
         SignupTextField(
             text = "이지훈",
             placeHolder = "이름",
+            label = "이름",
             onValueChange = {}
         )
     }
