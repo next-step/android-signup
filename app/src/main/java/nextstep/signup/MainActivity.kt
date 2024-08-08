613a57fde69a4950260163e7d0fab3a6798834d9
yongho2157 <youngho2157@naver.com>
2024-08-08 21:18:33 +0900

    feat: 회원가입 버튼 추가

--- app/src/main/java/nextstep/signup/MainActivity.kt
+++ app/src/main/java/nextstep/signup/MainActivity.kt
@@ -1,12 +1,17 @@
 package nextstep.signup
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -42,6 +48,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@SuppressLint("ResourceAsColor")
 @Composable
 fun SignUpScreen(
     modifier: Modifier = Modifier
@@ -87,6 +94,23 @@ fun SignUpScreen(
             value = passwordConfirm,
             onValueChange = { passwordConfirm = it },
             label = stringResource(id = R.string.passwordConfirmLabel)
+        )
+
+        Button(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(
+                disabledContainerColor = Color(R.color.blue),
+                disabledContentColor = Color.White
+            ),
+            content = {
+                Text(
+                    text = stringResource(id = R.string.signUp)
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .padding(20.dp)
         )
     }
 }
