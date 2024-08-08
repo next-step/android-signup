c20346ea36fcdd22023fc7f7a0587d5534f38d18
ezhoon <dlwlgns1240@gmail.com>
2024-08-08 10:07:37 +0900

    feat: Scaffold PaddingValues 적용

--- app/src/main/java/nextstep/signup/MainActivity.kt
+++ app/src/main/java/nextstep/signup/MainActivity.kt
@@ -3,7 +3,9 @@ package nextstep.signup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
 import nextstep.signup.screen.SignUpScreen
 import nextstep.signup.ui.theme.SignupTheme
 
@@ -13,7 +15,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             SignupTheme {
                 Scaffold {
-                    SignUpScreen()
+                    SignUpScreen(modifier = Modifier.padding(it))
                 }
             }
         }
