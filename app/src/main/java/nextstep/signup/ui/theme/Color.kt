590565543eadb1e5ff7f6f9e99360fa931e96d1f
eshc123 <>
2024-08-09 08:57:09 +0900

    feat: Color, Typography, string 추가

--- app/src/main/java/nextstep/signup/ui/theme/Color.kt
+++ app/src/main/java/nextstep/signup/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Blue50 = Color(0xFF2196F3)
+
+val Gray40 = Color(0xFF49454F)
+val Gray10 = Color(0xFF1D1B20)
