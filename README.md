4e7033831150120f4c33294d0c84655bcb0a657c
ezhoon <dlwlgns1240@gmail.com>
2024-08-09 21:19:13 +0900

    test: ValidatedTextField 실패 테스트 케이스 추가
    - 삽질 기록 포함

--- README.md
+++ README.md
@@ -22,3 +22,88 @@
 - [ ] 각 컴포넌트에 테스트 코드 구현
  - [ ] 성공 케이스
  - [ ] 실페 케이스
+
+
+### 삽질 기록
+
+```kotlin
+@Composable
+fun ValidatedTextField(
+    field: InputFieldModel,
+    modifier: Modifier = Modifier,
+) {
+    var errorRes by remember { mutableStateOf<Int?>(null) }
+    Column(modifier = modifier) {
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = "lee-ji-hoon",
+            label = field.label,
+            isError = errorRes != null,
+            onValueChange = {
+                field.onValueChange(it)
+                val result = field.validator.validate(it)
+                errorRes = if (result.isValid.not()) result.message else null
+            },
+            singleLine = true,
+            visualTransformation = field.visualTransformation
+        )
+        errorRes?.let {
+            Text(
+                text = stringResource(id = it),
+                color = MaterialTheme.colorScheme.error,
+                style = MaterialTheme.typography.labelSmall,
+                modifier = Modifier.padding(start = 16.dp, top = 4.dp)
+            )
+        }
+    }
+}
+
+class ValidatedTextFieldTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    private val context = InstrumentationRegistry.getInstrumentation().targetContext
+
+    private val username = mutableStateOf("")
+    private val validator = mutableStateOf<Validator>(NameValidator())
+
+    @Before
+    fun setup() {
+        composeTestRule.setContent {
+            ValidatedTextField(field = InputFieldModel(
+                value = username.value,
+                onValueChange = { username.value = it },
+                validator = validator.value,
+                label = { Text("사용자 이름") }
+            ))
+        }
+    }
+
+    @Test
+    fun 사용자_이름은_2에서_5자여야_한다() {
+        validator.value = NameValidator()
+        username.value = "lee-ji-hoon"
+
+        composeTestRule
+            .onNodeWithText(context.getString(R.string.signup_name_length_error))
+            .assertExists()
+    }
+}
+```
+
+> 위 학습 예시대로면 위 결과는 정상적으로 나와야 한다.
+
+<img width="2432" alt="image" src="https://github.com/user-attachments/assets/35fb3d32-47e3-4a01-b79e-489604345663">
+
+하지만 실제로는 테스트 코드가 실패하는데 lee-ji-hoon을 입력했을 때 Validate로 안잡히는 것인지 UI로 테스트 해보면 아래와 같다.
+
+<img width="572" alt="image" src="https://github.com/user-attachments/assets/7474985f-8892-452f-8853-42573afa96db">
+
+입력 결과 정상적으로 Error로 나오는 모습이라서 테스트 코드에 `assertExists()` 디버그 포인트를 찍고 확인해보니 무엇이 문제인지 확인이 됐다.
+
+<img width="537" alt="image" src="https://github.com/user-attachments/assets/a6e11422-de3d-42ad-ac46-f31c02df3bf1">
+
+`.value` 로 값을 넣는 상황인데 내가 짠 `TextField` 는 초기값은 아예 고려가 되지 않은 상태여서 에러가 나는 것이였다.
+
+그래서 테스트 코드를 `performTextInput` 로 변경하는 방법도 있지만 초기값을 체크하는게 맞다고 생각을 해서 `ValidateTextField` 자체를 수정하는 것으로 변경
--- app/src/androidTest/java/nextstep/signup/ui/component/ValidatedTextFieldTest.kt
+++ app/src/androidTest/java/nextstep/signup/ui/component/ValidatedTextFieldTest.kt
@@ -1,11 +1,17 @@
 package nextstep.signup.ui.component
 
 import androidx.compose.material3.Text
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.test.platform.app.InstrumentationRegistry
+import nextstep.signup.R
+import nextstep.signup.core.validation.EmailValidator
 import nextstep.signup.core.validation.NameValidator
+import nextstep.signup.core.validation.PasswordMatchValidator
+import nextstep.signup.core.validation.PasswordValidator
+import nextstep.signup.core.validation.Validator
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -14,67 +20,84 @@ class ValidatedTextFieldTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
-    private val emptyText = ""
-    private val userName = "이지훈"
-    private val labelText = "username-label"
-    private val labelTag = "username-label-tag"
+    private val context = InstrumentationRegistry.getInstrumentation().targetContext
 
-    /**
-     * given
-     *  - `emptyText` 제공
-     *  - `labelText` 제공
-     *
-     * then
-     *  - `SignUpTextField`에 text가 비어있어도 label은 존재해야 한다.
-     */
-    @Test
-    fun signUpEmptyTextField() {
+    private val textFieldValue = mutableStateOf("")
+    private val validator = mutableStateOf<Validator>(NameValidator())
+
+    @Before
+    fun setup() {
         composeTestRule.setContent {
             ValidatedTextField(
-                InputFieldModel(
-                    value = emptyText,
-                    onValueChange = {},
-                    validator = NameValidator(),
-                    label = {
-                        Text("labelText", modifier = Modifier.testTag(labelTag))
-                    }
+                field = InputFieldModel(
+                    value = textFieldValue.value,
+                    onValueChange = { textFieldValue.value = it },
+                    validator = validator.value,
+                    label = { Text("사용자 이름") }
                 )
             )
         }
+    }
+
+    @Test
+    fun 사용자_이름은_2에서_5자여야_한다() {
+        validator.value = NameValidator()
+        textFieldValue.value = "lee-ji-hoon"
 
         composeTestRule
-            .onNodeWithTag(labelTag, useUnmergedTree = true)
+            .onNodeWithText(context.getString(R.string.signup_name_length_error))
             .assertExists()
     }
 
-    /**
-     * given
-     *  - `userName` 제공
-     *  - `labelText` 제공
-     *
-     * then
-     *  - `SignUpTextField`에 text가 존재해도 label이 존재해야 한다.
-     */
     @Test
-    fun signUpTextField() {
-        composeTestRule.setContent {
-            ValidatedTextField(
-                InputFieldModel(
-                    value = userName,
-                    onValueChange = {},
-                    validator = NameValidator(),
-                    label = {
-                        Text(
-                            modifier = Modifier.testTag(labelTag),
-                            text = labelText
-                        )
-                    },
-                )
-            )
-        }
+    fun 사용자_이름은_숫자나_기호가_포함될_수_없다() {
+        validator.value = NameValidator()
+        textFieldValue.value = "lee1"
 
         composeTestRule
-            .onNodeWithTag(labelTag, useUnmergedTree = true)
+            .onNodeWithText(context.getString(R.string.signup_name_character_error))
+            .assertExists()
+    }
+
+    @Test
+    fun 비밀번호는_8에서_16자여야_한다() {
+        validator.value = PasswordValidator()
+        textFieldValue.value = "short1"
+
+
+        composeTestRule
+            .onNodeWithText(context.getString(R.string.signup_password_length_error))
+            .assertExists()
+    }
+
+    @Test
+    fun 비밀번호는_영문과_숫자를_포함해야_한다() {
+        validator.value = PasswordValidator()
+        textFieldValue.value = "Password"
+
+        composeTestRule
+            .onNodeWithText(context.getString(R.string.signup_password_complexity_error))
+            .assertExists()
+    }
+
+    @Test
+    fun 비밀번호가_일치해야_한다() {
+        val originalPassword = "CorrectPassword123"
+        validator.value = PasswordMatchValidator(originalPassword)
+        textFieldValue.value = "WrongPassword"
+
+        composeTestRule
+            .onNodeWithText(context.getString(R.string.signup_password_mismatch_error))
+            .assertExists()
+    }
+
+    @Test
+    fun 이메일은_올바른_형식이어야_한다() {
+        validator.value = EmailValidator()
+        textFieldValue.value = "invalid_email"
+
+        composeTestRule
+            .onNodeWithText(context.getString(R.string.sign_up_invalid_email))
             .assertExists()
     }
 }
