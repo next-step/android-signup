56b80b0d588a16867c2b04d89bf0421e28ffb018
eshc123 <>
2024-08-09 13:01:21 +0900

    feat: Sign Up Button 추가

--- app/src/main/java/nextstep/signup/screen/SignUpScreen.kt
+++ app/src/main/java/nextstep/signup/screen/SignUpScreen.kt
@@ -4,10 +4,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
@@ -17,6 +20,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
@@ -85,6 +89,21 @@ fun SignUpScreen(
                 labelText = stringResource(id = R.string.sign_up_password_confirm_label),
                 visualTransformation = PasswordVisualTransformation()
             )
+            TextButton(
+                modifier = Modifier
+                    .padding(top = 6.dp)
+                    .fillMaxWidth()
+                    .height(50.dp),
+                colors = ButtonDefaults.textButtonColors(
+                    containerColor = Blue50
+                ),
+                onClick = { /*TODO*/ }
+            ) {
+                Text(
+                    text = stringResource(id = R.string.sign_up_button),
+                    color = Color.White
+                )
+            }
         }
     }
 }
