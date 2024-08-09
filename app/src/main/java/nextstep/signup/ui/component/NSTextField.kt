b77bfe4e248b82db57934c939635e8e7001cda24
OYJ <89826723+oyj7677@users.noreply.github.com>
2024-08-09 13:30:07 +0900

    Step3 - 회원가입(유효성 검사) (#41)
    * Refactor : TextFieldCompose의 TextField 컴포넌트 속성에 maxLines 속성 추가
    
    1. TextField에 텍스트 1줄로 고정
    
    * Refactor : NSTextField 컴포넌트 이름 변경 TextFieldComposes -> SignUpTextField
    
    * Test : 회원가입에 사용되는 TextField별 테스트 추가
    
    1. UserName, Email, Password
    2. Password Confirm은 제외
    
    * Refactor : SignUpTextField/TextField의 testTag 속성 제거
    
    * Docs : Step3 READMD.md 작성
    
    * Feat: InputErrorText 컴포넌트 생성
    
    * Test : 입력값에 따른 유효성 검사 로직 테스트
    
    1. UserName, Email, Password 유효성 검사
    2. 조건 불일치 시 에러 메시지(Text 컴포넌트) 노출
    
    * Feat : TextField 카테고리 별 유효성 검증 로직 추가
    
    1. UserName, Email, Password, PasswordConfirm TextField에 로직 추가
    2. SignUpTextField의 text값을 State Hoisting 디자인 패턴 적용
    
    * Docs : READMD.md 수정
    
    * Style : 코드 오토 포맷팅
    
    * Docs : README.md 변경
    
    1. 입력 항목 별 컴포넌트 구현
    2. 입력 항목 별 유효성 검사
    
    * Refactor : 입력 항목 별 컴포넌트 생성 및 UI 테스트 구현
    
    1. UserName, Email, Password, PasswordConfirm 컴포넌트 생성
    2. 에러 텍스트 노출에 관한 유효성 검사 결과 파라미터로 받아서 처리
    
    * Test : InputErrorText 컴포넌트 변경에 따른 학습 테스트 코드 변경
    
    * Refactor : InputErrorText 파라미터 정리
    
    1. 사용하지 않는 text: String 파라미터 제거
    2. 관련 코드 수정
    
    * Refactor : SignUpTextField 컴포넌트 제거
    
    1. 입력 항목 별 컴포넌트 생성으로 사용하지 않아 제거
    2. 관련 학습 테스트 코드 수정

--- app/src/main/java/nextstep/signup/ui/component/NSTextField.kt
+++ app/src/main/java/nextstep/signup/ui/component/NSTextField.kt
@@ -1,29 +1,94 @@
 package nextstep.signup.ui.component
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
+import nextstep.signup.ui.screen.emailRegex
+import nextstep.signup.ui.screen.passwordRegex
+import nextstep.signup.ui.screen.userNameRegex
+
 
 @Composable
-fun TextFieldCompose(placeholder: String, isTextHidden: Boolean = false) {
-    var text by remember { mutableStateOf("") }
+fun UserNameTextField(text: String, onTextValueChange: (String) -> Unit) {
+    Column {
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth(),
+            label = { Text("UserName") },
+            value = text,
+            maxLines = 1,
+            onValueChange = onTextValueChange
+        )
 
-    TextField(
-        modifier = Modifier
-            .fillMaxWidth()
-            .testTag("user textField"),
-        visualTransformation = if (isTextHidden) PasswordVisualTransformation() else VisualTransformation.None,
-        placeholder = { Text(placeholder) },
-        value = text,
-        onValueChange = { text = it }
-    )
+        InputErrorText(
+            userNameErrorMsg,
+            !text.matches(Regex(userNameRegex)) && text.isNotEmpty()
+        )
+    }
 }
+
+@Composable
+fun EmailTextField(text: String, onTextValueChange: (String) -> Unit) {
+    Column {
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth(),
+            label = { Text("Email") },
+            value = text,
+            maxLines = 1,
+            onValueChange = onTextValueChange
+        )
+
+        InputErrorText(
+            emailErrorMsg,
+            !text.matches(Regex(emailRegex)) && text.isNotEmpty()
+        )
+    }
+}
+
+@Composable
+fun PasswordTextField(text: String, onTextValueChange: (String) -> Unit) {
+    Column {
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth(),
+            label = { Text("Password") },
+            value = text,
+            visualTransformation = PasswordVisualTransformation(),
+            maxLines = 1,
+            onValueChange = onTextValueChange
+        )
+        InputErrorText(
+            passwordErrorMsg,
+            !text.matches(Regex(passwordRegex)) && text.isNotEmpty()
+        )
+    }
+}
+
+@Composable
+fun PasswordConfirmTextField(text: String, onTextValueChange: (String) -> Unit, password: String) {
+    Column {
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth(),
+            label = { Text("PasswordConfirm") },
+            value = text,
+            visualTransformation = PasswordVisualTransformation(),
+            maxLines = 1,
+            onValueChange = onTextValueChange
+        )
+        InputErrorText(
+            passwordConfirmErrorMsg,
+            password != text && text.isNotEmpty()
+        )
+    }
+}
+
+private const val userNameErrorMsg = "이름은 2~5자여야 합니다.\n이름에는 숫자나 기호가 포함될 수 없습니다."
+private const val emailErrorMsg = "이메일 형식이 올바르지 않습니다."
+private const val passwordErrorMsg = "비밀번호는 8~16자, 영문, 숫자 조합이어야 합니다.\n비밀번호는 영문과 숫자를 포함해야 합니다."
+private const val passwordConfirmErrorMsg = "비밀번호가 일치하지 않습니다."
--- app/src/main/java/nextstep/signup/ui/screen/SignUpScreen.kt
+++ app/src/main/java/nextstep/signup/ui/screen/SignUpScreen.kt
@@ -8,6 +8,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
@@ -16,10 +20,27 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import nextstep.signup.ui.component.TextFieldCompose
+import nextstep.signup.ui.component.EmailTextField
+import nextstep.signup.ui.component.PasswordConfirmTextField
+import nextstep.signup.ui.component.PasswordTextField
+import nextstep.signup.ui.component.UserNameTextField
+
+const val userNameRegex = "^[a-zA-Z가-힣]{2,6}$"
+const val emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$"
+const val passwordRegex = "^(?=.*[a-zA-Z])(?=.*[0-9]).{8,16}$"
+
+const val userNameErrorMsg = "이름은 2~5자여야 합니다.\n이름에는 숫자나 기호가 포함될 수 없습니다."
+const val emailErrorMsg = "이메일 형식이 올바르지 않습니다."
+const val passwordErrorMsg = "비밀번호는 8~16자, 영문, 숫자 조합이어야 합니다.\n비밀번호는 영문과 숫자를 포함해야 합니다."
+const val passwordConfirmErrorMsg = "비밀번호가 일치하지 않습니다."
 
 @Composable
 fun SignUpScreen() {
+    var userName by rememberSaveable { mutableStateOf("") }
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    var passwordConfirm by rememberSaveable { mutableStateOf("") }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -40,17 +61,31 @@ fun SignUpScreen() {
             modifier = Modifier.padding(top = 62.dp),
             verticalArrangement = Arrangement.spacedBy(32.dp)
         ) {
-            TextFieldCompose("UserName")
-            TextFieldCompose("Email")
-            TextFieldCompose("Password", isTextHidden = true)
-            TextFieldCompose("Password Confirm", isTextHidden = true)
+            UserNameTextField(
+                userName,
+                onTextValueChange = { userName = it }
+            )
+
+            EmailTextField(
+                text = email,
+                onTextValueChange = { email = it }
+            )
+
+            PasswordTextField(
+                text = password,
+                onTextValueChange = { password = it }
+            )
+
+            PasswordConfirmTextField(
+                text = passwordConfirm,
+                onTextValueChange = { passwordConfirm = it },
+                password
+            )
         }
 
-        Button(
-            modifier = Modifier
-                .padding(32.dp)
-                .fillMaxWidth(),
-            onClick = {}) {
+        Button(modifier = Modifier
+            .padding(32.dp)
+            .fillMaxWidth(), onClick = {}) {
             Text(
                 text = "Sign Up",
                 modifier = Modifier.padding(top = 16.dp, bottom = 16.dp, start = 24.dp, end = 24.dp)
