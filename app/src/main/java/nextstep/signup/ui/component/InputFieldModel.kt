01947289cea426c749763dbd4e3598b60facc2d7
ezhoon <dlwlgns1240@gmail.com>
2024-08-09 21:18:22 +0900

    refactor: InputFieldModel @Stable 추가

--- app/src/main/java/nextstep/signup/ui/component/InputFieldModel.kt
+++ app/src/main/java/nextstep/signup/ui/component/InputFieldModel.kt
@@ -1,9 +1,11 @@
 package nextstep.signup.ui.component
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.text.input.VisualTransformation
 import nextstep.signup.core.validation.Validator
 
+@Stable
 data class InputFieldModel(
     val value: String,
     val onValueChange: (String) -> Unit,
