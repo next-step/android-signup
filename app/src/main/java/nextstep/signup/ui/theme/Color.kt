1fb8ffb37a05086302c033523db9da5daa3d5bee
realizer12 <dlwldlwldus123@gmail.com>
2024-08-10 20:26:33 +0900

    style: 필요한 string resource 및 color 정의

--- app/src/main/java/nextstep/signup/ui/theme/Color.kt
+++ app/src/main/java/nextstep/signup/ui/theme/Color.kt
@@ -1,6 +1,7 @@
 package nextstep.signup.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import nextstep.signup.R
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
@@ -9,3 +10,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val BlueGrey20 = Color(0xFFE3E8F1)
+val Blue50 = Color(0xFF2196F3)
