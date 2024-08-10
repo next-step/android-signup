0a67efc72512afa02c256670351436fcc95cead7
realizer12 <dlwldlwldus123@gmail.com>
2024-08-10 22:13:51 +0900

    feat: 회원가입 화면용 title 뷰용 composable 함수 추가

--- app/src/main/java/nextstep/signup/MainActivity.kt
+++ app/src/main/java/nextstep/signup/MainActivity.kt
@@ -1,32 +1,9 @@
 package nextstep.signup
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import nextstep.signup.screen.SignUpScreen
 import nextstep.signup.ui.theme.SignupTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,8 +11,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SignupTheme {
-
+                SignUpScreen()
             }
         }
     }
 }
+
+
---
+++ app/src/main/java/nextstep/signup/screen/SignUpScreen.kt
@@ -0,0 +1,79 @@
+package nextstep.signup.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import nextstep.signup.R
+
+@Preview
+@Composable
+fun SignUpScreen() {
+    Scaffold(
+        Modifier.background(Color.White)
+    ) { contentPadding ->
+        Column(
+            modifier = Modifier.padding(contentPadding)
+        ) {
+            SignUpTitle(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 70.dp)
+            )
+        }
+    }
+}
+
+
+
+
+/**
+ * 회원가입 제목용 composeable 함수
+ **/
+@Composable
+fun SignUpTitle(
+    modifier: Modifier
+) {
+    Text(
+        text = stringResource(id = R.string.signe_up_title),
+        modifier = modifier,
+        style = TextStyle(
+            fontSize = 26.sp,
+            fontFamily = FontFamily.SansSerif,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+    )
+}
+
+
+/**
+ * 회원가입 제목용 composeable 함수 미리보기
+ * (@preview 사용위해 parameter없는 composable 생성)
+ **/
+@Preview(
+    backgroundColor = 0xFFFFFFFFL,
+    showBackground = true,
+)
+@Composable
+fun PreviewSignUpTitle() {
+    SignUpTitle(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 70.dp)
+    )
+}
+
