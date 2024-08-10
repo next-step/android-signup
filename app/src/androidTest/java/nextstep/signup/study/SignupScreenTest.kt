0c73932f1342e9f329cbe85497e379d4d8670762
realizer12 <dlwldlwldus123@gmail.com>
2024-08-11 00:01:17 +0900

    test: 회원가입 title 뷰  알맞은 텍스트 있는지 여부 체크

--- app/src/androidTest/java/nextstep/signup/study/SignupScreenTest.kt
+++ app/src/androidTest/java/nextstep/signup/study/SignupScreenTest.kt
@@ -2,6 +2,7 @@ package nextstep.signup.study
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+<<<<<<< HEAD
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
@@ -51,52 +52,4 @@ class SignupScreenTest {
             .assertExists()
     }
 
-    /**
-     * 회원가입에 입력된 text가 일치하는지 체크
-     * password인경우는 불일치 나와야함 ***처리 임으로
-    **/
-    @Test
-    fun signUpTextFieldInputTextCheck(){
-        val signUpType = SignUpUserInputTextFieldType.entries.toTypedArray().random()
-
-        val testInputText = "test 입니다."
-
-        composeTestRule.setContent {
-            SigneUpTextField(
-                modifier = Modifier
-                    .testTag("signupTextField")
-                    .padding(
-                        top = 17.dp,
-                        bottom = 17.dp,
-                        start = 30.dp,
-                        end = 30.dp
-                    ),
-                label = signUpType.label,
-                keyboardType = signUpType.keyboardType,
-                visualTransformation = signUpType.visualTransformation,
-                textFiledColor = TextFieldDefaults.colors(
-                    focusedLabelColor = signUpType.focusedLabelColor,
-                    focusedIndicatorColor = signUpType.focusedIndicatorColor,
-                    cursorColor = signUpType.cursorColor,
-                    unfocusedContainerColor = signUpType.containerColor,
-                    focusedContainerColor = signUpType.containerColor
-                )
-            )
-        }
-
-        composeTestRule
-            .onNodeWithTag("signupTextField")
-            .performTextInput(testInputText)
-
-        composeTestRule
-            .onNodeWithTag("signupTextField")
-            .run {
-                //비밀번호 타입인 경우 text 일치하면 안됨.
-                if(signUpType.visualTransformation == PasswordVisualTransformation()) {
-                    assert(!hasText(testInputText))
-                }else{//비밀번호 타입 아닌 경우 text 일치해야함.
-                    assert(hasText(testInputText))
-                }
-            }
-    }
 }
