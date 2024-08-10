1b1b5b24ca2efcafa8d6e93ad1407264a79502b6
minhyukseul <mioyo2@gmail.com>
2024-08-11 00:46:00 +0900

    feat: 비밀번호는 영문과 숫자를 포함해야 한다.

--- app/src/androidTest/java/nextstep/signup/PasswordInputValidationTest.kt
+++ app/src/androidTest/java/nextstep/signup/PasswordInputValidationTest.kt
@@ -57,7 +57,41 @@ class PasswordInputValidationTest {
             .assertExists()
     }
 
+    @Test
+    fun 비밀번호는_영문과_숫자를_포함해야한다() {
+        // when
+        passwordData.value = "1q2w3e4r"
+
+        // then
+        composeTestRule
+            .onNodeWithText(PASSWORD_LENGTH_ERROR)
+            .assertDoesNotExist()
+    }
+
+    @Test
+    fun 비밀번호는_영문이_없으면_에러메시지_노출() {
+        // when
+        passwordData.value = "12345678"
+
+        // then
+        composeTestRule
+            .onNodeWithText(PASSWORD_REGEX_ERROR)
+            .assertDoesNotExist()
+    }
+
+    @Test
+    fun 비밀번호는_숫자가_없으면_에러메시지_노출() {
+        // when
+        passwordData.value = "abcdefghi"
+
+        // then
+        composeTestRule
+            .onNodeWithText(PASSWORD_REGEX_ERROR)
+            .assertDoesNotExist()
+    }
+
     companion object {
         private const val PASSWORD_LENGTH_ERROR = "비밀번호는 8~16자여야 합니다."
+        private const val PASSWORD_REGEX_ERROR = "비밀번호는 8~16자여야 합니다."
     }
 }
