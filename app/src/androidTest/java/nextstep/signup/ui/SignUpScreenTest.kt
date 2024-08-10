272fff3aa8b604d569af2b020ceaaddbdcaa32c3
jay.lee <stkment@gmail.com>
2024-08-11 01:15:15 +0900

    test: 회원가입 성공, 스낵바 노출 테스트 추가

--- app/src/androidTest/java/nextstep/signup/ui/SignUpScreenTest.kt
+++ app/src/androidTest/java/nextstep/signup/ui/SignUpScreenTest.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.test.assertIsEnabled
 import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import androidx.test.platform.app.InstrumentationRegistry
 import nextstep.signup.R
 import org.junit.Before
@@ -162,5 +163,25 @@ class SignUpScreenTest {
         composeTestRule
             .onNodeWithTag(context.getString(R.string.test_tag_btn_sign_up))
             .assertIsNotEnabled()
+    }
+
+    @Test
+    fun 회원가입_버튼을_누르면_회원가입이_완료되었다고_스낵바가_노출된다() {
+        // given
+        username.value = "user"
+        email.value = "user@yopmail.com"
+        password.value = "password12"
+        passwordConfirm.value = "password12"
+
+        // when
+        composeTestRule
+            .onNodeWithTag(context.getString(R.string.test_tag_btn_sign_up))
+            .assertIsEnabled()
+            .performClick()
+
+        // then
+        composeTestRule
+            .onNodeWithTag(context.getString(R.string.test_tag_snackbar))
+            .assertExists()
     }
 }
