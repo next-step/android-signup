3f653463edb6e2693ffc333285dd8c17946117cc
ezhoon <dlwlgns1240@gmail.com>
2024-08-11 13:53:50 +0900

    refactor: errorRes의 remember key값으로 field.validator 추가
    - 비밀번호 변경 확인처럼 Validato만 갱신이 되는 상황 고려

--- app/src/main/java/nextstep/signup/ui/component/ValidatedTextField.kt
+++ app/src/main/java/nextstep/signup/ui/component/ValidatedTextField.kt
@@ -2,31 +2,33 @@ package nextstep.signup.ui.component
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun ValidatedTextField(
     field: InputFieldModel,
     modifier: Modifier = Modifier,
 ) {
-    var errorRes by remember { mutableStateOf<Int?>(null) }
+    fun validateField(value: String): Int? {
+        return if (value.isNotEmpty()) {
+            val result = field.validator.validate(value)
+            if (result.isValid.not()) result.message else null
+        } else {
+            null
+        }
+    }
 
-    LaunchedEffect(field.value, field.validator) {
-        if (field.value.isEmpty()) return@LaunchedEffect
-        val result = field.validator.validate(field.value)
-        errorRes = if (result.isValid.not()) result.message else null
+    var errorRes by remember(field.validator) {
+        mutableStateOf(validateField(field.value))
     }
 
     Column(modifier = modifier) {
@@ -35,17 +37,21 @@ fun ValidatedTextField(
             value = field.value,
             label = field.label,
             isError = errorRes != null,
-            onValueChange = { field.onValueChange(it) },
+            onValueChange = {
+                field.onValueChange(it)
+                errorRes = validateField(it)
+            },
             singleLine = true,
-            visualTransformation = field.visualTransformation
+            visualTransformation = field.visualTransformation,
+            supportingText = {
+                errorRes?.let {
+                    Text(
+                        text = stringResource(id = it),
+                        color = MaterialTheme.colorScheme.error,
+                        style = MaterialTheme.typography.labelSmall,
+                    )
+                }
+            }
         )
-        errorRes?.let {
-            Text(
-                text = stringResource(id = it),
-                color = MaterialTheme.colorScheme.error,
-                style = MaterialTheme.typography.labelSmall,
-                modifier = Modifier.padding(start = 16.dp, top = 4.dp)
-            )
-        }
     }
 }
