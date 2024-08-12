cf3b7f446b5963d1b0f175198ef4a3c8fb3dcce0
장진혁/StoreClient팀/원스토어 <ironelder@onestorecorp.com>
2024-08-12 15:03:50 +0900

    Step2: 화면 구성 수정
     - 각 컴포넌트간의 여백을 수정하였습니다

--- app/src/main/java/nextstep/signup/ui/component/InputView.kt
+++ app/src/main/java/nextstep/signup/ui/component/InputView.kt
@@ -1,6 +1,7 @@
 package nextstep.signup.ui.component
 
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
@@ -10,7 +11,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
--- app/src/main/java/nextstep/signup/ui/screen/SignUpScreen.kt
+++ app/src/main/java/nextstep/signup/ui/screen/SignUpScreen.kt
@@ -2,6 +2,7 @@ package nextstep.signup.ui.screen
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -10,10 +11,13 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import nextstep.signup.ui.component.InputView
@@ -22,31 +26,37 @@ import nextstep.signup.ui.component.InputView
 fun SignUpScreen() {
     Scaffold { paddingValues ->
         Column(
-            modifier = Modifier.padding(paddingValues)
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(paddingValues),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
+                modifier = Modifier.padding(
+                    top = 112.dp,
+                ),
                 text = "Welcome to Compose \uD83D\uDE80",
                 fontWeight = FontWeight.Bold,
                 fontSize = 26.sp,
                 color = Color(0xFF000000)
             )
-            Spacer(modifier = Modifier.padding(8.dp))
+            Spacer(modifier = Modifier.padding(pxToDp(42)))
             InputView(hint = "UserName") {
                 
             }
-            Spacer(modifier = Modifier.padding(8.dp))
+            Spacer(modifier = Modifier.padding(pxToDp(42)))
             InputView(hint = "Email") {
 
             }
-            Spacer(modifier = Modifier.padding(8.dp))
+            Spacer(modifier = Modifier.padding(pxToDp(42)))
             InputView(hint = "Password") {
 
             }
-            Spacer(modifier = Modifier.padding(8.dp))
+            Spacer(modifier = Modifier.padding(pxToDp(42)))
             InputView(hint = "Password Confirm") {
 
             }
-            Spacer(modifier = Modifier.padding(8.dp))
+            Spacer(modifier = Modifier.padding(pxToDp(42)))
             Button(
                 modifier = Modifier
                     .width(296.dp)
@@ -68,4 +78,10 @@ fun SignUpScreen() {
 @Composable
 fun PreviewSignUpScreen() {
     SignUpScreen()
+}
+
+@Composable
+fun pxToDp(px: Int): Dp {
+    val density = LocalDensity.current
+    return with(density) { px.toDp() }
 }
