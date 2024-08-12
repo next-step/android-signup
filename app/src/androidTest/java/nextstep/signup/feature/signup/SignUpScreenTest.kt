12f006bd2607099a5341ffd522d9dd064c95e111
dino <sjjeong1225@gmail.com>
2024-08-13 02:55:28 +0900

    test: 회원가입 버튼 활성화 테스트코드 추가

--- app/src/androidTest/java/nextstep/signup/feature/signup/SignUpScreenTest.kt
+++ app/src/androidTest/java/nextstep/signup/feature/signup/SignUpScreenTest.kt
@@ -3,8 +3,11 @@ package nextstep.signup.feature.signup
 import android.content.Context
 import androidx.compose.ui.test.assert
 import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.isEnabled
+import androidx.compose.ui.test.isNotEnabled
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performTextInput
 import androidx.test.platform.app.InstrumentationRegistry
 import nextstep.signup.R
 import org.junit.Before
@@ -50,5 +53,72 @@ class SignUpScreenTest {
         composeTestRule.onNodeWithTag("passwordConfirm")
             .assertExists()
             .assert(hasText(context.getString(R.string.sign_up_password_confirm)))
+    }
+
+    @Test
+    fun 최초에_입력값이_없을땐_회원가입_버튼이_비활성화된다() {
+        composeTestRule.onNodeWithTag("signUpButton")
+            .assertExists()
+            .assert(isNotEnabled())
+    }
+
+    @Test
+    fun 모든_유효성_검사가_통과하면_회원가입_버튼이_활성화된다() {
+        composeTestRule.onNodeWithTag("username")
+            .performTextInput("dino")
+        composeTestRule.onNodeWithTag("email")
+            .performTextInput("dino@next.step")
+        composeTestRule.onNodeWithTag("password")
+            .performTextInput("password1234")
+        composeTestRule.onNodeWithTag("passwordConfirm")
+            .performTextInput("password1234")
+
+        composeTestRule.onNodeWithTag("signUpButton")
+            .assertExists()
+            .assert(isEnabled())
+    }
+
+    @Test
+    fun 이름을_입력하지_않으면_회원가입_버튼이_비활성화된다() {
+        composeTestRule.onNodeWithTag("email")
+            .performTextInput("dino@next.step")
+        composeTestRule.onNodeWithTag("password")
+            .performTextInput("password1234")
+        composeTestRule.onNodeWithTag("passwordConfirm")
+            .performTextInput("password123")
+
+        composeTestRule.onNodeWithTag("signUpButton")
+            .assertExists()
+            .assert(isNotEnabled())
+    }
+
+    @Test
+    fun 이메일을_입력하지_않으면_회원가입_버튼이_비활성화된다() {
+        composeTestRule.onNodeWithTag("username")
+            .performTextInput("dino")
+        composeTestRule.onNodeWithTag("password")
+            .performTextInput("password1234")
+        composeTestRule.onNodeWithTag("passwordConfirm")
+            .performTextInput("password123")
+
+        composeTestRule.onNodeWithTag("signUpButton")
+            .assertExists()
+            .assert(isNotEnabled())
+    }
+
+    @Test
+    fun 비밀번호와_비밀번호확인이_일치하지_않으면_회원가입_버튼이_비활성화된다() {
+        composeTestRule.onNodeWithTag("username")
+            .performTextInput("dino")
+        composeTestRule.onNodeWithTag("email")
+            .performTextInput("dino@next.step")
+        composeTestRule.onNodeWithTag("password")
+            .performTextInput("password1234")
+        composeTestRule.onNodeWithTag("passwordConfirm")
+            .performTextInput("password123")
+
+        composeTestRule.onNodeWithTag("signUpButton")
+            .assertExists()
+            .assert(isNotEnabled())
     }
 }
--- app/src/main/java/nextstep/signup/feature/signup/SignUpScreen.kt
+++ app/src/main/java/nextstep/signup/feature/signup/SignUpScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -88,7 +89,9 @@ fun SignUpScreen() {
             )
             Spacer(modifier = Modifier.height(42.dp))
             Button(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .testTag("signUpButton")
+                    .fillMaxWidth(),
                 onClick = {
                     scope.launch {
                         snackbarHostState.showSnackbar("회원가입 완료")
