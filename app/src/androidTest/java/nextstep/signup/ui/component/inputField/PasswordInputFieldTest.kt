11fe588eedaa4b4f1cf3df58c139aaed38c9cf49
dino <sjjeong1225@gmail.com>
2024-08-13 03:29:45 +0900

    test: 테스트 코드에서 string resource를 사용하는 코드를 제거하고 하드코드로 변경

--- app/src/androidTest/java/nextstep/signup/ui/component/inputField/PasswordInputFieldTest.kt
+++ app/src/androidTest/java/nextstep/signup/ui/component/inputField/PasswordInputFieldTest.kt
@@ -5,8 +5,6 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performTextInput
-import androidx.test.platform.app.InstrumentationRegistry
-import nextstep.signup.R
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -15,7 +13,6 @@ class PasswordInputFieldTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
-    private val context = InstrumentationRegistry.getInstrumentation().targetContext
     private val password = mutableStateOf("")
 
     @Before
@@ -32,7 +29,6 @@ class PasswordInputFieldTest {
     @Test
     fun 비밀번호_길이는_8자_이상_16자_이하여야_한다() {
         // given
-        val errorMessage = context.getString(R.string.sign_up_password_input_validation_length_message)
         val valueSource = listOf("password", "passwordpassword")
 
         valueSource.forEach {
@@ -41,7 +37,7 @@ class PasswordInputFieldTest {
 
             // then
             composeTestRule
-                .onNodeWithText(errorMessage)
+                .onNodeWithText("비밀번호는 8~16자여야 합니다.")
                 .assertDoesNotExist()
         }
     }
@@ -49,7 +45,6 @@ class PasswordInputFieldTest {
     @Test
     fun 비밀번호_길이가_8자_이상_16자_이하가_아니면_에러메시지가_노출된다() {
         // given
-        val errorMessage = context.getString(R.string.sign_up_password_input_validation_length_message)
         val valueSource = listOf("passwor", "passwordpasswordp")
 
         valueSource.forEach {
@@ -58,30 +53,24 @@ class PasswordInputFieldTest {
 
             // then
             composeTestRule
-                .onNodeWithText(errorMessage)
+                .onNodeWithText("비밀번호는 8~16자여야 합니다.")
                 .assertExists()
         }
     }
 
     @Test
     fun 비밀번호가_영문_또는_숫자_포함하지_않으면_에러메시지가_노출된다() {
-        // given
-        val errorMessage = context.getString(R.string.sign_up_password_input_validation_message)
-
         // when
         password.value = "password"
 
         // then
         composeTestRule
-            .onNodeWithText(errorMessage)
+            .onNodeWithText("비밀번호는 영문과 숫자를 포함해야 합니다.")
             .assertExists()
     }
 
     @Test
     fun 비밀번호와_비밀번호확인이_일치하지_않으면_에러메시지가_노출된다() {
-        // given
-        val errorMessage = context.getString(R.string.sign_up_password_not_equals_password_confirm_message)
-
         // when
         password.value = "password1234"
         composeTestRule.onNodeWithTag("passwordConfirm")
@@ -89,7 +78,7 @@ class PasswordInputFieldTest {
 
         // then
         composeTestRule
-            .onNodeWithText(errorMessage)
+            .onNodeWithText("비밀번호가 일치하지 않습니다.")
             .assertExists()
     }
 }
--- app/src/androidTest/java/nextstep/signup/ui/component/inputField/UsernameInputFieldTest.kt
+++ app/src/androidTest/java/nextstep/signup/ui/component/inputField/UsernameInputFieldTest.kt
@@ -3,8 +3,6 @@ package nextstep.signup.ui.component.inputField
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
-import androidx.test.platform.app.InstrumentationRegistry
-import nextstep.signup.R
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -13,7 +11,6 @@ class UsernameInputFieldTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
-    private val context = InstrumentationRegistry.getInstrumentation().targetContext
     private val username = mutableStateOf("")
 
     @Before
@@ -30,7 +27,6 @@ class UsernameInputFieldTest {
     @Test
     fun 사용자_이름은_2에서_5자여야_한다() {
         // given
-        val errorMessage = context.getString(R.string.sign_up_username_input_validation_length_message)
         val valueSource = listOf("김컴", "김컴포", "김컴포즈", "김컴포즈다")
 
         valueSource.forEach {
@@ -39,7 +35,7 @@ class UsernameInputFieldTest {
 
             // then
             composeTestRule
-                .onNodeWithText(errorMessage)
+                .onNodeWithText("이름은 2~5자여야 합니다.")
                 .assertDoesNotExist()
         }
     }
@@ -47,7 +43,6 @@ class UsernameInputFieldTest {
     @Test
     fun 사용자_이름이_2에서_5자가_아니면_에러메시지가_노출된다() {
         // given
-        val errorMessage = context.getString(R.string.sign_up_username_input_validation_length_message)
         val valueSource = listOf("김", "김컴포즈다아")
 
         valueSource.forEach {
@@ -56,22 +51,19 @@ class UsernameInputFieldTest {
 
             // then
             composeTestRule
-                .onNodeWithText(errorMessage)
+                .onNodeWithText("이름은 2~5자여야 합니다.")
                 .assertExists()
         }
     }
 
     @Test
     fun 사용자_이름이_특수문자를_포함하면_에러메시지가_노출된다() {
-        // given
-        val errorMessage = context.getString(R.string.sign_up_username_input_validation_message)
-
         // when
         username.value = "김컴포즈!"
 
         // then
         composeTestRule
-            .onNodeWithText(errorMessage)
+            .onNodeWithText("이름에는 숫자나 기호가 포함될 수 없습니다.")
             .assertExists()
     }
 }
