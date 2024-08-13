7f07e77da76960f084f5da4f4604af626c6b068c
diana <thxall92@github.com>
2024-08-13 21:08:43 +0900

    style: TextField 색상 변경

--- app/src/main/java/nextstep/signup/ui/component/InputTextField.kt
+++ app/src/main/java/nextstep/signup/ui/component/InputTextField.kt
@@ -1,14 +1,18 @@
 package nextstep.signup.ui.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import nextstep.signup.ui.theme.Blue50
+import nextstep.signup.ui.theme.BlueGrey20
 
 @Composable
 fun InputTextField(
@@ -20,7 +24,10 @@ fun InputTextField(
 ) {
     TextField(
         modifier = modifier
-            .then(Modifier.fillMaxWidth()),
+            .then(
+                Modifier
+                    .fillMaxWidth()
+                    .background(BlueGrey20)),
         value = value,
         onValueChange = onValueChange,
         label = { Text(label) },
@@ -30,5 +37,10 @@ fun InputTextField(
         } else {
             VisualTransformation.None
         },
+        colors = TextFieldDefaults.colors(
+            focusedLabelColor = Blue50,
+            focusedIndicatorColor = Blue50,
+            cursorColor = Blue50
+        )
     )
 }
