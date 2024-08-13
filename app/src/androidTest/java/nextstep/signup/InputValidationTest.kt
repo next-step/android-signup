3698791855bac20fd03b1bc31590d9e6855aec9d
OYJ <oyj7677@gmail.com>
2024-08-13 17:44:32 +0900

    Test : 회원가입 입력 값 유효성 결과 상태 관리 추가에 따른 테스트 코드 수정
    1. 관련 커밋 7a5ea8b

--- app/src/androidTest/java/nextstep/signup/InputValidationTest.kt
+++ app/src/androidTest/java/nextstep/signup/InputValidationTest.kt
@@ -33,7 +33,7 @@ class InputValidationTest {
     @Test
     fun 입력항목의_유효성_검사가_통과된다면_에러메시지가_노출되지_않는다() {
         // when : 사용자 이름이 2에서 5자를 넘어 유효성 검사를 통과한 경우
-        viewModel.userName = "김컴포즈"
+        viewModel.setUserName("김컴포즈")
 
         // then : 사용자 이름 에러 메시지가 노출되지 않는다
         composeTestRule
@@ -44,7 +44,7 @@ class InputValidationTest {
     @Test
     fun 입력항목의_유효성_검사가_통과되지_않으면_에러메시지가_노출된다() {
         // when : 사용자 이름이 2에서 5자를 넘어 유효성 검사를 통과하지 못한 경우
-        viewModel.userName = "김컴포즈입니다"
+        viewModel.setUserName("김컴포즈입니다")
 
         // then : 사용자 이름 에러 메시지가 노출된다
         composeTestRule
@@ -55,10 +55,10 @@ class InputValidationTest {
     @Test
     fun 모든_입력항목의_유효성_검사가_통과될_때_회원가입_버튼이_활성화된다() {
         // when
-        viewModel.userName = "김컴포즈"
-        viewModel.email = "oyj7677@gmail.com"
-        viewModel.password = "12345678dd"
-        viewModel.passwordConfirm = "12345678dd"
+        viewModel.setUserName("김컴포즈")
+        viewModel.setEmail("oyj7677@gmail.com")
+        viewModel.setPassword("12345678dd")
+        viewModel.setPasswordConfirm("12345678dd")
 
         composeTestRule
             .onNodeWithText(context.getString(R.string.sign_up))
@@ -68,10 +68,10 @@ class InputValidationTest {
     @Test
     fun 입력항목중_하나라도_통과하지_못한다면_회원가입_버튼은_비활성화된다() {
         // when : 이메일 형식이 올바르지 않은 경우
-        viewModel.userName = "김컴포즈"
-        viewModel.email = "oyj7677@gmail"
-        viewModel.password = "12345678dd"
-        viewModel.passwordConfirm = "12345678dd"
+        viewModel.setUserName("김컴포즈")
+        viewModel.setEmail("oyj7677@gmail")
+        viewModel.setPassword("12345678dd")
+        viewModel.setPasswordConfirm("12345678dd")
 
         composeTestRule
             .onNodeWithText(context.getString(R.string.sign_up))
@@ -80,10 +80,10 @@ class InputValidationTest {
 
     @Test
     fun 활성화된_회원가입_버튼을_누르면_스낵바가_노출된다() {
-        viewModel.userName = "김컴포즈"
-        viewModel.email = "oyj7677@gmail.com"
-        viewModel.password = "12345678dd"
-        viewModel.passwordConfirm = "12345678dd"
+        viewModel.setUserName("김컴포즈")
+        viewModel.setEmail("oyj7677@gmail.com")
+        viewModel.setPassword("12345678dd")
+        viewModel.setPasswordConfirm("12345678dd")
 
         composeTestRule
             .onNodeWithText(context.getString(R.string.sign_up))
@@ -96,10 +96,10 @@ class InputValidationTest {
 
     @Test
     fun 비활성화된_회원가입_버튼을_누르면_스낵바가_노출되지_않는다() {
-        viewModel.userName = "김컴포즈"
-        viewModel.email = "oyj7677@gmail.com"
-        viewModel.password = "12345678dd"
-        viewModel.passwordConfirm = "12345678"
+        viewModel.setUserName("김컴포즈")
+        viewModel.setEmail("oyj7677@gmail.com")
+        viewModel.setPassword("12345678dd")
+        viewModel.setPasswordConfirm("123456")
 
         composeTestRule
             .onNodeWithText(context.getString(R.string.sign_up))
--- app/src/test/java/nextstep/signup/SignUpViewModelTest.kt
+++ app/src/test/java/nextstep/signup/SignUpViewModelTest.kt
@@ -9,10 +9,10 @@ class SignUpViewModelTest {
     @Test
     fun 사용자_이름은_2에서_5자일_경우_유효성_검사를_통과한다() {
         // given
-        viewModel.userName = "김컴포즈"
+        viewModel.setUserName("김컴포즈")
 
         // when
-        val actual = viewModel.validateUserName()
+        val actual = viewModel.isUserNameError
 
         // then
         assertThat(actual).isTrue()
@@ -21,10 +21,10 @@ class SignUpViewModelTest {
     @Test
     fun 사용자_이름이_2에서_5자를_넘을_경우_유효성_검사를_통과하지_못한다() {
         // given
-        viewModel.userName = "김컴포즈입니다"
+        viewModel.setUserName("김컴포즈입니다")
 
         // when
-        val actual = viewModel.validateUserName()
+        val actual = viewModel.isUserNameError
 
         // then
         assertThat(actual).isFalse()
@@ -33,10 +33,10 @@ class SignUpViewModelTest {
     @Test
     fun 이메일_형식이_올바를_경우_유효성_검사를_통과한다() {
         // given
-        viewModel.email = "oyj7677@gmail.com"
+        viewModel.setEmail("oyj7677@gmail.com")
 
         // when
-        val actual = viewModel.validateEmail()
+        val actual = viewModel.isEmailError
 
         // then
         assertThat(actual).isTrue()
@@ -45,10 +45,10 @@ class SignUpViewModelTest {
     @Test
     fun 이메일_형식이_올바르지_않을_경우_유효성_검사를_통과하지_못한다() {
         // given
-        viewModel.email = "oyj7677@gmail"
+        viewModel.setEmail("oyj7677@gmail")
 
         // when
-        val actual = viewModel.validateEmail()
+        val actual = viewModel.isEmailError
 
         // then
         assertThat(actual).isFalse()
@@ -57,10 +57,10 @@ class SignUpViewModelTest {
     @Test
     fun 비밀번호는_8에서_16자이고_영문_숫자_조합이어야_한다() {
         // given
-        viewModel.password = "1234qwer"
+        viewModel.setPassword("1234qwer")
 
         // when
-        val actual = viewModel.validatePassword()
+        val actual = viewModel.isPasswordError
 
         // then
         assertThat(actual).isTrue()
@@ -69,10 +69,10 @@ class SignUpViewModelTest {
     @Test
     fun 비밀번호가_8에서_16자가_아닐_경우_유효성_검사를_통과하지_못한다() {
         // given
-        viewModel.password = "12a34"
+        viewModel.setPassword("12a34")
 
         // when
-        val actual = viewModel.validatePassword()
+        val actual = viewModel.isPasswordError
 
         // then
         assertThat(actual).isFalse()
@@ -81,10 +81,10 @@ class SignUpViewModelTest {
     @Test
     fun 비밀번호는_영문과_숫자를_포함하지_않을_경우_유효성_검사를_통과하지_못한다() {
         // given
-        viewModel.password = "12345678"
+        viewModel.setPassword("12345678")
 
         // when
-        val actual = viewModel.validatePassword()
+        val actual = viewModel.isPasswordError
 
         // then
         assertThat(actual).isFalse()
@@ -94,11 +94,11 @@ class SignUpViewModelTest {
     @Test
     fun 비밀번호와_비밀번호_확인값이_일치할_경우_유효성_검사를_통과한다() {
         // given
-        viewModel.password = "1234qwer"
-        viewModel.passwordConfirm = "1234qwer"
+        viewModel.setPassword("1234qwer")
+        viewModel.setPasswordConfirm("1234qwer")
 
         // when
-        val actual = viewModel.validatePasswordConfirm()
+        val actual = viewModel.isPasswordConfirmError
 
         // then
         assertThat(actual).isTrue()
@@ -107,11 +107,11 @@ class SignUpViewModelTest {
     @Test
     fun 비밀번호와_비밀번호_확인값이_일치하지_않을_경우_유효성_검사를_통과하지_못한다() {
         // given
-        viewModel.password = "1234qwer"
-        viewModel.passwordConfirm = "1234qwer!"
+        viewModel.setPassword("1234qwer")
+        viewModel.setPasswordConfirm("1234qwer!")
 
         // when
-        val actual = viewModel.validatePasswordConfirm()
+        val actual = viewModel.isPasswordConfirmError
 
         // then
         assertThat(actual).isFalse()
@@ -120,10 +120,10 @@ class SignUpViewModelTest {
     @Test
     fun 모든_입력값이_올바를_경우_전체_유효성_검사를_통과한다() {
         // given
-        viewModel.userName = "김컴포즈"
-        viewModel.email = "oyj7677@gmail.com"
-        viewModel.password = "1234qwer"
-        viewModel.passwordConfirm = "1234qwer"
+        viewModel.setUserName("김컴포즈")
+        viewModel.setEmail("oyj7677@gmail.com")
+        viewModel.setPassword("1234qwer")
+        viewModel.setPasswordConfirm("1234qwer")
 
         // when
         val actual = viewModel.validateAll()
@@ -135,10 +135,10 @@ class SignUpViewModelTest {
     @Test
     fun 모든_입력값이_올바르지_않을_경우_전체_유효성_검사를_통과하지_못한다() {
         // given : 비밀번호와 비밀번호 확인 값이 다름
-        viewModel.userName = "김컴포즈입니다"
-        viewModel.email = "oyj7677@gmail"
-        viewModel.password = "1234qwer"
-        viewModel.passwordConfirm = "1234qwer!"
+        viewModel.setUserName("김컴포즈")
+        viewModel.setEmail("oyj7677@gmail.com")
+        viewModel.setPassword("1234qwer")
+        viewModel.setPasswordConfirm("1234qwer!")
 
         // when
         val actual = viewModel.validateAll()
