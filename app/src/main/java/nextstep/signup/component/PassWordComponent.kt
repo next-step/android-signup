95e7e4a56214526ce6bcd4f0e8e6bdd724de5e1c
simjunbo <simuelunbo@gmail.com>
2024-08-14 20:10:37 +0900

    refactor: 회원 가입 비밀번호 컴포넌트 생성

---
+++ app/src/main/java/nextstep/signup/component/PassWordComponent.kt
@@ -0,0 +1,64 @@
+package nextstep.signup.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import nextstep.signup.component.textField.PasswordConfirmTextField
+import nextstep.signup.component.textField.PasswordTextField
+import nextstep.signup.model.PasswordConfirmState
+import nextstep.signup.model.PasswordState
+import nextstep.signup.valid.RegexBasedSignUpValidator
+import nextstep.signup.valid.SignUpValidator
+
+@Composable
+fun PasswordComponent(
+    password: String,
+    passwordConfirm: String,
+    onPasswordChange: (String) -> Unit,
+    onPasswordConfirmChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    validator: SignUpValidator = RegexBasedSignUpValidator()
+) {
+    val passwordState by remember(password) {
+        derivedStateOf { validator.validatePassword(password) }
+    }
+    val passwordConfirmState by remember(password, passwordConfirm) {
+        derivedStateOf { validator.validatePasswordConfirm(password, passwordConfirm) }
+    }
+
+    Column(modifier = modifier) {
+        PasswordTextField(
+            password = password,
+            onPasswordChange = onPasswordChange,
+            passwordState = passwordState
+        )
+
+        Spacer(modifier = Modifier.height(42.dp))
+
+        PasswordConfirmTextField(
+            passwordConfirm = passwordConfirm,
+            onPasswordConfirmChange = onPasswordConfirmChange,
+            passwordConfirmState = passwordConfirmState
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun PasswordComponentPreview() {
+    PasswordComponent(
+        password = "Password123",
+        passwordConfirm = "Password123",
+        onPasswordChange = {},
+        onPasswordConfirmChange = {}
+    )
+}
