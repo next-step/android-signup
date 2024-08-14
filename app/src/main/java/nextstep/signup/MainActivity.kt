60e21e5da4df4ae63ab304bace57c2cc3a3eba8b
THX All Grace <33936888+thxallgrace@users.noreply.github.com>
2024-08-14 11:07:09 +0900

    Step2 - 회원가입 (뷰) (#75)
    * docs: README 작성 - 기능 목록 정리
    
    * feat: 회원가입 뷰 구현
    
    * style: TextField 색상 변경
    
    ---------
    
    Co-authored-by: diana <thxall92@github.com>

--- app/src/main/java/nextstep/signup/MainActivity.kt
+++ app/src/main/java/nextstep/signup/MainActivity.kt
@@ -1,20 +1,15 @@
 package nextstep.signup
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import nextstep.signup.ui.signup.SignUpScreen
 import nextstep.signup.ui.theme.SignupTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,41 +17,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SignupTheme {
-                Checker()
+                Scaffold { paddingValues ->
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(paddingValues),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        SignUpScreen()
+                    }
+                }
             }
         }
     }
-}
-
-@Composable
-fun Counter() {
-    var number by remember { mutableStateOf(0) } // 2. 업데이트가 된다.
-
-    Column {
-        Text(text = "카운터 앱!")
-        Button(onClick = { number++ }) { // 1. 버튼이 눌린다.
-            Text(text = "카운팅: ", fontWeight = FontWeight.Bold)
-            Text(text = number.toString()) // 3. 숫자가 늘어난다.
-        }
-    }
-}
-
-@SuppressLint("UnrememberedMutableState")
-@Composable
-fun Checker() {
-    val checked = remember {
-        mutableStateOf(false)
-    }
-    Column {
-        Checkbox(
-            checked = checked.value,
-            onCheckedChange = { checked.value = !checked.value }
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun checkerPreview() {
-    Checker()
 }
---
+++ app/src/main/java/nextstep/signup/ui/component/InputTextField.kt
@@ -0,0 +1,46 @@
+package nextstep.signup.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import nextstep.signup.ui.theme.Blue50
+import nextstep.signup.ui.theme.BlueGrey20
+
+@Composable
+fun InputTextField(
+    modifier: Modifier = Modifier,
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String = "",
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default
+) {
+    TextField(
+        modifier = modifier
+            .then(
+                Modifier
+                    .fillMaxWidth()
+                    .background(BlueGrey20)),
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(label) },
+        keyboardOptions = keyboardOptions,
+        visualTransformation = if (keyboardOptions.keyboardType == KeyboardType.Password) {
+            PasswordVisualTransformation()
+        } else {
+            VisualTransformation.None
+        },
+        colors = TextFieldDefaults.colors(
+            focusedLabelColor = Blue50,
+            focusedIndicatorColor = Blue50,
+            cursorColor = Blue50
+        )
+    )
+}
