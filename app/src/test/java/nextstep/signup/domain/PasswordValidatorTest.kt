883f62bf395ed2aa869e769425803519fc2d7af4
Jihoi-Kang <jihoi.kang@gmail.com>
2024-08-14 10:16:55 +0900

    test: add validator test code

---
+++ app/src/test/java/nextstep/signup/domain/PasswordValidatorTest.kt
@@ -0,0 +1,68 @@
+package nextstep.signup.domain
+
+import org.junit.Assert
+import org.junit.Test
+
+class PasswordValidatorTest {
+
+    @Test
+    fun 비밀번호가_유효하면_VALID를_반환한다() {
+        // given
+        val password = "aa120000"
+
+        // when
+        val result = PasswordValidator.match(password)
+
+        // then
+        Assert.assertEquals(result, PasswordValidType.VALID)
+    }
+
+    @Test
+    fun 비밀번호가_8자리_미만이면_INVALID_LENGTH를_반환한다() {
+        // given
+        val password = "aa12000"
+
+        // when
+        val result = PasswordValidator.match(password)
+
+        // then
+        Assert.assertEquals(result, PasswordValidType.INVALID_LENGTH)
+    }
+
+    @Test
+    fun 비밀번호가_16자리_초과하면_INVALID_LENGTH를_반환한다() {
+        // given
+        val password = "aa120000aa120000a"
+
+        // when
+        val result = PasswordValidator.match(password)
+
+        // then
+        Assert.assertEquals(result, PasswordValidType.INVALID_LENGTH)
+    }
+
+    @Test
+    fun 비밀번호가_숫자로만_되어있으면_INVALID_REGEX를_반환한다() {
+        // given
+        val password = "11223344"
+
+        // when
+        val result = PasswordValidator.match(password)
+
+        // then
+        Assert.assertEquals(result, PasswordValidType.INVALID_REGEX)
+    }
+
+    @Test
+    fun 비밀번호가_영문으로만_되어있으면_INVALID_REGEX를_반환한다() {
+        // given
+        val password = "aabbccdd"
+
+        // when
+        val result = PasswordValidator.match(password)
+
+        // then
+        Assert.assertEquals(result, PasswordValidType.INVALID_REGEX)
+    }
+
+}
---
+++ app/src/test/java/nextstep/signup/domain/UsernameValidatorTest.kt
@@ -0,0 +1,68 @@
+package nextstep.signup.domain
+
+import org.junit.Assert
+import org.junit.Test
+
+class UsernameValidatorTest {
+
+    @Test
+    fun 사용자_이름이_유효하면_VALID를_반환한다() {
+        // given
+        val username = "jihoi"
+
+        // when
+        val result = UsernameValidator.match(username)
+
+        // then
+        Assert.assertEquals(result, UsernameValidType.VALID)
+    }
+
+    @Test
+    fun 사용자_이름이_2글자_미만이면_INVALID_LENGTH를_반환한다() {
+        // given
+        val username = "j"
+
+        // when
+        val result = UsernameValidator.match(username)
+
+        // then
+        Assert.assertEquals(result, UsernameValidType.INVALID_LENGTH)
+    }
+
+    @Test
+    fun 사용자_이름이_5글자_초과하면_INVALID_LENGTH를_반환한다() {
+        // given
+        val username = "jihoia"
+
+        // when
+        val result = UsernameValidator.match(username)
+
+        // then
+        Assert.assertEquals(result, UsernameValidType.INVALID_LENGTH)
+    }
+
+    @Test
+    fun 사용자_이름에_숫자가_포함되면_INVALID_REGEX를_반환한다() {
+        // given
+        val username = "jay1"
+
+        // when
+        val result = UsernameValidator.match(username)
+
+        // then
+        Assert.assertEquals(result, UsernameValidType.INVALID_REGEX)
+    }
+
+    @Test
+    fun 사용자_이름에_기호가_포함되면_INVALID_REGEX를_반환한다() {
+        // given
+        val username = "jay@"
+
+        // when
+        val result = UsernameValidator.match(username)
+
+        // then
+        Assert.assertEquals(result, UsernameValidType.INVALID_REGEX)
+    }
+
+}
