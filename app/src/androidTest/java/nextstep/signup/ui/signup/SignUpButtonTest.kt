feb753d2a3ad0798e9116d5c5b91e0dc76d32d13
Jihoi-Kang <jihoi.kang@gmail.com>
2024-08-15 22:33:39 +0900

    refactor: add sign up button

--- app/src/androidTest/java/nextstep/signup/ui/signup/SignUpButtonTest.kt
+++ app/src/androidTest/java/nextstep/signup/ui/signup/SignUpButtonTest.kt
@@ -1,14 +1,14 @@
 package nextstep.signup.ui.signup
 
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.test.assertIsEnabled
 import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithText
-import nextstep.signup.domain.SignUpValidator
-import nextstep.signup.model.SignUpParams
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import nextstep.signup.ui.component.SIGN_UP_BUTTON_TAG
+import nextstep.signup.ui.component.SignUpButton
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -17,103 +17,137 @@ class SignUpButtonTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
-    private val enabled = mutableStateOf(false)
+    private val username = mutableStateOf("")
+    private val email = mutableStateOf("")
+    private val password = mutableStateOf("")
+    private val passwordConfirm = mutableStateOf("")
+    private val clicked = mutableStateOf(false)
 
     @Before
     fun setup() {
         composeTestRule.setContent {
-            Button(
-                onClick = {  },
-                enabled = enabled.value,
-            ) {
-                Text(text = "abc")
-            }
+            SignUpButton(
+                username = username.value,
+                email = email.value,
+                password = password.value,
+                passwordConfirm = passwordConfirm.value,
+                onClick = { clicked.value = true },
+            )
         }
     }
 
     @Test
     fun 모든값이_유효하면_버튼은_활성화_상태이다() {
         // given
-        val username = "jihoi"
-        val email = "jihoi.kang@gmail.com"
-        val password = "aa120000"
-        val passwordConfirm = "aa120000"
+        username.value = "jihoi"
+        email.value = "jihoi.kang@gmail.com"
+        password.value = "aa120000"
+        passwordConfirm.value = "aa120000"
 
         // when
-        enabled.value = SignUpValidator.valid(SignUpParams(username, email, password, passwordConfirm))
 
         // then
         composeTestRule
-            .onNodeWithText("abc")
+            .onNodeWithTag(SIGN_UP_BUTTON_TAG)
             .assertIsEnabled()
     }
 
     @Test
     fun 사용자_이름이_유효하지_않으면_버튼은_비활성화_상태이다() {
         // given
-        val username = "jihoik"
-        val email = "jihoi.kang@gmail.com"
-        val password = "aa120000"
-        val passwordConfirm = "aa120000"
+        username.value = "jihoik"
+        email.value = "jihoi.kang@gmail.com"
+        password.value = "aa120000"
+        passwordConfirm.value = "aa120000"
 
         // when
-        enabled.value = SignUpValidator.valid(SignUpParams(username, email, password, passwordConfirm))
 
         // then
         composeTestRule
-            .onNodeWithText("abc")
+            .onNodeWithTag(SIGN_UP_BUTTON_TAG)
             .assertIsNotEnabled()
     }
 
     @Test
     fun 이메일이_유효하지_않으면_버튼은_비활성화_상태이다() {
         // given
-        val username = "jihoi"
-        val email = "jihoi.kang"
-        val password = "aa120000"
-        val passwordConfirm = "aa120000"
+        username.value = "jihoi"
+        email.value = "jihoi.kang"
+        password.value = "aa120000"
+        passwordConfirm.value = "aa120000"
 
         // when
-        enabled.value = SignUpValidator.valid(SignUpParams(username, email, password, passwordConfirm))
 
         // then
         composeTestRule
-            .onNodeWithText("abc")
+            .onNodeWithTag(SIGN_UP_BUTTON_TAG)
             .assertIsNotEnabled()
     }
 
     @Test
     fun 비밀번호가_유효하지_않으면_버튼은_비활성화_상태이다() {
         // given
-        val username = "jihoi"
-        val email = "jihoi.kang@gmail.com"
-        val password = "aa12"
-        val passwordConfirm = "aa12"
+        username.value = "jihoi"
+        email.value = "jihoi.kang@gmail.com"
+        password.value = "aa12"
+        passwordConfirm.value = "aa12"
 
         // when
-        enabled.value = SignUpValidator.valid(SignUpParams(username, email, password, passwordConfirm))
 
         // then
         composeTestRule
-            .onNodeWithText("abc")
+            .onNodeWithTag(SIGN_UP_BUTTON_TAG)
             .assertIsNotEnabled()
     }
 
     @Test
     fun 두_비밀번호가_일치하지_않으면_버튼은_비활성화_상태이다() {
         // given
-        val username = "jihoi"
-        val email = "jihoi.kang@gmail.com"
-        val password = "aa120000"
-        val passwordConfirm = "aa120001"
+        username.value = "jihoi"
+        email.value = "jihoi.kang@gmail.com"
+        password.value = "aa120000"
+        passwordConfirm.value = "aa120001"
 
         // when
-        enabled.value = SignUpValidator.valid(SignUpParams(username, email, password, passwordConfirm))
 
         // then
         composeTestRule
-            .onNodeWithText("abc")
+            .onNodeWithTag(SIGN_UP_BUTTON_TAG)
             .assertIsNotEnabled()
+    }
+
+    @Test
+    fun 버튼을_누르면_clicked는_true가_된다() {
+        // given
+        username.value = "jihoi"
+        email.value = "jihoi.kang@gmail.com"
+        password.value = "aa120000"
+        passwordConfirm.value = "aa120000"
+
+        // when
+        composeTestRule
+            .onNodeWithTag(SIGN_UP_BUTTON_TAG)
+            .performClick()
+
+        // then
+        Assert.assertEquals(clicked.value, true)
+    }
+
+    @Test
+    fun 버튼이_비활성화인_상태에서_버튼을_눌러도_clicked는_바뀌지않고_false이다() {
+        // given
+        username.value = ""
+        email.value = ""
+        password.value = ""
+        passwordConfirm.value = ""
+
+        // when
+        composeTestRule
+            .onNodeWithTag(SIGN_UP_BUTTON_TAG)
+            .performClick()
+
+        // then
+        Assert.assertEquals(clicked.value, false)
     }
 
 }
--- app/src/main/java/nextstep/signup/ui/signup/SignUpScreen.kt
+++ app/src/main/java/nextstep/signup/ui/signup/SignUpScreen.kt
@@ -2,10 +2,7 @@ package nextstep.signup.ui.signup
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -22,13 +19,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import nextstep.signup.R
-import nextstep.signup.domain.SignUpValidator
-import nextstep.signup.model.SignUpParams
 import nextstep.signup.ui.component.EmailTextField
 import nextstep.signup.ui.component.PasswordConfirmTextField
 import nextstep.signup.ui.component.PasswordTextField
+import nextstep.signup.ui.component.SignUpButton
 import nextstep.signup.ui.component.UsernameTextField
-import nextstep.signup.ui.theme.Blue50
 
 @Composable
 fun SignUpScreen(
@@ -40,18 +35,6 @@ fun SignUpScreen(
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var passwordConfirm by remember { mutableStateOf("") }
-    val enabled by remember(username, email, password, passwordConfirm) {
-        mutableStateOf(
-            SignUpValidator.valid(
-                SignUpParams(
-                    username,
-                    email,
-                    password,
-                    passwordConfirm
-                )
-            )
-        )
-    }
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -100,17 +83,16 @@ fun SignUpScreen(
                 .padding(top = 32.dp),
         )
 
-        Button(
+        SignUpButton(
+            username = username,
+            email = email,
+            password = password,
+            passwordConfirm = passwordConfirm,
             onClick = { onShowSnackbar(context.getString(R.string.success_sign_up)) },
-            colors = ButtonDefaults.buttonColors(containerColor = Blue50),
-            enabled = enabled,
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(top = 32.dp)
-                .height(50.dp),
-        ) {
-            Text(text = stringResource(id = R.string.sign_up))
-        }
+                .padding(top = 32.dp),
+        )
     }
 }
 
