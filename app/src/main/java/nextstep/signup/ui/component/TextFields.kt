9e576d1611abd0d736b1419dbcf31901d98ed02d
1stpie <1stpie@naver.com>
2024-08-22 17:38:05 +0900

    refactor: 화면 및 컴포넌트 수정

--- app/src/main/java/nextstep/signup/ui/component/TextFields.kt
+++ app/src/main/java/nextstep/signup/ui/component/TextFields.kt
@@ -6,10 +6,8 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
@@ -19,6 +17,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import nextstep.signup.ui.theme.Blue50
 import nextstep.signup.ui.theme.ErrorTextColor
+import nextstep.signup.ui.theme.keyboardEmailNextType
+import nextstep.signup.ui.theme.keyboardPasswordNextType
 import nextstep.signup.ui.theme.keyboardTextNextType
 
 data class SignUpTextFieldState(
@@ -32,12 +32,10 @@ data class SignUpTextFieldState(
 @Composable
 @Preview
 fun SignUpTextField(
-    state: SignUpTextFieldState = SignUpTextFieldState(label = "Username", supportingText = "Hello"),
+    fieldState: SignUpTextFieldState = SignUpTextFieldState(label = "Username", supportingText = "Hello"),
     keyboardOptions: KeyboardOptions = keyboardTextNextType,
-    onTextChanged: ((SignUpTextFieldState) -> SignUpTextFieldState)? = null
+    onTextChanged: ((String) -> Unit) = { },
 ) {
-    var fieldState by remember { mutableStateOf(state) }
-
     TextField(
         value = fieldState.text,
         modifier = Modifier.width(296.dp),
@@ -46,10 +44,7 @@ fun SignUpTextField(
             focusedIndicatorColor = fieldState.textFieldColors,
         ),
         keyboardOptions = keyboardOptions,
-        onValueChange = {
-            val new = fieldState.copy(text = it)
-            fieldState = onTextChanged?.invoke(new) ?: new
-        },
+        onValueChange = onTextChanged,
         label = {
             Text(
                 text = fieldState.label,
@@ -62,5 +57,185 @@ fun SignUpTextField(
             )
         },
         visualTransformation = if (keyboardOptions.keyboardType == KeyboardType.Password) PasswordVisualTransformation() else VisualTransformation.None
+    )
+}
+
+@Composable
+@Preview
+fun UsernameTextField(
+    username: MutableState<String> = mutableStateOf("Username"),
+) {
+    SignUpTextField(
+        fieldState = getUsernameTextFieldState(username.value),
+        onTextChanged = username::value::set
+    )
+}
+
+@Composable
+@Preview
+fun EmailTextField(
+    email: MutableState<String> = mutableStateOf("email"),
+) {
+    SignUpTextField(
+        fieldState = getEmailTextFieldState(email.value),
+        keyboardOptions = keyboardEmailNextType,
+        onTextChanged = email::value::set
+    )
+}
+
+@Composable
+@Preview
+fun PasswordTextField(
+    password: MutableState<String> = mutableStateOf("Password"),
+) {
+    SignUpTextField(
+        fieldState = getPasswordTextFieldState(password.value),
+        keyboardOptions = keyboardPasswordNextType,
+        onTextChanged = password::value::set
+    )
+}
+
+@Composable
+@Preview
+fun PasswordConfirmTextField(
+    passwordConfirm: MutableState<String> = mutableStateOf("Password Confirm"),
+    passwordProvider: () -> String = { "" },
+) {
+    SignUpTextField(
+        fieldState = getPasswordConfirmTextFieldState(text = passwordConfirm.value, inputedPassword = passwordProvider()),
+        keyboardOptions = keyboardPasswordNextType,
+        onTextChanged = passwordConfirm::value::set
+    )
+}
+
+const val USERNAME_REGEX = "^[a-zA-Z가-힣]+$"
+const val EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$"
+const val PASSWORD_REGEX = "^(?=.*[a-zA-Z])(?=.*[0-9]).{8,16}$"
+
+const val userNameLengthErrorText = "이름은 2~5자여야 합니다."
+const val userNameNotMatchErrorText = "이름에는 숫자나 기호가 포함될 수 없습니다."
+const val emailFormatNotMatchErrorText = "이메일 형식이 올바르지 않습니다."
+const val passwordFormatNotMatchErrorText = "비밀번호는 영문과 숫자를 포함해야 합니다."
+const val passwordLengthErrorText = "비밀번호는 8~16자여야 합니다."
+const val passwordConfirmNotMatchErrorText = "비밀번호가 일치하지 않습니다."
+
+fun getPasswordConfirmTextFieldState(
+    label: String = "Username",
+    text: String,
+    inputedPassword: String,
+) = when {
+    text.isEmpty() -> SignUpTextFieldState(
+        label = label,
+        text = text,
+        textFieldColors = Blue50,
+        supportingText = ""
+    )
+
+    text != inputedPassword -> SignUpTextFieldState(
+        label = label,
+        text = text,
+        textFieldColors = ErrorTextColor,
+        supportingText = passwordConfirmNotMatchErrorText
+    )
+
+    else -> SignUpTextFieldState(
+        label = label,
+        text = text,
+        textFieldColors = Blue50,
+        supportingText = ""
+    )
+}
+
+
+
+fun getPasswordTextFieldState(
+    text: String,
+    label: String = "Password",
+) = when {
+    text.isEmpty() -> SignUpTextFieldState(
+        label = label,
+        text = text,
+        textFieldColors = Blue50,
+        supportingText = ""
+    )
+
+    text.length !in 8..16 -> SignUpTextFieldState(
+        label = label,
+        text = text,
+        textFieldColors = ErrorTextColor,
+        supportingText = passwordLengthErrorText
+    )
+
+    !text.matches(Regex(PASSWORD_REGEX)) -> SignUpTextFieldState(
+        label = label,
+        text = text,
+        textFieldColors = ErrorTextColor,
+        supportingText = passwordFormatNotMatchErrorText
+    )
+
+    else -> SignUpTextFieldState(
+        label = label,
+        text = text,
+        textFieldColors = Blue50,
+        supportingText = ""
+    )
+}
+
+fun getEmailTextFieldState(
+    text: String,
+    label: String = "Email",
+) = when {
+    text.isEmpty() -> SignUpTextFieldState(
+        label = label,
+        text = text,
+        textFieldColors = Blue50,
+        supportingText = ""
+    )
+
+    !text.matches(Regex(EMAIL_REGEX)) -> SignUpTextFieldState(
+        label = label,
+        text = text,
+        textFieldColors = ErrorTextColor,
+        supportingText = emailFormatNotMatchErrorText
+    )
+
+    else -> SignUpTextFieldState(
+        label = label,
+        text = text,
+        textFieldColors = Blue50,
+        supportingText = ""
+    )
+}
+
+fun getUsernameTextFieldState(
+    text: String,
+    label: String = "Username",
+) = when {
+    text.isEmpty() -> SignUpTextFieldState(
+        label = label,
+        text = text,
+        textFieldColors = Blue50,
+        supportingText = ""
+    )
+
+    text.length !in 2..5 -> SignUpTextFieldState(
+        label = label,
+        text = text,
+        textFieldColors = ErrorTextColor,
+        supportingText = userNameLengthErrorText
+    )
+
+    !text.matches(Regex(USERNAME_REGEX)) -> SignUpTextFieldState(
+        label = label,
+        text = text,
+        textFieldColors = ErrorTextColor,
+        supportingText = userNameNotMatchErrorText
+    )
+
+    else -> SignUpTextFieldState(
+        label = label,
+        text = text,
+        textFieldColors = Blue50,
+        supportingText = ""
     )
 }
--- app/src/main/java/nextstep/signup/ui/screen/SignUpScreen.kt
+++ app/src/main/java/nextstep/signup/ui/screen/SignUpScreen.kt
@@ -7,23 +7,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import nextstep.signup.ui.component.EmailTextField
+import nextstep.signup.ui.component.PasswordConfirmTextField
+import nextstep.signup.ui.component.PasswordTextField
 import nextstep.signup.ui.component.SignUpButton
-import nextstep.signup.ui.component.SignUpTextField
-import nextstep.signup.ui.component.SignUpTextFieldState
-import nextstep.signup.ui.theme.Blue50
-import nextstep.signup.ui.theme.ErrorTextColor
+import nextstep.signup.ui.component.UsernameTextField
 import nextstep.signup.ui.theme.SignupTheme
-import nextstep.signup.ui.theme.keyboardEmailNextType
-import nextstep.signup.ui.theme.keyboardPasswordNextType
-import nextstep.signup.ui.theme.keyboardPasswordSendType
 
 @Preview(showBackground = true)
 @Composable
@@ -32,118 +27,25 @@ fun SignUpScreen() {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth()
         ) {
-            var inputedPassword by remember { mutableStateOf("") }
+            val username = remember { mutableStateOf("") }
+            val email = remember { mutableStateOf("") }
+            val password = remember { mutableStateOf("") }
+            val passwordConfirm = remember { mutableStateOf("") }
 
             Spacer(Modifier.height(112.dp))
             SignUpTitle()
             Spacer(Modifier.height(36.dp))
-            SignUpTextField(SignUpTextFieldState(label = "Username", supportingText = "")) { userName ->
-                updateUserName(userName)
-            }
+            UsernameTextField(username)
             Spacer(Modifier.height(36.dp))
-            SignUpTextField(SignUpTextFieldState(label = "Email", supportingText = ""), keyboardOptions = keyboardEmailNextType) { email ->
-                updateEmail(email)
-            }
+            EmailTextField(email)
             Spacer(Modifier.height(36.dp))
-            SignUpTextField(SignUpTextFieldState(label = "Password", supportingText = ""), keyboardOptions = keyboardPasswordNextType) { password ->
-                updatePassword(password).also {
-                    inputedPassword = it.text
-                }
-            }
+            PasswordTextField(password)
             Spacer(Modifier.height(36.dp))
-            SignUpTextField(SignUpTextFieldState(label = "Password Confirm", supportingText = ""), keyboardOptions = keyboardPasswordSendType) { passwordConfirm ->
-                updatePasswordConfirm(passwordConfirm, inputedPassword)
-            }
+            PasswordConfirmTextField(passwordConfirm, passwordProvider = { password.value })
             Spacer(Modifier.height(36.dp))
             SignUpButton("Sign Up")
         }
     }
-}
-
-private fun updatePasswordConfirm(
-    passwordConfirm: SignUpTextFieldState,
-    inputedPassword: String,
-) = when {
-    passwordConfirm.text.isEmpty() -> passwordConfirm.copy(
-        textFieldColors = Blue50,
-        supportingText = ""
-    )
-
-    passwordConfirm.text != inputedPassword -> passwordConfirm.copy(
-        textFieldColors = ErrorTextColor,
-        supportingText = "비밀번호가 일치하지 않습니다."
-    )
-
-    else -> passwordConfirm.copy(
-        textFieldColors = Blue50,
-        supportingText = ""
-    )
-}
-
-private fun updatePassword(password: SignUpTextFieldState) = when {
-    password.text.isEmpty() -> password.copy(
-        textFieldColors = Blue50,
-        supportingText = ""
-    )
-
-    password.text.length !in 8..16 -> password.copy(
-        textFieldColors = ErrorTextColor,
-        supportingText = "비밀번호는 8~16자여야 합니다."
-    )
-
-    !password.text.matches(Regex(PASSWORD_REGEX)) -> password.copy(
-        textFieldColors = ErrorTextColor,
-        supportingText = "비밀번호는 영문과 숫자를 포함해야 합니다."
-    )
-
-    else -> password.copy(
-        textFieldColors = Blue50,
-        supportingText = ""
-    )
-}
-
-private fun updateEmail(email: SignUpTextFieldState) = when {
-    email.text.isEmpty() -> email.copy(
-        textFieldColors = Blue50,
-        supportingText = ""
-    )
-
-    !email.text.matches(Regex(EMAIL_REGEX)) -> email.copy(
-        textFieldColors = ErrorTextColor,
-        supportingText = "이메일 형식이 올바르지 않습니다."
-    )
-
-    else -> email.copy(
-        textFieldColors = Blue50,
-        supportingText = ""
-    )
-}
-
-const val USERNAME_REGEX = "^[a-zA-Z가-힣]+$"
-const val EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$"
-const val PASSWORD_REGEX = "^(?=.*[a-zA-Z])(?=.*[0-9]).{8,16}$"
-
-
-private fun updateUserName(state: SignUpTextFieldState) = when {
-    state.text.isEmpty() -> state.copy(
-        textFieldColors = Blue50,
-        supportingText = ""
-    )
-
-    state.text.length !in 2..5 -> state.copy(
-        textFieldColors = ErrorTextColor,
-        supportingText = "이름은 2~5자여야 합니다."
-    )
-
-    !state.text.matches(Regex(USERNAME_REGEX)) -> state.copy(
-        textFieldColors = ErrorTextColor,
-        supportingText = "이름에는 숫자나 기호가 포함될 수 없습니다."
-    )
-
-    else -> state.copy(
-        textFieldColors = Blue50,
-        supportingText = ""
-    )
 }
 
 @Composable
