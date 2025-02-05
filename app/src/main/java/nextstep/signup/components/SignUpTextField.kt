7738ddb109a47b42c7e8b43785f498639915b5c1
Bandal <no1msh1217@gmail.com>
2025-02-06 01:01:14 +0900

    feat: 가입화면 텍스트 필드에 키보드 타입을 지정할 수 있게 변경

--- app/src/main/java/nextstep/signup/components/SignUpTextField.kt
+++ app/src/main/java/nextstep/signup/components/SignUpTextField.kt
@@ -1,5 +1,7 @@
 package nextstep.signup.components
 
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -9,6 +11,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,12 +24,14 @@ internal fun SignUpTextField(
     onValueChange: (String) -> Unit,
     label: String,
     modifier: Modifier = Modifier,
+    keyboardType: KeyboardType = KeyboardType.Text,
     needHide: Boolean = false,
 ) {
     TextField(
         value = value,
         onValueChange = onValueChange,
         label = { Text(text = label) },
+        keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
         visualTransformation = if (needHide) PasswordVisualTransformation() else VisualTransformation.None,
         modifier = modifier,
         colors = TextFieldDefaults.colors(
