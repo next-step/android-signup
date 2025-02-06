502ce98e93b5333eb87db8e87df6ae24cf4a21b8
parkduksung <duksung1234@naver.com>
2025-02-06 20:01:23 +0900

    feat : 제목 컴포넌트 추가

---
+++ app/src/main/java/nextstep/signup/register/component/SignUpTitle.kt
@@ -0,0 +1,34 @@
+package nextstep.signup.register.component
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import nextstep.signup.R
+
+
+@Composable
+fun SignUpTitle(
+    modifier: Modifier = Modifier,
+    title : String = stringResource(R.string.sign_up_title)
+) {
+
+    Text(
+        modifier = modifier,
+        text = title,
+        fontSize = 26.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color.Black
+    )
+}
+
+
+@Preview
+@Composable
+private fun PreviewSignUpTitle() {
+    SignUpTitle()
+}
