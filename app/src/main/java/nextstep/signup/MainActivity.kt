8fe7e4df1b09635651179e60c646e40a405cbbd1
ver <ver@sendy.ai>
2025-02-07 08:58:14 +0900

    feat: 앱을 진입하면 회원가입 화면을 띄운다

--- app/src/main/java/nextstep/signup/MainActivity.kt
+++ app/src/main/java/nextstep/signup/MainActivity.kt
@@ -3,14 +3,17 @@ package nextstep.signup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import nextstep.signup.ui.theme.SignupTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SignupTheme {
-
+            MaterialTheme {
+                Scaffold { innerPadding ->
+                    SignUpScreen(innerPadding)
+                }
             }
         }
     }
