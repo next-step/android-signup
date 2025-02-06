270b8b8a416c7aee4a6f7de69a7593752071dc1a
Bandal <no1msh1217@gmail.com>
2025-02-06 23:23:59 +0900

    test: SignUpTextField에 값이 입력될 때 라벨과 함께 표시된다

--- app/src/androidTest/java/nextstep/signup/component/SignUpTextFieldTest.kt
+++ app/src/androidTest/java/nextstep/signup/component/SignUpTextFieldTest.kt
@@ -2,7 +2,12 @@ package nextstep.signup.component
 
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.assert
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import nextstep.signup.components.SignUpTextField
 import org.junit.Before
@@ -21,6 +26,7 @@ class SignUpTextFieldTest {
                 value = text.value,
                 onValueChange = { text.value = it },
                 label = "username",
+                modifier = Modifier.testTag(TAG_SIGN_UP_TEXT_FIELD)
             )
         }
     }
@@ -33,19 +39,22 @@ class SignUpTextFieldTest {
 
         // then
         composeTestRule
-            .onNodeWithText("username")
-            .assertExists()
+            .onNodeWithTag(TAG_SIGN_UP_TEXT_FIELD)
+            .assert(hasText("username"))
     }
 
     @Test
-    fun 입력_되었을_때도_계속_라벨이_표시된다() {
+    fun 입력_되었을_때도_입력된_값과_라벨이_표시된다() {
         // when
         text.value = "김컴포즈"
 
         // then
         composeTestRule
-            .onNodeWithText("username")
-            .assertExists()
+            .onNodeWithTag(TAG_SIGN_UP_TEXT_FIELD)
+            .assert(hasText("김컴포즈") and hasText("username"))
     }
 
+    companion object {
+        private const val TAG_SIGN_UP_TEXT_FIELD = "SignUpTextField"
+    }
 }
