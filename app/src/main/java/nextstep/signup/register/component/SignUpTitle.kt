6810b325fe914e1a47ae690c877d78db33004e0d
parkduksung <duksung1234@naver.com>
2025-02-06 20:18:19 +0900

    feat : SignUpTitle textAlign Center 적용.

--- app/src/main/java/nextstep/signup/register/component/SignUpTitle.kt
+++ app/src/main/java/nextstep/signup/register/component/SignUpTitle.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import nextstep.signup.R
@@ -22,6 +23,7 @@ fun SignUpTitle(
         text = title,
         fontSize = 26.sp,
         fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center,
         color = Color.Black
     )
 }
