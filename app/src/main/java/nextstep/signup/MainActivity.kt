3df4b0f11b11229d853086bc3ef2282545899d7a
raindragonn <raindragonn@gmail.com>
2025-02-06 22:47:31 +0900

    feat(component): SignupTextField 컴포넌트 추가

--- app/src/main/java/nextstep/signup/MainActivity.kt
+++ app/src/main/java/nextstep/signup/MainActivity.kt
@@ -5,18 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import nextstep.signup.component.SignupTextField
 import nextstep.signup.component.TitleText
 import nextstep.signup.ui.theme.SignupTheme
 
@@ -39,15 +40,45 @@ class MainActivity : ComponentActivity() {
 internal fun SignupScreen(
     modifier: Modifier = Modifier,
 ) {
+    val (userName, setUserName) = remember { mutableStateOf("") }
+    val (email, setEmail) = remember { mutableStateOf("") }
+    val (password, setPassword) = remember { mutableStateOf("") }
+    val (passwordConfirm, setPasswordConfirm) = remember { mutableStateOf("") }
+
     Column(
         modifier = modifier
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .padding(top = 40.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(36.dp),
     ) {
-        Spacer(modifier = Modifier.height(40.dp))
         TitleText(
-            text = stringResource(R.string.signup_title),
+            modifier = Modifier.padding(top = 6.dp),
+            text = stringResource(R.string.signup_title)
+        )
+        SignupTextField(
+            label = stringResource(R.string.signup_label_user_name),
+            text = userName,
+            onValueChange = setUserName,
+            isPassword = false
+        )
+        SignupTextField(
+            label = stringResource(R.string.signup_label_email),
+            text = email,
+            onValueChange = setEmail,
+            isPassword = false
+        )
+        SignupTextField(
+            label = stringResource(R.string.signup_label_password),
+            text = password,
+            onValueChange = setPassword,
+            isPassword = true
+        )
+        SignupTextField(
+            label = stringResource(R.string.signup_label_password_confirm),
+            text = passwordConfirm,
+            onValueChange = setPasswordConfirm,
+            isPassword = true
         )
     }
 }
