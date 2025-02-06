ed5209d9bd926d46c2c29eea7014ec8981c8e3d7
seungohjin <corrobolea@gmail.com>
2025-02-06 18:44:20 +0900

    feat: 가입화면 InputField 컴포넌트 구현

--- app/src/main/java/nextstep/signup/ui/screen/SignUpScreen.kt
+++ app/src/main/java/nextstep/signup/ui/screen/SignUpScreen.kt
@@ -4,17 +4,31 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import nextstep.signup.ui.theme.Blue50
+import nextstep.signup.ui.theme.BlueGrey50
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 
 @Composable
@@ -22,6 +36,11 @@ fun SignUpScreen(modifier: Modifier) {
     Surface(
         modifier = modifier.background(Color.White),
     ) {
+        var userName by remember { mutableStateOf("") }
+        var email by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+        var confirmPassword by remember { mutableStateOf("") }
+
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -30,6 +49,32 @@ fun SignUpScreen(modifier: Modifier) {
         ) {
             Spacer(modifier = Modifier.padding(top = 60.dp))
             TitleComponent("Welcome to Compose \uD83D\uDE80")
+            Spacer(modifier = Modifier.padding(top = 42.dp))
+            InputField(
+                label = "Username",
+                value = userName,
+                onValueChange = { userName = it },
+            )
+            Spacer(modifier = Modifier.padding(top = 36.dp))
+            InputField(
+                label = "Email",
+                value = email,
+                onValueChange = { email = it },
+            )
+            Spacer(modifier = Modifier.padding(top = 36.dp))
+            InputField(
+                label = "Password",
+                value = password,
+                inputType = KeyboardType.Password,
+                onValueChange = { password = it },
+            )
+            Spacer(modifier = Modifier.padding(top = 36.dp))
+            InputField(
+                label = "Password Confirm",
+                value = confirmPassword,
+                inputType = KeyboardType.Password,
+                onValueChange = { confirmPassword = it },
+            )
         }
     }
 }
@@ -43,8 +88,48 @@ fun TitleComponent(title: String) {
     )
 }
 
+@Composable
+fun InputField(
+    label: String,
+    value: String,
+    inputType: KeyboardType = KeyboardType.Text,
+    onValueChange: (String) -> Unit,
+) {
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        keyboardOptions = KeyboardOptions.Default.copy(
+            imeAction = ImeAction.Done,
+            keyboardType = KeyboardType.Password
+        ),
+        visualTransformation = if (inputType == KeyboardType.Password)
+            PasswordVisualTransformation()
+        else
+            VisualTransformation.None,
+        label = { Text(label) },
+        maxLines = 1,
+        colors = TextFieldDefaults.colors(
+            focusedIndicatorColor = Blue50,
+            unfocusedContainerColor = BlueGrey50,
+            focusedContainerColor = BlueGrey50,
+            focusedLabelColor = Blue50,
+        ),
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 private fun TitlePreView() {
     TitleComponent("Welcome to Compose \uD83D\uDE80")
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun InputFieldPreView() {
+    InputField(
+        label = "Username",
+        value = "홀리몰리",
+        onValueChange = { }
+    )
 }
