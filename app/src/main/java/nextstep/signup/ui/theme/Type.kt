347a22999aa32be310aa24c7df705cbdf5256ecd
ver <ver@sendy.ai>
2025-02-07 08:16:44 +0900

    chore: Roboto 폰트 추가

--- app/src/main/java/nextstep/signup/ui/theme/Type.kt
+++ app/src/main/java/nextstep/signup/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package nextstep.signup.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import nextstep.signup.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -31,4 +33,11 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+val roboto = FontFamily(
+    Font(R.font.roboto_light, FontWeight.Light),
+    Font(R.font.roboto_regular, FontWeight.Normal),
+    Font(R.font.roboto_medium, FontWeight.Medium),
+    Font(R.font.roboto_bold, FontWeight.Bold)
 )
