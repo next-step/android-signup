6e4eb0234ab9ebb108991340944be4b5e355127b
ver <ver@sendy.ai>
2025-02-07 08:27:03 +0900

    feat: 회원가입 화면 배경 및 환영 문구 구현

--- app/src/main/java/nextstep/signup/SignUpScreen.kt
+++ app/src/main/java/nextstep/signup/SignUpScreen.kt
@@ -1,13 +1,21 @@
 package nextstep.signup
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -26,7 +34,7 @@ fun SignUpScreen(
             .padding(paddingValues),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Spacer(modifier = Modifier.padding(36.dp))
+        Spacer(modifier = Modifier.height(60.dp))
 
         Text(
             text = "Welcome to Compose \uD83D\uDE80",
@@ -35,10 +43,68 @@ fun SignUpScreen(
             fontWeight = FontWeight.Bold
         )
 
-        Spacer(modifier = Modifier.padding(32.dp))
+        Spacer(modifier = Modifier.height(40.dp))
 
-
+        Column(
+            verticalArrangement = Arrangement.spacedBy(36.dp),
+            modifier = Modifier.padding(horizontal = 32.dp)
+        ) {
+            UserNameInputForm()
+            EmailInputForm()
+            PasswordInputForm()
+            PasswordConfirmInputForm()
+        }
     }
+}
+
+
+@Composable
+private fun UserNameInputForm(modifier: Modifier = Modifier) {
+    var userName by remember { mutableStateOf("") }
+
+    TextField(
+        value = userName,
+        onValueChange = { userName = it },
+        label = { Text("Username") },
+        modifier = modifier.fillMaxWidth()
+    )
+}
+
+@Composable
+private fun EmailInputForm(modifier: Modifier = Modifier) {
+    var email by remember { mutableStateOf("") }
+
+    TextField(
+        value = email,
+        onValueChange = { email = it },
+        label = { Text("Email") },
+        modifier = modifier.fillMaxWidth()
+    )
+}
+
+@Composable
+private fun PasswordInputForm(modifier: Modifier = Modifier) {
+    var password by remember { mutableStateOf("") }
+
+    TextField(
+        value = password,
+        onValueChange = { password = it },
+        label = { Text("Password") },
+        modifier = modifier.fillMaxWidth()
+    )
+}
+
+
+@Composable
+private fun PasswordConfirmInputForm(modifier: Modifier = Modifier) {
+    var passwordConfirm by remember { mutableStateOf("") }
+
+    TextField(
+        value = passwordConfirm,
+        onValueChange = { passwordConfirm = it },
+        label = { Text("Password Confirm") },
+        modifier = modifier.fillMaxWidth()
+    )
 }
 
 @Preview(showBackground = true)
