ad60ff9b37b598f288a2c2678554576d63f61402
YoonJam <yhw1904@gmail.com>
2025-02-07 10:57:15 +0900

    feat: 타이틀 컴포저블 추가

--- app/src/main/java/nextstep/signup/MainActivity.kt
+++ app/src/main/java/nextstep/signup/MainActivity.kt
@@ -3,20 +3,12 @@ package nextstep.signup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import nextstep.signup.ui.theme.SignupTheme
 
@@ -33,29 +25,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun HelloView(
-    name: String,
-    modifier: Modifier = Modifier
-) {
-   Text(
-       text = "$name 안녕하세요",
-       fontSize = 20.sp,
-       color = Color.White,
-       modifier = modifier
-           .background(
-               brush = Brush.linearGradient(
-                   colors = listOf(Color.Blue, Color.Green),
-                     start = Offset.Zero,
-                     end = Offset.Infinite
-               ),
-               shape = RoundedCornerShape(16.dp)
-           )
-           .padding(16.dp)
-   )
+fun SignUpTitle(text: String, modifier: Modifier = Modifier) {
+    Text(
+        text = text,
+        fontSize = 26.sp,
+        fontWeight = FontWeight.W700,
+        color = Color.Black,
+        lineHeight = 20.sp,
+        modifier = modifier,
+    )
 }
 
-@Preview
+
+@Preview(showBackground = true)
 @Composable
-private fun HelloViewPreview() {
-    HelloView(name = "홍길동")
+private fun TitleComposablePreview() {
+    SignUpTitle("Welcome to Compose \uD83D\uDE80")
 }
