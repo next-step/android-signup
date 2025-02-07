0968df6d541f8411f7a1b51ea3370d392c8b7f29
YoonJam <yhw1904@gmail.com>
2025-02-07 22:49:46 +0900

    🚀 2단계 - 회원가입(뷰) (#115)
    * docs: ReadMe 수정
    
    * feat: 타이틀 컴포저블 추가
    
    * feat: 텍스트필드 컴포저블 추가
    
    * feat: 버튼 컴포저블 추가
    
    * feat: 정의된 컴포저블들로 화면 구성하기
    
    * feat: color, string 리소스 추가
    
    * feat: string, color 리소스 적용
    
    * feat: label 텍스트 스타일 추가
    
    * feat: 테스트 코드 추가

--- app/src/main/java/nextstep/signup/MainActivity.kt
+++ app/src/main/java/nextstep/signup/MainActivity.kt
@@ -3,21 +3,38 @@ package nextstep.signup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import nextstep.signup.ui.theme.Blue50
 import nextstep.signup.ui.theme.SignupTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,37 +42,147 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SignupTheme {
-                // A surface container using the 'background' color from the theme
-
+                SignUpScreen()
             }
         }
     }
 }
 
 @Composable
-fun HelloView(
-    name: String,
-    modifier: Modifier = Modifier
-) {
-   Text(
-       text = "$name 안녕하세요",
-       fontSize = 20.sp,
-       color = Color.White,
-       modifier = modifier
-           .background(
-               brush = Brush.linearGradient(
-                   colors = listOf(Color.Blue, Color.Green),
-                     start = Offset.Zero,
-                     end = Offset.Infinite
-               ),
-               shape = RoundedCornerShape(16.dp)
-           )
-           .padding(16.dp)
-   )
+fun SignUpScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        SignUpTitle(Modifier.padding(top = 60.dp))
+        SignUpTextField(SignUpTextFieldType.USERNAME, Modifier.padding(top = 42.dp))
+        SignUpTextField(SignUpTextFieldType.EMAIL, Modifier.padding(top = 33.dp))
+        SignUpTextField(SignUpTextFieldType.PASSWORD, Modifier.padding(top = 33.dp))
+        SignUpTextField(SignUpTextFieldType.PASSWORD_CONFIRM, Modifier.padding(top = 33.dp))
+        SignUpButton(Modifier.padding(top = 39.dp))
+    }
 }
 
-@Preview
 @Composable
-private fun HelloViewPreview() {
-    HelloView(name = "홍길동")
+fun SignUpTitle(modifier: Modifier = Modifier) {
+    Text(
+        text = stringResource(R.string.welcome_to_compose),
+        fontSize = 26.sp,
+        fontWeight = FontWeight.W700,
+        color = Color.Black,
+        lineHeight = 20.sp,
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun SignUpTextField(
+    type: SignUpTextFieldType,
+    modifier: Modifier = Modifier
+) {
+    var input by remember { mutableStateOf("") }
+
+    TextField(
+        value = input,
+        onValueChange = {
+            input = it
+        },
+        label = {
+            Text(
+                text = type.hint,
+                fontSize = 16.sp,
+                lineHeight = 24.sp,
+                fontWeight = FontWeight.W400,
+                letterSpacing = 0.5.sp
+            )
+        },
+        singleLine = true,
+        colors = TextFieldDefaults.colors(
+            focusedTextColor = Color(0xFF1D1B20),
+            unfocusedTextColor = Color(0xFF1D1B20),
+            focusedLabelColor = Blue50,
+            unfocusedLabelColor = Color(0xFF49454F),
+            focusedIndicatorColor = Blue50,
+            unfocusedIndicatorColor = Color(0xFF49454F),
+            cursorColor = Blue50,
+            focusedContainerColor = Color(0xFFE3E8F1),
+            unfocusedContainerColor = Color(0xFFE3E8F1),
+        ),
+        visualTransformation = when (type) {
+            SignUpTextFieldType.USERNAME, SignUpTextFieldType.EMAIL -> VisualTransformation.None
+            SignUpTextFieldType.PASSWORD, SignUpTextFieldType.PASSWORD_CONFIRM -> PasswordVisualTransformation()
+        },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = when (type) {
+                SignUpTextFieldType.USERNAME -> KeyboardType.Text
+                SignUpTextFieldType.EMAIL -> KeyboardType.Email
+                SignUpTextFieldType.PASSWORD, SignUpTextFieldType.PASSWORD_CONFIRM -> KeyboardType.Password
+            }
+        ),
+        modifier = modifier
+            .width(296.dp)
+            .height(56.dp)
+            .clip(shape = RoundedCornerShape(topStart = 4.dp, topEnd = 4.dp))
+    )
+}
+
+@Composable
+fun SignUpButton(modifier: Modifier = Modifier) {
+    Button(
+        onClick = { },
+        modifier = modifier
+            .width(296.dp)
+            .height(50.dp),
+        shape = RoundedCornerShape(100.dp),
+        contentPadding = PaddingValues(0.dp),
+        colors = ButtonColors(
+            containerColor = Color(0xFF2196F3),
+            contentColor = Color.White,
+            disabledContainerColor = Color(0x1D1B201F),
+            disabledContentColor = Color(0x1D1B201F),
+        ),
+        content = {
+            Text(
+                text = stringResource(R.string.sign_up),
+                fontSize = 14.sp,
+                fontWeight = FontWeight.W500,
+                lineHeight = 20.sp,
+                letterSpacing = 0.1.sp
+            )
+        }
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun SignUpTitlePreview() {
+    SignUpTitle()
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SignUpTextFieldPreview() {
+    SignUpTextField(SignUpTextFieldType.USERNAME)
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SignUpButtonPreview() {
+    SignUpButton()
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SignUpScreenPreview() {
+    SignUpScreen()
+}
+
+enum class SignUpTextFieldType(val hint: String) {
+    USERNAME("Username"),
+    EMAIL("Email"),
+    PASSWORD("Password"),
+    PASSWORD_CONFIRM("Password Confirm")
 }
