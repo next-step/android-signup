9662aa860fe1a41b51d29b1224965a7e2bbc754d
DoTheBestMayB <send1684@gmail.com>
2025-02-07 17:53:11 +0900

    feat: Welcome Text 추가

--- app/src/main/java/nextstep/signup/ui/signup/SignUpScreen.kt
+++ app/src/main/java/nextstep/signup/ui/signup/SignUpScreen.kt
@@ -20,9 +20,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import nextstep.signup.R
 import nextstep.signup.ui.theme.SignupTheme
 
 @Composable
@@ -91,6 +94,12 @@ fun SignUpScreen(
             verticalArrangement = Arrangement.spacedBy(36.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
+            Text(
+                modifier = Modifier.padding(top = 48.dp),
+                text = stringResource(R.string.sign_up_welcome),
+                fontWeight = FontWeight.Bold,
+                fontSize = 26.sp,
+            )
             for (inputState in state.getInputStateList()) {
                 TextField(
                     modifier = Modifier.fillMaxWidth(),
