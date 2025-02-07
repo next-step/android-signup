f655157bc7f0d9e2d45a579396c4f342b89e60a6
minoo <minwoo@dailyshot.co>
2025-02-07 19:42:37 +0900

    refactor: Main 화면에 미사용 코드 제거

--- app/src/main/java/nextstep/signup/MainActivity.kt
+++ app/src/main/java/nextstep/signup/MainActivity.kt
@@ -3,18 +3,6 @@ package nextstep.signup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import nextstep.signup.ui.SignupScreen
 import nextstep.signup.ui.theme.SignupTheme
 
@@ -27,32 +15,4 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
-
-@Composable
-fun HelloView(
-    name: String,
-    modifier: Modifier = Modifier
-) {
-    Text(
-        text = "$name 안녕하세요!",
-        fontSize = 20.sp,
-        color = Color.White,
-        modifier = modifier
-            .background(
-                brush = Brush.linearGradient(
-                    colors = listOf(Color.Blue, Color.Green),
-                    start = Offset.Zero,
-                    end = Offset.Infinite
-                ),
-                shape = RoundedCornerShape(16.dp)
-            )
-            .padding(16.dp)
-    )
-}
-
-@Preview
-@Composable
-fun HelloViewPreview() {
-    HelloView(name = "여러분")
 }
