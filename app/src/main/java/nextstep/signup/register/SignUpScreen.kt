96c5414f12f0849d42b3fc728a1e6d2ada391e09
parkduksung <duksung1234@naver.com>
2025-02-08 13:02:27 +0900

    chore : 불필요한 부분 제거

--- app/src/main/java/nextstep/signup/register/SignUpScreen.kt
+++ app/src/main/java/nextstep/signup/register/SignUpScreen.kt
@@ -1,9 +1,7 @@
 package nextstep.signup.register
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
