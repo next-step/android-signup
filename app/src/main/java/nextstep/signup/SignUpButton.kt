6536b0f7f7ebd3000eb0b3c389ad4ed0a1779398
yangsooplus <yangsooplus@naver.com>
2025-02-08 19:09:07 +0900

    feat: Button 컴포넌트 작성

---
+++ app/src/main/java/nextstep/signup/SignUpButton.kt
@@ -0,0 +1,45 @@
+package nextstep.signup
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import nextstep.signup.ui.theme.Blue50
+import nextstep.signup.ui.theme.SignupTheme
+
+@Composable
+fun SignUpButton(
+    text: String,
+    textStyle: TextStyle = MaterialTheme.typography.labelLarge,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    Button(
+        modifier = modifier.fillMaxWidth().height(50.dp),
+        onClick = onClick,
+        content = {
+            Text(text = text, style = textStyle)
+        },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Blue50
+        )
+    )
+}
+
+@Preview
+@Composable
+private fun SignUpButtonPreview() {
+    SignupTheme {
+        SignUpButton(
+            text = "Sign Up",
+            onClick = {}
+        )
+    }
+}
