820aecf5c28f4dad84fa5f710ffab40165e960f1
sana-20 <sana20.dev@gmail.com>
2025-02-08 14:22:57 +0900

    feat: 제목 Text UI 구현

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ### step2 기능 목록
 
-- [ ]  제목 Text 구현
+- [x]  제목 Text 구현
 - [ ]  공통 InputTextField 구현
 - [ ]  SignUp Button 구현
 - [ ]  UI 컴포넌트 조합
---
+++ app/src/main/java/nextstep/signup/view/SignUpTitle.kt
@@ -0,0 +1,30 @@
+package nextstep.signup.view
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SignUpTitle(
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        modifier = modifier,
+        text = text,
+        fontWeight = FontWeight.Bold,
+        lineHeight = 20.sp,
+        fontSize = 26.sp,
+        color = Color.Black
+    )
+}
+
+@Preview
+@Composable
+fun SignUpTitle_Preview() {
+    SignUpTitle("Welcome to Compose 🚀")
+}
