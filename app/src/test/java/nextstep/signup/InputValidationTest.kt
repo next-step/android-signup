680e2df86e1972aeaaf89a8d7c6677e3b92e2a16
parkduksung <duksung1234@naver.com>
2025-02-10 05:52:29 +0900

    refactor : 검증 함수를 호출하여 테스트 작성 개선

--- app/src/test/java/nextstep/signup/InputValidationTest.kt
+++ app/src/test/java/nextstep/signup/InputValidationTest.kt
@@ -1,7 +1,6 @@
 package nextstep.signup
 
 import nextstep.signup.register.SignUpValidation
-import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
@@ -10,50 +9,40 @@ import org.junit.Test
 class InputValidationTest {
 
     @Test
-    fun should_length_2_5_when_enter_username() {
-        assertFalse("a".matches(Regex(SignUpValidation.USER_LENGTH_REGEX)))
-        assertTrue("aa".matches(Regex(SignUpValidation.USER_LENGTH_REGEX)))
-        assertTrue("aaa".matches(Regex(SignUpValidation.USER_LENGTH_REGEX)))
-        assertTrue("aaaa".matches(Regex(SignUpValidation.USER_LENGTH_REGEX)))
-        assertTrue("aaaaa".matches(Regex(SignUpValidation.USER_LENGTH_REGEX)))
-        assertFalse("aaaaaa".matches(Regex(SignUpValidation.USER_LENGTH_REGEX)))
+    fun should_be_ture_when_username_length_2_5_and_not_contain_digit_or_symbol(){
+        assertFalse(SignUpValidation.isValidUserName("z"))
+        assertTrue(SignUpValidation.isValidUserName("aa"))
+        assertFalse(SignUpValidation.isValidUserName("!!"))
+        assertFalse(SignUpValidation.isValidUserName("a2"))
+        assertFalse(SignUpValidation.isValidUserName("a%"))
+        assertTrue(SignUpValidation.isValidUserName("cde"))
+        assertTrue(SignUpValidation.isValidUserName("fghi"))
+        assertTrue(SignUpValidation.isValidUserName("jklmn"))
+        assertFalse(SignUpValidation.isValidUserName("jk!mn"))
+        assertFalse(SignUpValidation.isValidUserName("qwerqw"))
     }
 
     @Test
-    fun should_not_contain_digit_or_symbol_when_enter_username() {
-        assertFalse("1".matches(Regex(SignUpValidation.USERNAME_REGEX)))
-        assertFalse("!".matches(Regex(SignUpValidation.USERNAME_REGEX)))
-        assertTrue("a".matches(Regex(SignUpValidation.USERNAME_REGEX)))
-        assertTrue("aa".matches(Regex(SignUpValidation.USERNAME_REGEX)))
-        assertTrue("aaa".matches(Regex(SignUpValidation.USERNAME_REGEX)))
-        assertFalse("aaa1".matches(Regex(SignUpValidation.USERNAME_REGEX)))
-        assertFalse("aaaaaaa2".matches(Regex(SignUpValidation.USERNAME_REGEX)))
+    fun should_be_ture_when_email_format(){
+        assertFalse(SignUpValidation.isValidEmail("abew1@"))
+        assertFalse(SignUpValidation.isValidEmail("abew1@naver."))
+        assertTrue(SignUpValidation.isValidEmail("abew1@naver.com"))
+        assertFalse(SignUpValidation.isValidEmail("abew1@naver.c"))
     }
 
     @Test
-    fun should_be_correct_email_format_when_enter_email() {
-        assertTrue("a@a.com".matches(Regex(SignUpValidation.EMAIL_REGEX)))
-        assertTrue("a@a.co.kr".matches(Regex(SignUpValidation.EMAIL_REGEX)))
+    fun should_be_ture_when_password_length_8_16_and_contain_english_and_digit(){
+        assertFalse(SignUpValidation.isValidPassword("1234abc"))
+        assertFalse(SignUpValidation.isValidPassword("1234567891234abcd"))
+        assertFalse(SignUpValidation.isValidPassword("asdf123"))
+        assertFalse(SignUpValidation.isValidPassword("asdfqwerasdfq1234"))
+        assertTrue(SignUpValidation.isValidPassword("2345asdf"))
+        assertTrue(SignUpValidation.isValidPassword("234567891234abcd"))
     }
 
     @Test
-    fun should_length_8_16_when_enter_password() {
-        assertFalse("1234567".matches(Regex(SignUpValidation.PASSWORD_LENGTH_REGEX)))
-        assertTrue("123456789".matches(Regex(SignUpValidation.PASSWORD_LENGTH_REGEX)))
+    fun should_be_ture_when_password_confirm_equals_password() {
+        assertFalse(SignUpValidation.isValidPasswordConfirm("asdf1234","asdf123"))
+        assertTrue(SignUpValidation.isValidPasswordConfirm("asdf1234","asdf1234"))
     }
-
-    @Test
-    fun should_contain_english_or_digit_when_enter_password() {
-        assertFalse("".matches(Regex(SignUpValidation.PASSWORD_REGEX)))
-        assertFalse("a".matches(Regex(SignUpValidation.PASSWORD_REGEX)))
-        assertFalse("1".matches(Regex(SignUpValidation.PASSWORD_REGEX)))
-        assertTrue("a1".matches(Regex(SignUpValidation.PASSWORD_REGEX)))
-    }
-
-    @Test
-    fun should_equal_password_when_enter_password_confirm() {
-        val mockPassword = "qwer1234"
-        assertEquals("qwer1234", mockPassword)
-    }
-
 }
