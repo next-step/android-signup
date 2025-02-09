089eca27cbfc8231d3e3b8998d153c8be8026c70
parkduksung <duksung1234@naver.com>
2025-02-10 07:00:37 +0900

    feat : SignUpScreen 검증 관련 추가 및 완료버튼 클릭시 스낵바 추가

--- app/src/main/java/nextstep/signup/register/SignUpScreen.kt
+++ app/src/main/java/nextstep/signup/register/SignUpScreen.kt
@@ -5,11 +5,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+import nextstep.signup.R
 import nextstep.signup.register.component.SignUpButton
 import nextstep.signup.register.component.SignUpTextFiled
 import nextstep.signup.register.component.SignUpTitle
@@ -17,19 +28,38 @@ import nextstep.signup.register.component.SignUpTitle
 
 @Composable
 fun SignUpScreen() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = 16.dp, vertical = 32.dp)
-            .verticalScroll(rememberScrollState()),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        SignUpTitle(modifier = Modifier.padding(bottom = 40.dp))
-        SignUpTextFiled()
-        SignUpButton(
-            modifier = Modifier.padding(top = 40.dp),
-            onSignUp = {}
-        )
+
+    var isEnableSignUp by remember { mutableStateOf(false) }
+
+    val context = LocalContext.current
+
+    val snackBarHostState = remember { SnackbarHostState() }
+
+    val scope = rememberCoroutineScope()
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        snackbarHost = { SnackbarHost(snackBarHostState) }
+    ) { _ ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp, vertical = 32.dp)
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            SignUpTitle(modifier = Modifier.padding(bottom = 40.dp))
+            SignUpTextFiled(onChangeValid = { isEnableSignUp = it })
+            SignUpButton(
+                modifier = Modifier.padding(top = 40.dp),
+                isEnabled = isEnableSignUp,
+                onSignUp = {
+                    scope.launch {
+                        snackBarHostState.showSnackbar(context.getString(R.string.sign_up_snack_bar))
+                    }
+                }
+            )
+        }
     }
 }
 
