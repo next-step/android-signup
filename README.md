59e5cd68a194cf426a0badb6fe721a5f14942a13
hyemdooly <hyemdooly@gmail.com>
2025-02-10 23:18:05 +0900

    feat: BlueGrey20 컬러 정의

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ## step2
 - [x] Blue50(#2196F3) 색상 정의
+- [x] BlueGrey20(#E3E8F1) 색상 정의
 - [ ] TextField 컴포넌트 구현
 - [ ] Button 컴포넌트 구현
 - [ ] 구현한 컴포넌트를 사용하여 화면 구현
--- app/src/main/java/nextstep/signup/ui/theme/Color.kt
+++ app/src/main/java/nextstep/signup/ui/theme/Color.kt
@@ -3,6 +3,7 @@ package nextstep.signup.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Blue50 = Color(0xFF2196F3)
+val BlueGrey20 = Color(0xFFE3E8F1)
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
