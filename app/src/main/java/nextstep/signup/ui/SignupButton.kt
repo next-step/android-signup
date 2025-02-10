85332e628c5f32f28d3b64d7a2f14cef9474f3db
minoo <minwoo@dailyshot.co>
2025-02-10 13:34:26 +0900

    style: SignupButton, SignupTextField 텍스트 스타일을 MaterialTheme.typography 로 변경

--- app/src/main/java/nextstep/signup/ui/SignupButton.kt
+++ app/src/main/java/nextstep/signup/ui/SignupButton.kt
@@ -5,13 +5,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import nextstep.signup.ui.theme.Blue50
 import nextstep.signup.ui.theme.White
 
@@ -37,8 +36,7 @@ fun SignupButton(
     ) {
         Text(
             text = text,
-            fontSize = 14.sp,
-            fontWeight = FontWeight.W500,
+            style = MaterialTheme.typography.labelLarge,
         )
     }
 }
--- app/src/main/java/nextstep/signup/ui/SignupTextField.kt
+++ app/src/main/java/nextstep/signup/ui/SignupTextField.kt
@@ -4,17 +4,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import nextstep.signup.ui.theme.BlackDark
 import nextstep.signup.ui.theme.BlackGrey
 import nextstep.signup.ui.theme.Blue50
@@ -35,13 +33,12 @@ fun SignupTextField(
         value = value,
         onValueChange = onValueChange,
         modifier = modifier.fillMaxWidth(),
-        textStyle = TextStyle(
-            color = BlackDark,
-            fontSize = 16.sp,
-            fontWeight = FontWeight.W400
-        ),
+        textStyle = MaterialTheme.typography.bodyLarge,
         label = {
-            Text(text = label)
+            Text(
+                text = label,
+                style = MaterialTheme.typography.bodySmall
+            )
         },
         singleLine = true,
         visualTransformation = visualTransformation,
