859bb927308aeddf7e7a174f19858b8b389cad57
fbghgus123 <fbghgus123@naver.com>
2025-02-10 19:40:38 +0900

    refactor(userRegisterTest): Regex 변수로 저장
    기존 함수 호출마다 생성하던 Regex를 미리 만들어 두는 것으로 변경

--- app/src/main/java/nextstep/signup/util/RegexUtil.kt
+++ app/src/main/java/nextstep/signup/util/RegexUtil.kt
@@ -2,11 +2,11 @@ package nextstep.signup.util
 
 object RegexUtil {
 
-    private const val WITHOUT_SPECIAL_CHARS_REGEX = "^[a-zA-Z가-힣0-9]+$"
-    private const val EMAIL_FORMAT_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$"
-    private const val PASSWORD_FORMAT_REGEX = "^(?=.*[a-zA-Z])(?=.*[0-9]).{8,16}$"
+    private val WITHOUT_SPECIAL_CHARS_REGEX = "^[a-zA-Z가-힣0-9]+$".toRegex()
+    private val EMAIL_FORMAT_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$".toRegex()
+    private val PASSWORD_FORMAT_REGEX = "^(?=.*[a-zA-Z])(?=.*[0-9]).{8,16}$".toRegex()
 
-    fun String.isWithoutSpecialChars() = matches(WITHOUT_SPECIAL_CHARS_REGEX.toRegex())
-    fun String.isEmailFormat() = matches(EMAIL_FORMAT_REGEX.toRegex())
-    fun String.isPasswordFormat() = matches(PASSWORD_FORMAT_REGEX.toRegex())
+    fun String.isWithoutSpecialChars() = matches(WITHOUT_SPECIAL_CHARS_REGEX)
+    fun String.isEmailFormat() = matches(EMAIL_FORMAT_REGEX)
+    fun String.isPasswordFormat() = matches(PASSWORD_FORMAT_REGEX)
 }
