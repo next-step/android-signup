6148e51d657a5fd8b74fcde8e7fb43fc222f497c
hyemdooly <hyemdooly@gmail.com>
2025-02-10 23:57:03 +0900

    feat: MainActivity 작성 및 password 케이스 구현

--- app/src/main/java/nextstep/signup/MainActivity.kt
+++ app/src/main/java/nextstep/signup/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import nextstep.signup.ui.screen.SignupScreen
@@ -19,7 +21,30 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = Color.White
                 ) {
-                    SignupScreen(modifier = Modifier.fillMaxSize())
+                    val username = remember { mutableStateOf("") }
+                    val email = remember { mutableStateOf("") }
+                    val password = remember { mutableStateOf("") }
+                    val passwordConfirm = remember { mutableStateOf("") }
+
+                    SignupScreen(
+                        modifier = Modifier.fillMaxSize(),
+                        username = username.value,
+                        email = email.value,
+                        password = password.value,
+                        passwordConfirm = passwordConfirm.value,
+                        onUsernameChange = {
+                            username.value = it
+                        },
+                        onEmailChange = {
+                            email.value = it
+                        },
+                        onPasswordChange = {
+                            password.value = it
+                        },
+                        onPasswordConfirmChange = {
+                            passwordConfirm.value = it
+                        }
+                    )
                 }
             }
         }
--- app/src/main/java/nextstep/signup/ui/screen/SignupScreen.kt
+++ app/src/main/java/nextstep/signup/ui/screen/SignupScreen.kt
@@ -1,19 +1,21 @@
 package nextstep.signup.ui.screen
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -24,6 +26,14 @@ import nextstep.signup.ui.theme.SignupTheme
 
 @Composable
 fun SignupScreen(
+    username: String,
+    email: String,
+    password: String,
+    passwordConfirm: String,
+    onUsernameChange: (String) -> Unit,
+    onEmailChange: (String) -> Unit,
+    onPasswordChange: (String) -> Unit,
+    onPasswordConfirmChange: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
     Column(
@@ -43,30 +53,41 @@ fun SignupScreen(
         )
         Spacer(modifier = Modifier.height(42.dp))
         SignupTextField(
-            value = "",
-            onValueChange = {},
+            value = username,
+            onValueChange = onUsernameChange,
             label = stringResource(R.string.username),
             modifier = Modifier.fillMaxWidth()
         )
         Spacer(modifier = Modifier.height(33.dp))
         SignupTextField(
-            value = "",
-            onValueChange = {},
+            value = email,
+            onValueChange = onEmailChange,
             label = stringResource(R.string.email),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email
+            ),
             modifier = Modifier.fillMaxWidth()
         )
         Spacer(modifier = Modifier.height(33.dp))
         SignupTextField(
-            value = "",
-            onValueChange = {},
+            value = password,
+            onValueChange = onPasswordChange,
             label = stringResource(R.string.password),
+            keyboardOptions = KeyboardOptions(
+                autoCorrectEnabled = false,
+                keyboardType = KeyboardType.Password
+            ),
             modifier = Modifier.fillMaxWidth()
         )
         Spacer(modifier = Modifier.height(33.dp))
         SignupTextField(
-            value = "",
-            onValueChange = {},
+            value = passwordConfirm,
+            onValueChange = onPasswordConfirmChange,
             label = stringResource(R.string.password_confirm),
+            keyboardOptions = KeyboardOptions(
+                autoCorrectEnabled = false,
+                keyboardType = KeyboardType.Password
+            ),
             modifier = Modifier.fillMaxWidth()
         )
         Spacer(modifier = Modifier.height(39.dp))
@@ -84,8 +105,29 @@ fun SignupScreen(
 @Composable
 private fun SignupScreenPreview() {
     SignupTheme {
-        SignupScreen(modifier = Modifier
-            .fillMaxSize()
-            .background(Color.White))
+        val username = remember { mutableStateOf("") }
+        val email = remember { mutableStateOf("") }
+        val password = remember { mutableStateOf("") }
+        val passwordConfirm = remember { mutableStateOf("") }
+
+        SignupScreen(
+            modifier = Modifier.fillMaxSize(),
+            username = username.value,
+            email = email.value,
+            password = password.value,
+            passwordConfirm = passwordConfirm.value,
+            onUsernameChange = {
+                username.value = it
+            },
+            onEmailChange = {
+                email.value = it
+            },
+            onPasswordChange = {
+                password.value = it
+            },
+            onPasswordConfirmChange = {
+                passwordConfirm.value = it
+            }
+        )
     }
 }
