16415e8edebc6166a5cb62e61c32e03013b3313c
KimMooHyeon <dlfb77@gmail.com>
2025-02-12 00:32:52 +0900

    feat : 회원가입 컴포넌트 유효성 검증에 따른 에러 문구 표출 기능 추가 및 화면적용

--- app/src/main/java/nextstep/signup/signup/component/SignUpContents.kt
+++ app/src/main/java/nextstep/signup/signup/component/SignUpContents.kt
@@ -7,14 +7,23 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import nextstep.signup.R
+import nextstep.signup.signup.util.ValidationUtil.isEmailValid
+import nextstep.signup.signup.util.ValidationUtil.isNameValid
+import nextstep.signup.signup.util.ValidationUtil.isPasswordMatch
+import nextstep.signup.signup.util.ValidationUtil.isPasswordValid
 
 @Composable
 fun SignUpContents(modifier: Modifier = Modifier) {
@@ -23,28 +32,57 @@ fun SignUpContents(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.spacedBy(36.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        var nameFieldValue by remember { mutableStateOf(TextFieldValue("")) }
+        var nameErrorMessage by remember { mutableStateOf("") }
+        var emailFieldValue by remember { mutableStateOf(TextFieldValue("")) }
+        var emailErrorMessage by remember { mutableStateOf("") }
+        var passwordFieldValue by remember { mutableStateOf(TextFieldValue("")) }
+        var passwordErrorMessage by remember { mutableStateOf("") }
+        var passwordConfirmFieldValue by remember { mutableStateOf(TextFieldValue("")) }
+        var passwordConfirmErrorMessage by remember { mutableStateOf("") }
 
         SignUpInputForm(
             placeHolderText = stringResource(R.string.signup_main_input_name),
-            keyboardType = KeyboardType.Text
+            keyboardType = KeyboardType.Text,
+            textFieldValue = nameFieldValue,
+            onValueChange = { newTextFieldValue ->
+                nameFieldValue = newTextFieldValue
+                nameErrorMessage = isNameValid(newTextFieldValue.text)
+            },
+            errorMessage = nameErrorMessage
         )
-
 
         SignUpInputForm(
             placeHolderText = stringResource(R.string.signup_main_input_email),
-            keyboardType = KeyboardType.Text
+            keyboardType = KeyboardType.Text,
+            textFieldValue = emailFieldValue,
+            onValueChange = { newTextFieldValue ->
+                emailFieldValue = newTextFieldValue
+                emailErrorMessage = isEmailValid(newTextFieldValue.text)
+            }, errorMessage = emailErrorMessage
         )
 
         SignUpInputForm(
             placeHolderText = stringResource(R.string.signup_main_input_password),
-            keyboardType = KeyboardType.NumberPassword,
-            visualTransformation = PasswordVisualTransformation()
+            keyboardType = KeyboardType.Password,
+            visualTransformation = PasswordVisualTransformation(),
+            textFieldValue = passwordFieldValue,
+            onValueChange = { newTextFieldValue ->
+                passwordFieldValue = newTextFieldValue
+                passwordErrorMessage = isPasswordValid(newTextFieldValue.text)
+            },
+            errorMessage = passwordErrorMessage
         )
 
         SignUpInputForm(
             placeHolderText = stringResource(R.string.signup_main_input_password_confirm),
-            keyboardType = KeyboardType.NumberPassword,
-            visualTransformation = PasswordVisualTransformation()
+            keyboardType = KeyboardType.Password,
+            visualTransformation = PasswordVisualTransformation(),
+            textFieldValue = passwordConfirmFieldValue,
+            onValueChange = { newTextFieldValue ->
+                passwordConfirmFieldValue = newTextFieldValue
+            },
+            errorMessage = isPasswordMatch(passwordFieldValue.text, passwordConfirmFieldValue.text)
         )
 
         SignUpButton(
--- app/src/main/java/nextstep/signup/signup/component/SignUpInputForm.kt
+++ app/src/main/java/nextstep/signup/signup/component/SignUpInputForm.kt
@@ -1,7 +1,10 @@
 package nextstep.signup.signup.component
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -12,10 +15,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import nextstep.signup.R
 
 
 @Composable
@@ -23,30 +29,51 @@ fun SignUpInputForm(
     modifier: Modifier = Modifier,
     placeHolderText: String,
     keyboardType: KeyboardType,
-    visualTransformation: VisualTransformation = VisualTransformation.None
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    errorMessage: String,
+    textFieldValue: TextFieldValue,
+    onValueChange: (TextFieldValue) -> Unit
 ) {
-    var textFieldValue by remember { mutableStateOf(TextFieldValue("")) }
-    TextField(
-        value = textFieldValue,
-        onValueChange = { newTextFieldValue ->
-            textFieldValue = newTextFieldValue
-        },
-        label = { Text(placeHolderText) },
-        visualTransformation = visualTransformation,
-        singleLine = true,
-        placeholder = { Text(placeHolderText) },
-        keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
-        modifier = modifier.fillMaxWidth(),
-        colors = TextFieldDefaults.colors(
-            focusedContainerColor = Color(0xFFE3E8F1),
-            unfocusedContainerColor = Color(0xFFE3E8F1)
-        ),
-    )
+
+    Column {
+        TextField(
+            value = textFieldValue,
+            onValueChange = onValueChange,
+            label = { Text(placeHolderText) },
+            visualTransformation = visualTransformation,
+            singleLine = true,
+            placeholder = { Text(placeHolderText) },
+            keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
+            modifier = modifier.fillMaxWidth(),
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color(0xFFE3E8F1),
+                unfocusedContainerColor = Color(0xFFE3E8F1)
+            ),
+            isError = errorMessage.isNotEmpty(),
+        )
+        if (errorMessage.isNotEmpty()) {
+            Text(
+                text = errorMessage,
+                color = MaterialTheme.colorScheme.error,
+                style = MaterialTheme.typography.bodySmall,
+                modifier = modifier.fillMaxWidth().padding(horizontal = 16.dp),
+            )
+        }
+    }
 }
 
 
 @Preview(showBackground = true)
 @Composable
 private fun SignUpInputFormPreview() {
-    SignUpInputForm(placeHolderText = "placeHolderText", keyboardType = KeyboardType.Text)
+    var nameFieldValue by remember { mutableStateOf(TextFieldValue("")) }
+    SignUpInputForm(
+        placeHolderText = stringResource(R.string.signup_main_input_email),
+        keyboardType = KeyboardType.Text,
+        textFieldValue = nameFieldValue,
+        onValueChange = { newTextFieldValue ->
+            nameFieldValue = newTextFieldValue
+        },
+        errorMessage = "error 문구"
+    )
 }
