378d8826bea69e4252d214edf611a159c0a1ae4c
KimMooHyeon <dlfb77@gmail.com>
2025-02-12 01:08:44 +0900

    refactor : 에러 메세지 상수로 변경

--- app/src/main/java/nextstep/signup/signup/util/ValidationUtil.kt
+++ app/src/main/java/nextstep/signup/signup/util/ValidationUtil.kt
@@ -1,18 +1,25 @@
 package nextstep.signup.signup.util
 
-const val USERNAME_REGEX = "^[a-zA-Z가-힣]+$"
-const val EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$"
-const val PASSWORD_REGEX = "^(?=.*[a-zA-Z])(?=.*[0-9]).{8,16}$"
-
 object ValidationUtil {
+
+    private const val USERNAME_LENGTH_ERROR = "이름은 2~5자여야 합니다."
+    private const val USERNAME_FORMAT_ERROR = "이름에는 숫자나 기호가 포함될 수 없습니다."
+    private const val EMAIL_FORMAT_ERROR = "이메일 형식이 올바르지 않습니다."
+    private const val PASSWORD_LENGTH_ERROR = "비밀번호는 8~16자여야 합니다."
+    private const val PASSWORD_FORMAT_ERROR = "비밀번호는 영문과 숫자를 포함해야 합니다."
+    private const val PASSWORD_MISMATCH_ERROR = "비밀번호가 일치하지 않습니다."
+    private const val USERNAME_REGEX = "^[a-zA-Z가-힣]+$"
+    private const val EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$"
+    private const val PASSWORD_REGEX = "^(?=.*[a-zA-Z])(?=.*[0-9]).{8,16}$"
+
     fun isNameValid(username: String): String {
         return when {
             username.length < 2 || username.length > 5 -> {
-                "이름은 2~5자여야 합니다."
+                USERNAME_LENGTH_ERROR
             }
 
             !username.matches(Regex(USERNAME_REGEX)) -> {
-                "이름에는 숫자나 기호가 포함될 수 없습니다."
+                USERNAME_FORMAT_ERROR
             }
 
             else -> {
@@ -25,7 +32,7 @@ object ValidationUtil {
         return when {
 
             !email.matches(Regex(EMAIL_REGEX)) -> {
-                "이메일 형식이 올바르지 않습니다."
+                EMAIL_FORMAT_ERROR
             }
 
             else -> {
@@ -37,11 +44,11 @@ object ValidationUtil {
     fun isPasswordValid(password: String): String {
         return when {
             password.length < 8 || password.length > 16 -> {
-                "비밀번호는 8~16자여야 합니다."
+                PASSWORD_LENGTH_ERROR
             }
 
             !password.matches(Regex(PASSWORD_REGEX)) -> {
-                "비밀번호는 영문과 숫자를 포함해야 합니다."
+                PASSWORD_FORMAT_ERROR
             }
 
             else -> {
@@ -57,7 +64,7 @@ object ValidationUtil {
             }
 
             password != passwordConfirm -> {
-                "비밀번호가 일치하지 않습니다."
+                PASSWORD_MISMATCH_ERROR
             }
 
             else -> {
