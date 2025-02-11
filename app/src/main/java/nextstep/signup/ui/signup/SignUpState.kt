44370d1c60c417603fa7f63ae1095e82845df8ac
DoTheBestMayB <send1684@gmail.com>
2025-02-11 19:53:35 +0900

    refactor: derivedStateOf 활용하기

--- app/src/main/java/nextstep/signup/ui/signup/SignUpState.kt
+++ app/src/main/java/nextstep/signup/ui/signup/SignUpState.kt
@@ -1,5 +1,6 @@
 package nextstep.signup.ui.signup
 
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -10,40 +11,40 @@ class SignUpState(
 
     var userName by mutableStateOf("")
         private set
-    var userNameValidation by mutableStateOf(UserNameValidationState())
-        private set
+    val userNameValidation by derivedStateOf {
+        inputValidator.checkUserName(userName)
+    }
 
     var email by mutableStateOf("")
         private set
-    var emailValidation by mutableStateOf(false)
-        private set
+    val emailValidation by derivedStateOf {
+        inputValidator.checkEmail(email)
+    }
 
     var password by mutableStateOf("")
         private set
-    var passwordValidation by mutableStateOf(PasswordValidation())
-        private set
+    val passwordValidation by derivedStateOf {
+        inputValidator.checkPassword(password)
+    }
 
     var passwordConfirm by mutableStateOf("")
         private set
-    var isPasswordConfirmValid by mutableStateOf(true)
-        private set
+    val isPasswordConfirmValid by derivedStateOf {
+        password == passwordConfirm
+    }
 
     fun onAction(action: SignUpAction) {
         when (action) {
             is SignUpAction.OnEmailChange -> {
                 email = action.value
-                emailValidation = inputValidator.checkEmail(action.value)
             }
 
             is SignUpAction.OnPasswordChange -> {
                 password = action.value
-                passwordValidation = inputValidator.checkPassword(action.value)
-                isPasswordConfirmValid = action.value == passwordConfirm
             }
 
             is SignUpAction.OnPasswordConfirmChange -> {
                 passwordConfirm = action.value
-                isPasswordConfirmValid = password == action.value
             }
 
             SignUpAction.OnSignUpClick -> {
@@ -52,7 +53,6 @@ class SignUpState(
 
             is SignUpAction.OnUsernameChange -> {
                 userName = action.value
-                userNameValidation = inputValidator.checkUserName(action.value)
             }
         }
     }
