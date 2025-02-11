24c772773c57fdfc91948e0b52849b8a369d290d
DoTheBestMayB <send1684@gmail.com>
2025-02-11 23:29:39 +0900

    test : InputValidator 사용자 닉네임 유닛 테스트 작성

---
+++ app/src/test/java/nextstep/signup/InputValidatorTest.kt
@@ -0,0 +1,100 @@
+package nextstep.signup
+
+import nextstep.signup.ui.signup.InputValidator
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+
+class InputValidatorTest {
+
+    private val inputValidator = InputValidator()
+
+    @ParameterizedTest
+    @MethodSource("usernameLengthProvider")
+    fun `사용자 닉네임 길이 테스트`(username: String, isInLength: Boolean) {
+        // when
+        val actual = inputValidator.checkUserName(username)
+
+        // then
+        assertEquals(isInLength, actual.isInLength)
+    }
+
+    @ParameterizedTest
+    @MethodSource("usernameNumberProvider")
+    fun `사용자 닉네임 숫자 테스트`(username: String, hasNumber: Boolean) {
+        // when
+        val actual = inputValidator.checkUserName(username)
+
+        // then
+        assertEquals(hasNumber, actual.hasNumber)
+    }
+
+    @ParameterizedTest
+    @MethodSource("usernameSpecialCharacterProvider")
+    fun `사용자 닉네임 특수문자 테스트`(username: String, hasSpecialCharacter: Boolean) {
+        // when
+        val actual = inputValidator.checkUserName(username)
+
+        // then
+        assertEquals(hasSpecialCharacter, actual.hasSpecialCharacter)
+    }
+
+    @ParameterizedTest
+    @MethodSource("usernameAllProvider")
+    fun `사용자 닉네임 Valid 테스트`(username: String, isValid: Boolean) {
+        // when
+        val actual = inputValidator.checkUserName(username)
+
+        // then
+        assertEquals(isValid, actual.isValidUsername)
+    }
+
+    companion object {
+        @JvmStatic
+        fun usernameLengthProvider(): Iterable<Array<Any>> {
+            return arrayListOf(
+                arrayOf("", false),
+                arrayOf("a", false),
+                arrayOf("ab", true),
+                arrayOf("abc", true),
+                arrayOf("abcd", true),
+                arrayOf("abcde", true),
+                arrayOf("abcdef", false),
+                arrayOf("abcdefg", false),
+            )
+        }
+
+        @JvmStatic
+        fun usernameNumberProvider(): Iterable<Array<Any>> {
+            return arrayListOf(
+                arrayOf("abc", false),
+                arrayOf("abc12", true),
+            )
+        }
+
+        @JvmStatic
+        fun usernameSpecialCharacterProvider(): Iterable<Array<Any>> {
+            return arrayListOf(
+                arrayOf("abc", false),
+                arrayOf("abc!", true),
+                arrayOf("abc😁", true),
+            )
+        }
+
+        @JvmStatic
+        fun usernameAllProvider(): Iterable<Array<Any>> {
+            return arrayListOf(
+                arrayOf("", false),
+                arrayOf("a", false),
+                arrayOf("ab", true),
+                arrayOf("12", false),
+                arrayOf("abc", true),
+                arrayOf("ab3", false),
+                arrayOf("ab!", false),
+                arrayOf("abcd", true),
+                arrayOf("abcde", true),
+                arrayOf("abcdef", false),
+            )
+        }
+    }
+}
