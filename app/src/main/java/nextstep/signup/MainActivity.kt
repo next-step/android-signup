d2449ada73dc30a1535741a5eca7aaa0f8074274
KimMooHyeon <dlfb77@gmail.com>
2025-02-11 23:24:08 +0900

    chore : 불필요한 Surface 컴포저블 제거

--- app/src/main/java/nextstep/signup/MainActivity.kt
+++ app/src/main/java/nextstep/signup/MainActivity.kt
@@ -3,9 +3,6 @@ package nextstep.signup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
 import nextstep.signup.signup.SignUpScreen
 import nextstep.signup.ui.theme.SignupTheme
 
@@ -14,12 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SignupTheme {
-                Surface(
-                    modifier = Modifier
-                        .fillMaxSize()
-                ) {
-                    SignUpScreen()
-                }
+                SignUpScreen()
             }
         }
     }
