5714494904fdfa164196db9cd7267e1e9f9379c1
KimMooHyeon <dlfb77@gmail.com>
2025-02-12 01:07:52 +0900

    test : 패스워드, 이메일 검증 테스트 코드 추가

--- app/src/androidTest/java/nextstep/signup/InputValidationTest.kt
+++ app/src/androidTest/java/nextstep/signup/InputValidationTest.kt
@@ -1,5 +1,6 @@
 package nextstep.signup
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.test.junit4.createComposeRule
@@ -7,7 +8,9 @@ import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import nextstep.signup.signup.component.SignUpInputForm
+import nextstep.signup.signup.util.ValidationUtil.isEmailValid
 import nextstep.signup.signup.util.ValidationUtil.isNameValid
+import nextstep.signup.signup.util.ValidationUtil.isPasswordValid
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -19,19 +22,51 @@ class InputValidationTest {
     private var nameFieldValue = mutableStateOf(TextFieldValue(""))
     private var nameErrorMessage = mutableStateOf("")
 
+    private var emailFieldValue = mutableStateOf(TextFieldValue(""))
+    private var emailErrorMessage = mutableStateOf("")
+
+    private var passwordFieldValue = mutableStateOf(TextFieldValue(""))
+    private var passwordErrorMessage = mutableStateOf("")
+
+
     @Before
     fun setup() {
         composeTestRule.setContent {
-            SignUpInputForm(
-                placeHolderText = stringResource(R.string.signup_main_input_name),
-                keyboardType = KeyboardType.Text,
-                textFieldValue = nameFieldValue.value,
-                onValueChange = { newTextFieldValue ->
-                    nameFieldValue.value = newTextFieldValue
-                    nameErrorMessage.value = isNameValid(newTextFieldValue.text)
-                },
-                errorMessage = nameErrorMessage.value
-            )
+            Column {
+                SignUpInputForm(
+                    placeHolderText = stringResource(R.string.signup_main_input_name),
+                    keyboardType = KeyboardType.Text,
+                    textFieldValue = nameFieldValue.value,
+                    onValueChange = { newTextFieldValue ->
+                        nameFieldValue.value = newTextFieldValue
+                        nameErrorMessage.value = isNameValid(newTextFieldValue.text)
+                    },
+                    errorMessage = nameErrorMessage.value
+                )
+
+                SignUpInputForm(
+                    placeHolderText = stringResource(R.string.signup_main_input_email),
+                    keyboardType = KeyboardType.Text,
+                    textFieldValue = emailFieldValue.value,
+                    onValueChange = { newTextFieldValue ->
+                        emailFieldValue.value = newTextFieldValue
+                        emailErrorMessage.value = isEmailValid(newTextFieldValue.text)
+                    },
+                    errorMessage = emailErrorMessage.value
+                )
+
+                SignUpInputForm(
+                    placeHolderText = stringResource(R.string.signup_main_input_password),
+                    keyboardType = KeyboardType.Text,
+                    textFieldValue = passwordFieldValue.value,
+                    onValueChange = { newTextFieldValue ->
+                        passwordFieldValue.value = newTextFieldValue
+                        passwordErrorMessage.value = isEmailValid(newTextFieldValue.text)
+                    },
+                    errorMessage = passwordErrorMessage.value
+                )
+            }
+
         }
     }
 
@@ -56,8 +91,54 @@ class InputValidationTest {
             .onNodeWithText(USERNAME_LENGTH_ERROR)
             .assertExists()
     }
+    @Test
+    fun 사용자_이름에_숫자나_기호가_포함되면_에러메시지가_노출된다() {
+        // when
+        nameFieldValue.value = TextFieldValue("김무1무")
+        nameErrorMessage.value = isNameValid(nameFieldValue.value.text)
+        // then
+        composeTestRule
+            .onNodeWithText(USERNAME_FORMAT_ERROR)
+            .assertExists()
+    }
+
+    @Test
+    fun 이메일이_올바른_형식이_아니면_에러메시지가_노출된다() {
+        // when
+        emailFieldValue.value = TextFieldValue("testasd")
+        emailErrorMessage.value = isEmailValid(emailFieldValue.value.text)
+        // then
+        composeTestRule
+            .onNodeWithText(EMAIL_FORMAT_ERROR)
+            .assertExists()
+    }
+    @Test
+    fun 비밀번호가_8에서_16자가_아니면_에러메시지가_노출된다() {
+        // when
+        passwordFieldValue.value = TextFieldValue("12das34")
+        passwordErrorMessage.value = isPasswordValid(passwordFieldValue.value.text)
+        // then
+        composeTestRule
+            .onNodeWithText(PASSWORD_LENGTH_ERROR)
+            .assertExists()
+    }
+    @Test
+    fun 비밀번호가_영문과_숫자를_포함하지_않으면_에러메시지가_노출된다() {
+        // when
+        passwordFieldValue.value = TextFieldValue("123456789")
+        passwordErrorMessage.value = isPasswordValid(passwordFieldValue.value.text)
+        // then
+        composeTestRule
+            .onNodeWithText(PASSWORD_FORMAT_ERROR)
+            .assertExists()
+    }
+
 
     companion object {
         private const val USERNAME_LENGTH_ERROR = "이름은 2~5자여야 합니다."
+        private const val USERNAME_FORMAT_ERROR = "이름에는 숫자나 기호가 포함될 수 없습니다."
+        private const val EMAIL_FORMAT_ERROR = "이메일 형식이 올바르지 않습니다."
+        private const val PASSWORD_LENGTH_ERROR = "비밀번호는 8~16자여야 합니다."
+        private const val PASSWORD_FORMAT_ERROR = "비밀번호는 영문과 숫자를 포함해야 합니다."
     }
 }
