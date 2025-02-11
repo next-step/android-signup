8b627a94c9d4599e5d3081a8d84898e13a89419b
KimMooHyeon <dlfb77@gmail.com>
2025-02-11 23:32:58 +0900

    refactor : LazyColumn를 Column으로 대체 후 세로 스크롤 적용

--- app/src/main/java/nextstep/signup/signup/component/SignUpContents.kt
+++ app/src/main/java/nextstep/signup/signup/component/SignUpContents.kt
@@ -1,9 +1,11 @@
 package nextstep.signup.signup.component
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,45 +18,41 @@ import nextstep.signup.R
 
 @Composable
 fun SignUpContents(modifier: Modifier = Modifier) {
-    LazyColumn(
-        modifier = modifier,
+    Column(
+        modifier = modifier.verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.spacedBy(36.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        item {
-            SignUpInputForm(
-                placeHolderText = stringResource(R.string.signup_main_input_name),
-                keyboardType = KeyboardType.Text
-            )
-        }
-        item {
-            SignUpInputForm(
-                placeHolderText = stringResource(R.string.signup_main_input_email),
-                keyboardType = KeyboardType.Text
-            )
-        }
-        item {
-            SignUpInputForm(
-                placeHolderText = stringResource(R.string.signup_main_input_password),
-                keyboardType = KeyboardType.NumberPassword,
-                visualTransformation = PasswordVisualTransformation()
-            )
-        }
-        item {
-            SignUpInputForm(
-                placeHolderText = stringResource(R.string.signup_main_input_password_confirm),
-                keyboardType = KeyboardType.NumberPassword,
-                visualTransformation = PasswordVisualTransformation()
-            )
-        }
-        item {
-            SignUpButton(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(50.dp),
-                buttonTitle = stringResource(R.string.signup_main_signtup_button),
-                onClick = {})
-        }
+
+        SignUpInputForm(
+            placeHolderText = stringResource(R.string.signup_main_input_name),
+            keyboardType = KeyboardType.Text
+        )
+
+
+        SignUpInputForm(
+            placeHolderText = stringResource(R.string.signup_main_input_email),
+            keyboardType = KeyboardType.Text
+        )
+
+        SignUpInputForm(
+            placeHolderText = stringResource(R.string.signup_main_input_password),
+            keyboardType = KeyboardType.NumberPassword,
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        SignUpInputForm(
+            placeHolderText = stringResource(R.string.signup_main_input_password_confirm),
+            keyboardType = KeyboardType.NumberPassword,
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        SignUpButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp),
+            buttonTitle = stringResource(R.string.signup_main_signtup_button),
+            onClick = {})
     }
 }
 
