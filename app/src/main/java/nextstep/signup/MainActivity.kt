ae1ae200d8f5461a9bbb82216479d3e168fac8c3
Bandal <no1msh1217@gmail.com>
2025-02-11 22:27:59 +0900

    feat: Sign up 버튼을 클릭하면 회원가입 완료 스낵바가 노출된다.

--- app/src/main/java/nextstep/signup/MainActivity.kt
+++ app/src/main/java/nextstep/signup/MainActivity.kt
@@ -7,11 +7,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import kotlinx.coroutines.launch
 import nextstep.signup.domain.Email
 import nextstep.signup.domain.Password
 import nextstep.signup.domain.PasswordConfirm
@@ -66,15 +70,23 @@ class MainActivity : ComponentActivity() {
                 )
             }
 
+            val snackBarHostState = remember { SnackbarHostState() }
+            val scope = rememberCoroutineScope()
+
             SignupTheme {
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
                     containerColor = MaterialTheme.colorScheme.background,
+                    snackbarHost = { SnackbarHost(snackBarHostState) },
                 ) { paddingValues ->
                     SignUpScreen(
                         inputFields = inputFields,
                         inputFieldChangeListeners = inputFieldChangeListeners,
-                        onSignUpButtonClick = {},
+                        onSignUpButtonClick = {
+                            scope.launch {
+                                snackBarHostState.showSnackbar(getString(R.string.sign_up_sign_up_done))
+                            }
+                        },
                         modifier = Modifier
                             .fillMaxSize()
                             .padding(paddingValues)
