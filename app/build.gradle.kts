3c5b52267b91ecf82e42c215ec451695a0525cda
DoTheBestMayB <send1684@gmail.com>
2025-02-11 21:47:03 +0900

    build: ParameterizedTest 사용을 위한 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     testImplementation(libs.junit)
+    testImplementation(libs.junit.jupiter.params)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
