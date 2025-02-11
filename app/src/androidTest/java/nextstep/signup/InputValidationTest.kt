20db1b694005da86a1dbad27dccbc1254874f9de
KimMooHyeon <dlfb77@gmail.com>
2025-02-12 01:22:45 +0900

    test : 테스트 코드 중복을 제거 하여 각 케이스별로 필요한 컴포넌트만 사용

--- app/src/androidTest/java/nextstep/signup/InputValidationTest.kt
+++ app/src/androidTest/java/nextstep/signup/InputValidationTest.kt
@@ -8,10 +8,14 @@ import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import nextstep.signup.signup.component.SignUpInputForm
+import nextstep.signup.signup.util.ValidationUtil.PASSWORD_FORMAT_ERROR
+import nextstep.signup.signup.util.ValidationUtil.USERNAME_LENGTH_ERROR
+import nextstep.signup.signup.util.ValidationUtil.USERNAME_FORMAT_ERROR
+import nextstep.signup.signup.util.ValidationUtil.EMAIL_FORMAT_ERROR
+import nextstep.signup.signup.util.ValidationUtil.PASSWORD_LENGTH_ERROR
 import nextstep.signup.signup.util.ValidationUtil.isEmailValid
 import nextstep.signup.signup.util.ValidationUtil.isNameValid
 import nextstep.signup.signup.util.ValidationUtil.isPasswordValid
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -19,18 +23,13 @@ class InputValidationTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
-    private var nameFieldValue = mutableStateOf(TextFieldValue(""))
-    private var nameErrorMessage = mutableStateOf("")
 
-    private var emailFieldValue = mutableStateOf(TextFieldValue(""))
-    private var emailErrorMessage = mutableStateOf("")
+    @Test
+    fun 사용자_이름은_2에서_5자여야_한다() {
+        // given
+        val nameFieldValue = mutableStateOf(TextFieldValue("김컴포즈"))
+        val nameErrorMessage = mutableStateOf("")
 
-    private var passwordFieldValue = mutableStateOf(TextFieldValue(""))
-    private var passwordErrorMessage = mutableStateOf("")
-
-
-    @Before
-    fun setup() {
         composeTestRule.setContent {
             Column {
                 SignUpInputForm(
@@ -43,7 +42,80 @@ class InputValidationTest {
                     },
                     errorMessage = nameErrorMessage.value
                 )
+            }
+        }
+        // when
+        nameErrorMessage.value = isNameValid(nameFieldValue.value.text)
+        // then
+        composeTestRule
+            .onNodeWithText(USERNAME_LENGTH_ERROR)
+            .assertDoesNotExist()
+    }
 
+    @Test
+    fun 사용자_이름이_2에서_5자가_아니면_에러메시지가_노출된다() {
+        // given
+        val nameFieldValue = mutableStateOf(TextFieldValue("김컴포즈입니다"))
+        val nameErrorMessage = mutableStateOf("")
+
+        composeTestRule.setContent {
+            Column {
+                SignUpInputForm(
+                    placeHolderText = stringResource(R.string.signup_main_input_name),
+                    keyboardType = KeyboardType.Text,
+                    textFieldValue = nameFieldValue.value,
+                    onValueChange = { newTextFieldValue ->
+                        nameFieldValue.value = newTextFieldValue
+                        nameErrorMessage.value = isNameValid(newTextFieldValue.text)
+                    },
+                    errorMessage = nameErrorMessage.value
+                )
+            }
+        }
+        // when
+        nameErrorMessage.value = isNameValid(nameFieldValue.value.text)
+        // then
+        composeTestRule
+            .onNodeWithText(USERNAME_LENGTH_ERROR)
+            .assertExists()
+    }
+
+    @Test
+    fun 사용자_이름에_숫자나_기호가_포함되면_에러메시지가_노출된다() {
+        // given
+        val nameFieldValue = mutableStateOf(TextFieldValue("김무1무"))
+        val nameErrorMessage = mutableStateOf("")
+
+        composeTestRule.setContent {
+            Column {
+                SignUpInputForm(
+                    placeHolderText = stringResource(R.string.signup_main_input_name),
+                    keyboardType = KeyboardType.Text,
+                    textFieldValue = nameFieldValue.value,
+                    onValueChange = { newTextFieldValue ->
+                        nameFieldValue.value = newTextFieldValue
+                        nameErrorMessage.value = isNameValid(newTextFieldValue.text)
+                    },
+                    errorMessage = nameErrorMessage.value
+                )
+            }
+        }
+        // when
+        nameErrorMessage.value = isNameValid(nameFieldValue.value.text)
+        // then
+        composeTestRule
+            .onNodeWithText(USERNAME_FORMAT_ERROR)
+            .assertExists()
+    }
+
+    @Test
+    fun 이메일이_올바른_형식이_아니면_에러메시지가_노출된다() {
+        // given
+        val emailFieldValue = mutableStateOf(TextFieldValue("testasd"))
+        val emailErrorMessage = mutableStateOf("")
+
+        composeTestRule.setContent {
+            Column {
                 SignUpInputForm(
                     placeHolderText = stringResource(R.string.signup_main_input_email),
                     keyboardType = KeyboardType.Text,
@@ -54,91 +126,69 @@ class InputValidationTest {
                     },
                     errorMessage = emailErrorMessage.value
                 )
-
-                SignUpInputForm(
-                    placeHolderText = stringResource(R.string.signup_main_input_password),
-                    keyboardType = KeyboardType.Text,
-                    textFieldValue = passwordFieldValue.value,
-                    onValueChange = { newTextFieldValue ->
-                        passwordFieldValue.value = newTextFieldValue
-                        passwordErrorMessage.value = isEmailValid(newTextFieldValue.text)
-                    },
-                    errorMessage = passwordErrorMessage.value
-                )
             }
-
         }
-    }
-
-    @Test
-    fun 사용자_이름은_2에서_5자여야_한다() {
         // when
-        nameFieldValue.value = TextFieldValue("김컴포즈")
-        nameErrorMessage.value = isNameValid(nameFieldValue.value.text)
-        // then
-        composeTestRule
-            .onNodeWithText(USERNAME_LENGTH_ERROR)
-            .assertDoesNotExist()
-    }
-
-    @Test
-    fun 사용자_이름이_2에서_5자가_아니면_에러메시지가_노출된다() {
-        // when
-        nameFieldValue.value = TextFieldValue("김컴포즈입니다")
-        nameErrorMessage.value = isNameValid(nameFieldValue.value.text)
-        // then
-        composeTestRule
-            .onNodeWithText(USERNAME_LENGTH_ERROR)
-            .assertExists()
-    }
-    @Test
-    fun 사용자_이름에_숫자나_기호가_포함되면_에러메시지가_노출된다() {
-        // when
-        nameFieldValue.value = TextFieldValue("김무1무")
-        nameErrorMessage.value = isNameValid(nameFieldValue.value.text)
-        // then
-        composeTestRule
-            .onNodeWithText(USERNAME_FORMAT_ERROR)
-            .assertExists()
-    }
-
-    @Test
-    fun 이메일이_올바른_형식이_아니면_에러메시지가_노출된다() {
-        // when
-        emailFieldValue.value = TextFieldValue("testasd")
         emailErrorMessage.value = isEmailValid(emailFieldValue.value.text)
         // then
         composeTestRule
             .onNodeWithText(EMAIL_FORMAT_ERROR)
             .assertExists()
     }
+
     @Test
     fun 비밀번호가_8에서_16자가_아니면_에러메시지가_노출된다() {
+        // given
+        val passwordFieldValue = mutableStateOf(TextFieldValue("12das34"))
+        val passwordErrorMessage = mutableStateOf("")
+
+        composeTestRule.setContent {
+            Column {
+                SignUpInputForm(
+                    placeHolderText = stringResource(R.string.signup_main_input_password),
+                    keyboardType = KeyboardType.Text,
+                    textFieldValue = passwordFieldValue.value,
+                    onValueChange = { newTextFieldValue ->
+                        passwordFieldValue.value = newTextFieldValue
+                        passwordErrorMessage.value = isPasswordValid(newTextFieldValue.text)
+                    },
+                    errorMessage = passwordErrorMessage.value
+                )
+            }
+        }
         // when
-        passwordFieldValue.value = TextFieldValue("12das34")
         passwordErrorMessage.value = isPasswordValid(passwordFieldValue.value.text)
         // then
         composeTestRule
             .onNodeWithText(PASSWORD_LENGTH_ERROR)
             .assertExists()
     }
+
     @Test
     fun 비밀번호가_영문과_숫자를_포함하지_않으면_에러메시지가_노출된다() {
+        // given
+        val passwordFieldValue = mutableStateOf(TextFieldValue("123456789"))
+        val passwordErrorMessage = mutableStateOf("")
+
+        composeTestRule.setContent {
+            Column {
+                SignUpInputForm(
+                    placeHolderText = stringResource(R.string.signup_main_input_password),
+                    keyboardType = KeyboardType.Text,
+                    textFieldValue = passwordFieldValue.value,
+                    onValueChange = { newTextFieldValue ->
+                        passwordFieldValue.value = newTextFieldValue
+                        passwordErrorMessage.value = isPasswordValid(newTextFieldValue.text)
+                    },
+                    errorMessage = passwordErrorMessage.value
+                )
+            }
+        }
         // when
-        passwordFieldValue.value = TextFieldValue("123456789")
         passwordErrorMessage.value = isPasswordValid(passwordFieldValue.value.text)
         // then
         composeTestRule
             .onNodeWithText(PASSWORD_FORMAT_ERROR)
             .assertExists()
-    }
-
-
-    companion object {
-        private const val USERNAME_LENGTH_ERROR = "이름은 2~5자여야 합니다."
-        private const val USERNAME_FORMAT_ERROR = "이름에는 숫자나 기호가 포함될 수 없습니다."
-        private const val EMAIL_FORMAT_ERROR = "이메일 형식이 올바르지 않습니다."
-        private const val PASSWORD_LENGTH_ERROR = "비밀번호는 8~16자여야 합니다."
-        private const val PASSWORD_FORMAT_ERROR = "비밀번호는 영문과 숫자를 포함해야 합니다."
     }
 }
--- app/src/main/java/nextstep/signup/signup/util/ValidationUtil.kt
+++ app/src/main/java/nextstep/signup/signup/util/ValidationUtil.kt
@@ -2,12 +2,12 @@ package nextstep.signup.signup.util
 
 object ValidationUtil {
 
-    private const val USERNAME_LENGTH_ERROR = "이름은 2~5자여야 합니다."
-    private const val USERNAME_FORMAT_ERROR = "이름에는 숫자나 기호가 포함될 수 없습니다."
-    private const val EMAIL_FORMAT_ERROR = "이메일 형식이 올바르지 않습니다."
-    private const val PASSWORD_LENGTH_ERROR = "비밀번호는 8~16자여야 합니다."
-    private const val PASSWORD_FORMAT_ERROR = "비밀번호는 영문과 숫자를 포함해야 합니다."
-    private const val PASSWORD_MISMATCH_ERROR = "비밀번호가 일치하지 않습니다."
+    const val USERNAME_LENGTH_ERROR = "이름은 2~5자여야 합니다."
+    const val USERNAME_FORMAT_ERROR = "이름에는 숫자나 기호가 포함될 수 없습니다."
+    const val EMAIL_FORMAT_ERROR = "이메일 형식이 올바르지 않습니다."
+    const val PASSWORD_LENGTH_ERROR = "비밀번호는 8~16자여야 합니다."
+    const val PASSWORD_FORMAT_ERROR = "비밀번호는 영문과 숫자를 포함해야 합니다."
+    const val PASSWORD_MISMATCH_ERROR = "비밀번호가 일치하지 않습니다."
     private const val USERNAME_REGEX = "^[a-zA-Z가-힣]+$"
     private const val EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$"
     private const val PASSWORD_REGEX = "^(?=.*[a-zA-Z])(?=.*[0-9]).{8,16}$"
