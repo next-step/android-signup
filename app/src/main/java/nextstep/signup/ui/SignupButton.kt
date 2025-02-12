e7c7ca280b1f2ffced660a5e779687cfc49ba9aa
minoo <minwoo@dailyshot.co>
2025-02-12 22:55:25 +0900

    style: SignUp 버튼 디자인 추가 (활성화, 비활성화)

--- app/src/main/java/nextstep/signup/ui/SignupButton.kt
+++ app/src/main/java/nextstep/signup/ui/SignupButton.kt
@@ -9,8 +9,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import nextstep.signup.ui.theme.BlackAlpha12
+import nextstep.signup.ui.theme.BlackDark
 import nextstep.signup.ui.theme.Blue50
 import nextstep.signup.ui.theme.White
 
@@ -19,15 +22,19 @@ import nextstep.signup.ui.theme.White
 fun SignupButton(
     text: String,
     onClick: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
 ) {
     Button(
         onClick = onClick,
         modifier = modifier.fillMaxWidth(),
+        enabled = enabled,
         shape = RoundedCornerShape(100.dp),
         colors = ButtonDefaults.buttonColors(
             containerColor = Blue50,
             contentColor = White,
+            disabledContainerColor = BlackAlpha12,
+            disabledContentColor = BlackDark
         ),
         contentPadding = PaddingValues(
             horizontal = 16.dp,
@@ -36,16 +43,27 @@ fun SignupButton(
     ) {
         Text(
             text = text,
+            modifier = if (enabled) modifier else modifier.alpha(0.38f),
             style = MaterialTheme.typography.labelLarge,
         )
     }
 }
 
-@Preview
+@Preview(name = "enable")
 @Composable
-fun SignupButtonPreview() {
+fun SignupButtonEnablePreview() {
     SignupButton(
         text = "Sign Up",
         onClick = { }
+    )
+}
+
+@Preview(name = "disabled", showBackground = true)
+@Composable
+fun SignupButtonDisablePreview() {
+    SignupButton(
+        text = "Sign Up",
+        onClick = { },
+        enabled = false
     )
 }
