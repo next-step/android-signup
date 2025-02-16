a225c82a9e195a1e653504ceda4d8ef890778c96
KimMooHyeon <dlfb77@gmail.com>
2025-02-16 23:35:32 +0900

    test : 회원가입 버튼 활성화 UI 테스트코드 추가

--- app/src/androidTest/java/nextstep/signup/InputValidationTest.kt
+++ app/src/androidTest/java/nextstep/signup/InputValidationTest.kt
@@ -2,12 +2,14 @@ package nextstep.signup
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.test.platform.app.InstrumentationRegistry
+import nextstep.signup.signup.component.SignUpContents
 import nextstep.signup.signup.component.SignUpInputForm
-import nextstep.signup.signup.component.getSignUpInputFieldErrorMessage
+import nextstep.signup.signup.getSignUpInputFieldErrorMessage
 import nextstep.signup.signup.util.ValidationUtil.isEmailValid
 import nextstep.signup.signup.util.ValidationUtil.isNameValid
 import nextstep.signup.signup.util.ValidationUtil.isPasswordValid
@@ -33,7 +35,8 @@ class InputValidationTest {
                 keyboardType = KeyboardType.Text,
                 inputText = nameFieldValue.value,
                 onValueChange = {},
-                errorMessage = nameErrorMessage.value
+                errorMessage = nameErrorMessage.value,
+                isFocused = true
             )
 
         }
@@ -61,7 +64,8 @@ class InputValidationTest {
                 keyboardType = KeyboardType.Text,
                 inputText = nameFieldValue.value,
                 onValueChange = {},
-                errorMessage = nameErrorMessage.value
+                errorMessage = nameErrorMessage.value,
+                isFocused = true
             )
 
         }
@@ -89,9 +93,9 @@ class InputValidationTest {
                 keyboardType = KeyboardType.Text,
                 inputText = nameFieldValue.value,
                 onValueChange = {},
-                errorMessage = nameErrorMessage.value
+                errorMessage = nameErrorMessage.value,
+                isFocused = true
             )
-
         }
         // when
         nameErrorMessage.value = getSignUpInputFieldErrorMessage(
@@ -116,7 +120,8 @@ class InputValidationTest {
                 keyboardType = KeyboardType.Text,
                 inputText = emailFieldValue.value,
                 onValueChange = {},
-                errorMessage = emailErrorMessage.value
+                errorMessage = emailErrorMessage.value,
+                isFocused = true
             )
         }
         // when
@@ -142,7 +147,8 @@ class InputValidationTest {
                 keyboardType = KeyboardType.Text,
                 inputText = passwordFieldValue.value,
                 onValueChange = {},
-                errorMessage = passwordErrorMessage.value
+                errorMessage = passwordErrorMessage.value,
+                isFocused = true
             )
         }
         // when
@@ -167,7 +173,8 @@ class InputValidationTest {
                 keyboardType = KeyboardType.Text,
                 inputText = passwordFieldValue.value,
                 onValueChange = {},
-                errorMessage = passwordErrorMessage.value
+                errorMessage = passwordErrorMessage.value,
+                isFocused = true
             )
         }
         // when
@@ -192,7 +199,8 @@ class InputValidationTest {
                 keyboardType = KeyboardType.Text,
                 inputText = inputText.value,
                 onValueChange = {},
-                errorMessage = ""
+                errorMessage = "",
+                isFocused = true
             )
         }
         // when
@@ -201,5 +209,111 @@ class InputValidationTest {
         composeTestRule
             .onNodeWithText("오류메세지입니다")
             .assertExists()
+    }
+
+
+    @Test
+    fun 비밀번호_인풋_비어있을때_버튼_비활성화_확인() {
+        // given when
+        composeTestRule.setContent {
+            SignUpContents(
+                nameInputText = "Philip Vincent",
+                nameErrorMessage = "Sherri Walsh",
+                emailInputText = "calvin.allen@example.com",
+                emailErrorMessage = "",
+                passwordInputText = "voluptaria",
+                passwordErrorMessage = "",
+                passwordConfirmInputText = "",
+                passwordConfirmErrorMessage = "",
+                buttonIsEnabled = false,
+                onShowSnackbar = {},
+                onNameValueChange = {},
+                onEmailValueChange = {},
+                onPasswordValueChange = {},
+                onPasswordConfirmValueChange = {}
+            )
+        }
+
+
+        // then
+        composeTestRule.onNodeWithText("Sign UP").assertIsNotEnabled()
+    }
+
+    @Test
+    fun 이메일_형식_안맞을때_버튼_비활성화_확인() {
+        // given when
+        composeTestRule.setContent {
+            SignUpContents(
+                nameInputText = "Philip Vincent",
+                nameErrorMessage = "",
+                emailInputText = "calvin.allen",
+                emailErrorMessage = "",
+                passwordInputText = "voluptaria",
+                passwordErrorMessage = "",
+                passwordConfirmInputText = "",
+                passwordConfirmErrorMessage = "",
+                buttonIsEnabled = false,
+                onShowSnackbar = {},
+                onNameValueChange = {},
+                onEmailValueChange = {},
+                onPasswordValueChange = {},
+                onPasswordConfirmValueChange = {}
+            )
+        }
+
+        // then
+        composeTestRule.onNodeWithText("Sign UP").assertIsNotEnabled()
+    }
+
+    @Test
+    fun 패스워드_매치_안될때_버튼_비활성화_확인() {
+        // given when
+        composeTestRule.setContent {
+            SignUpContents(
+                nameInputText = "Philip Vincent",
+                nameErrorMessage = "",
+                emailInputText = "calvin.allen",
+                emailErrorMessage = "",
+                passwordInputText = "12345678",
+                passwordErrorMessage = "",
+                passwordConfirmInputText = "123456789",
+                passwordConfirmErrorMessage = "",
+                buttonIsEnabled = false,
+                onShowSnackbar = {},
+                onNameValueChange = {},
+                onEmailValueChange = {},
+                onPasswordValueChange = {},
+                onPasswordConfirmValueChange = {}
+            )
+        }
+
+        // then
+        composeTestRule.onNodeWithText("Sign UP").assertIsNotEnabled()
+    }
+
+    @Test
+    fun 모든_입력필드_정상입력시_버튼_활성화_확인() {
+        // given when
+        composeTestRule.setContent {
+            SignUpContents(
+                nameInputText = "김무현",
+                nameErrorMessage = "",
+                emailInputText = "dlfb7@naver.com",
+                emailErrorMessage = "",
+                passwordInputText = "12345678",
+                passwordErrorMessage = "",
+                passwordConfirmInputText = "12345678",
+                passwordConfirmErrorMessage = "",
+                buttonIsEnabled = false,
+                onShowSnackbar = {},
+                onNameValueChange = {},
+                onEmailValueChange = {},
+                onPasswordValueChange = {},
+                onPasswordConfirmValueChange = {}
+            )
+        }
+
+        // then
+        composeTestRule.onNodeWithText("Sign UP").assertIsNotEnabled()
     }
 }
