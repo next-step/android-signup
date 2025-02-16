4d9887c58c5da3fc7022e96f0e1dc20146b2936b
KimMooHyeon <dlfb77@gmail.com>
2025-02-16 22:53:15 +0900

    feat : 회원가입 버튼 클릭시 스낵바 표출

--- app/src/main/java/nextstep/signup/signup/SignUpScreen.kt
+++ app/src/main/java/nextstep/signup/signup/SignUpScreen.kt
@@ -5,11 +5,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import nextstep.signup.R
 import nextstep.signup.signup.component.SignUpContents
 import nextstep.signup.signup.component.SignUpTitle
@@ -17,6 +22,9 @@ import nextstep.signup.ui.theme.SignupTheme
 
 @Composable
 fun SignUpScreen(modifier: Modifier = Modifier) {
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+
     Scaffold(
         modifier = modifier
             .fillMaxSize(),
@@ -28,8 +36,16 @@ fun SignUpScreen(modifier: Modifier = Modifier) {
                 title = stringResource(R.string.signup_main_title)
             )
         },
+        snackbarHost ={
+            SnackbarHost(snackbarHostState)
+        }
     ) { contentPadding ->
         SignUpContents(
+            onShowSnackbar = { message ->
+                scope.launch {
+                    snackbarHostState.showSnackbar(message)
+                }
+            },
             modifier = Modifier
                 .padding(contentPadding)
                 .padding(horizontal = 32.dp)
