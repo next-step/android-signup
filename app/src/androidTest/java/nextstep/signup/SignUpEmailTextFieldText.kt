d6c051ce70a84f385c23cd8e8f9ea77aac926bb9
YoonJam <yhw1904@gmail.com>
2025-02-19 23:08:45 +0900

    test: SignUpEmailTextFieldText 의 ui test case 추가

--- app/src/androidTest/java/nextstep/signup/SignUpEmailTextFieldText.kt
+++ app/src/androidTest/java/nextstep/signup/SignUpEmailTextFieldText.kt
@@ -12,7 +12,7 @@ class SignUpEmailTextFieldText {
     val composeTestRule = createComposeRule()
 
     @Test
-    fun 이메일_형식이_올바르지_않으면_오류_메시지를_노출한다() {
+    fun 이메일에_도메인이_없으면_오류_메시지를_노출한다() {
         // given
         composeTestRule.setContent {
             SignUpScreen()
@@ -21,10 +21,78 @@ class SignUpEmailTextFieldText {
         // then
         composeTestRule
             .onNodeWithText("Email")
-            .performTextInput("test")
+            .performTextInput("email@.com")
 
         composeTestRule
             .onNodeWithText("이메일 형식이 올바르지 않습니다.")
             .assertExists()
+    }
+
+    @Test
+    fun 이메일에_최상위_도메인이_없으면_오류_메시지를_노출한다() {
+        // given
+        composeTestRule.setContent {
+            SignUpScreen()
+        }
+
+        // then
+        composeTestRule
+            .onNodeWithText("Email")
+            .performTextInput("email@domain")
+
+        composeTestRule
+            .onNodeWithText("이메일 형식이 올바르지 않습니다.")
+            .assertExists()
+    }
+
+    @Test
+    fun 이메일에_골뱅이_기호가_없으면_오류_메시지를_노출한다() {
+        // given
+        composeTestRule.setContent {
+            SignUpScreen()
+        }
+
+        // then
+        composeTestRule
+            .onNodeWithText("Email")
+            .performTextInput("email.domain.com")
+
+        composeTestRule
+            .onNodeWithText("이메일 형식이 올바르지 않습니다.")
+            .assertExists()
+    }
+
+    @Test
+    fun 이메일_앞부분이_없으면_오류_메시지를_노출한다() {
+        // given
+        composeTestRule.setContent {
+            SignUpScreen()
+        }
+
+        // then
+        composeTestRule
+            .onNodeWithText("Email")
+            .performTextInput("@domain.com")
+
+        composeTestRule
+            .onNodeWithText("이메일 형식이 올바르지 않습니다.")
+            .assertExists()
+    }
+
+    @Test
+    fun 이메일이_올바르면_오류_메시지가_노출되지_않는다() {
+        // given
+        composeTestRule.setContent {
+            SignUpScreen()
+        }
+
+        // then
+        composeTestRule
+            .onNodeWithText("Email")
+            .performTextInput("email@domain.com")
+
+        composeTestRule
+            .onNodeWithText("이메일 형식이 올바르지 않습니다.")
+            .assertDoesNotExist()
     }
 }
