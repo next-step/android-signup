0c25fbaa80efc9473dc3f15a0e622fb7d01d4025
차민지 <ckalswl331@naver.com>
2025-03-04 18:47:33 +0900

    test: UI 테스트 코드 추가
    changes
    - Username 유효성 검사
    - Email 유효성 검사
    - password 유효성 검사
    - Confirm Password 유효성 검사

---
+++ app/src/androidTest/java/nextstep/signup/PasswordValidationTest.kt
@@ -0,0 +1,105 @@
+package nextstep.signup
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
+import nextstep.signup.ui.UserInputTextField
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class PasswordValidationTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+    private val password = mutableStateOf("")
+
+    @Before
+    fun setup() {
+        composeTestRule.setContent {
+            UserInputTextField(
+                value = password.value,
+                onValueChange = { newValue ->
+                    password.value = newValue
+                },
+                label = "password",
+                errorMessage = InputValidation.validatePassword(password.value)?.let {
+                    stringResource(it)
+                } ?: "",
+            )
+        }
+    }
+
+    @Test
+    fun 패스워드는_8에서_16자여야_한다() {
+        composeTestRule
+            .onNodeWithText("password")
+            .performTextInput("password12#")
+
+        composeTestRule
+            .onNodeWithText(PASSWORD_LENGTH_ERROR)
+            .assertDoesNotExist()
+    }
+
+    @Test
+    fun 패드웨드가_8자_미만이면_에러메시지가_노출된다() {
+        composeTestRule
+            .onNodeWithText("password")
+            .performTextInput("1234")
+
+        composeTestRule
+            .onNodeWithText(PASSWORD_LENGTH_ERROR)
+            .assertExists()
+    }
+
+    @Test
+    fun 패드웨드가_16자_초과이면_에러메시지가_노출된다() {
+        composeTestRule
+            .onNodeWithText("password")
+            .performTextInput("123456789012345678")
+
+        composeTestRule
+            .onNodeWithText(PASSWORD_LENGTH_ERROR)
+            .assertExists()
+    }
+
+    @Test
+    fun 패스워드는_영문과_숫자를_포함해야_한다() {
+        composeTestRule
+            .onNodeWithText("password")
+            .performTextInput("password12")
+
+        composeTestRule
+            .onNodeWithText(PASSWORD_CONTAIN_CHARACTERS_ERROR)
+            .assertDoesNotExist()
+    }
+
+    @Test
+    fun 패스워드가_영문을_포함하지_않으면_에러메시지가_노출된다() {
+        composeTestRule
+            .onNodeWithText("password")
+            .performTextInput("!@#$%^&12")
+
+        composeTestRule
+            .onNodeWithText(PASSWORD_CONTAIN_CHARACTERS_ERROR)
+            .assertExists()
+    }
+
+    @Test
+    fun 패스워드가_숫자를_포함하지_않으면_에러메시지가_노출된다() {
+        composeTestRule
+            .onNodeWithText("password")
+            .performTextInput("passwordpasd")
+
+        composeTestRule
+            .onNodeWithText(PASSWORD_CONTAIN_CHARACTERS_ERROR)
+            .assertExists()
+    }
+
+    companion object {
+        private const val PASSWORD_LENGTH_ERROR = "비밀번호는 8~16자여야 합니다."
+        private const val PASSWORD_CONTAIN_CHARACTERS_ERROR = "비밀번호는 영문과 숫자를 포함해야 합니다."
+    }
+}
